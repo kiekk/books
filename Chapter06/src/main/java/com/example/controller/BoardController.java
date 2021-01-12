effe1393097e41bf824ba8e42d2430204a6f84a9
kiekk <dbstnsgh2@naver.com>
2021-01-12 11:05:11 +0900

    day04 - 2

---
+++ Chapter06/src/main/java/com/example/controller/BoardController.java
@@ -0,0 +1,86 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Board;
+import com.example.service.BoardService;
+
+@Controller
+public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
+	/*
+	 	1. 컨트롤러에서 임의로 데이터를 생성하여 View 페이지로 데이터를 전달
+	@RequestMapping("/getBoardList")
+	public String getBoardList(Model model) {
+		List<Board> boardList = new ArrayList<>();
+		
+		for(int i=1;i<=10;i++) {
+			Board board = new Board();
+			board.setSeq(new Long(i));
+			board.setTitle("게시판 프로그램 예제");
+			board.setWriter("apple");
+			board.setContent("게시판 프로그램 예제 내용입니다.");
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+			boardList.add(board);
+		}
+		model.addAttribute("boardList", boardList);
+		return "getBoardList";
+	}
+	 */
+	
+	//2. JPA를 사용하여 실제 테이블에 저장되어 있는 데이터를 View 페이지로 전달
+	@RequestMapping("/getBoardList")
+	public String getBoardList(Model model, Board board) {
+		List<Board> boardList = boardService.getBoardList(board);
+		
+		model.addAttribute("boardList", boardList);
+		
+		return "getBoardList";
+	}
+	
+	//게시글 등록 페이지 이동
+	@GetMapping("/insertBoard")
+	public String insertBoardView() {
+		return "insertBoard";
+	}
+	
+	//게시글 등록 처리
+	@PostMapping("/insertBoard")
+	public String insertBoard(Board board) {
+		boardService.insertBoard(board);
+		return "redirect:getBoardList";
+	}
+	
+	//게시글 상세 조회
+	@GetMapping("/getBoard")
+	public String getBoard(Board board, Model model) {
+		model.addAttribute("board", boardService.getBoard(board));
+		return "getBoard";
+	}
+	
+	//게시글 수정
+	@PostMapping("/updateBoard")
+	public String updateBoard(Board board) {
+		boardService.updateBoard(board);
+		return "forward:getBoardList";
+	}
+	
+	//게시글 삭제
+	@GetMapping("/deleteBoard")
+	public String deleteBoard(Board board) {
+		boardService.deleteBoard(board);
+		return "forward:getBoardList";
+	}
+}
