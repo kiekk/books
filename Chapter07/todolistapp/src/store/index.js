67d6437d6911e1e74b80eb767e1a9fe2621f69bf
kiekk <dbstnsgh2@naver.com>
2021-05-11 22:36:18 +0900

    mapActions 추가

--- Chapter07/todolistapp/src/store/index.js
+++ Chapter07/todolistapp/src/store/index.js
@@ -27,6 +27,20 @@ const store = new Vuex.Store({
             var index = state.todolist.findIndex((item) => item.id === payload.id)
             state.todolist.splice(index, 1)
         }
+    },
+    actions: {
+        [Constant.ADD_TODO]: ({ commit }, payload) => {
+            console.log("###addTodo", payload)
+            commit(Constant.ADD_TODO, payload)
+        },
+        [Constant.DELETE_TODO]: ({ commit }, payload) => {
+            console.log("###deleteTodo", payload)
+            commit(Constant.DELETE_TODO, payload)
+        },
+        [Constant.DONE_TOGGLE]: ({ commit }, payload) => {
+            console.log("###doneToggle", payload)
+            commit(Constant.DONE_TOGGLE, payload)
+        }
     }
 })
 
