e33b878712a6227cb45651567e0e65ec1c4ee200
kiekk <dbstnsgh2@naver.com>
2021-07-12 08:45:54 +0900

    라우팅 모드

--- Chapter09/namedroutertest/src/App.vue
+++ Chapter09/namedroutertest/src/App.vue
@@ -29,9 +29,11 @@ import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Contacts from './components/Contact.vue'
 import ContactByNo from './components/ContactByNo.vue'
+import NotFound from './components/NotFound.vue'
 import VueRouter from 'vue-router'
 
 const router = new VueRouter({
+  mode: 'history',
   routes: [
     { path: '/', component: Home },
     { path: '/home', name: 'home', component: Home },
@@ -51,6 +53,7 @@ const router = new VueRouter({
         }
       ]
     },
+    { path: '*', component: NotFound }
   ]
 })
 
