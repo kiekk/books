e7b2f59657750f3b20016eee22eb90dc148cdb7d
kiekk <dbstnsgh2@naver.com>
2021-07-26 08:50:52 +0900

    컴포넌트 생성(화살표 함수 이용)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,30 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react'
+import MyComponent from './MyComponent'
 
-// 1. 함수형 컴포넌트
-// function App() {
-//     const name = '리액트';
-//     return (
-//         <>
-//             <div className="react">{name}</div>
-//             <input />
-//         </>
-//     )
-// }
-
-// 2. 클래스형 컴포넌트
-class App extends Component {
-    render() {
-        const name = 'React';
-        return (
-            <div className="react">{name}</div>
-        );
-    }
+const App = () => {
+    return (
+        <MyComponent />
+    );
 }
-
-/*
-    클래스형 컴포넌트에서는 render 함수가 꼭 있어야 하며, render 함수에서 JSX를 반환해야 합니다.
- */
 
 export default App;
---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+    return (
+        <div>
+            My First Component!
+        </div>
+    )
+}
+
+export default MyComponent;
