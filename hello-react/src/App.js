b52067ee8629f2616e62d42c2a0445fbb512f478
kiekk <dbstnsgh2@naver.com>
2021-07-26 07:54:49 +0900

    Fragmanet 생략 가능 → <Fragment> 대신 <> 사용 가능

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import {Fragment} from "react";
 
 function App() {
   return (
-      <Fragment>
+      <>
         <h1>Hello React!</h1>
         <h2>React Test</h2>
-      </Fragment>
+      </>
   );
 }
 
