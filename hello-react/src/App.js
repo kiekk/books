20c1405dde710924b139315a80f352d7ec520c35
kiekk <dbstnsgh2@naver.com>
2021-07-26 08:20:54 +0900

    class 설정 시 className 속성 사용 (class를 사용해도 동작은 하지만 아래와 같은 오류 발생)
    Invalid DOM property `class`. Did you mean `className`? at div at App

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,15 +3,8 @@ import './App.css';
 
 function App() {
     const name = '리액트';
-    const style = {
-        backgroundColor: 'black', // background-color => backgroundColor
-        color: 'aqua',
-        fontSize: '48px', // font-size => fontSize
-        fontWeight: 'bold', // font-weight => fontWeight
-        padding: 16 // 단위 생략시 px로 적용
-    }
     return (
-        <div style={style}>{name}</div>
+        <div className="react">{name}</div>
     )
 }
 
