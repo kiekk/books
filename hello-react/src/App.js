e87955a2d024b6eab0e4d0adad5bebf212e15cb0
kiekk <dbstnsgh2@naver.com>
2021-07-26 08:05:18 +0900

    잘못된 렌더링, undefined만 반환하여 렌더링 할 경우 아래와 같은 오류 발생
    Nothing was returned from render. This usually means a return statement is missing. Or, to render nothing, return null.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,14 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const name = '리액트'
-    return (
-        <>
-            {name === '리액트' && <h1>Hello {name}!</h1>}
-            {/* {name === '리액트' ? <h1>Hello {name}!</h1> : null } 과 동일하게 작동 */}
-            <h2>React Test</h2>
-        </>
-    );
+    const name = undefined;
+    return name;
 }
 
 export default App;
