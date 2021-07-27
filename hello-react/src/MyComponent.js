0986a11456aea0cca7e1321d6005776476a61c5b
kiekk <dbstnsgh2@naver.com>
2021-07-27 23:09:56 +0900

    isRequired를 이용하여 필수 propTyps 설정(오류)
    isRequired를 설정한 props를 전달하지 않을 경우 오류 발생
    
    Failed prop type: The prop `favoriteNumber` is marked as required in `MyComponent`, but its value is `undefined`.
        at MyComponent (http://localhost:3000/main.4256ac7ec996e03cb526.hot-update.js:24:3)
        at App

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
     return (
         <div>
             My First Component!<br />
             My Name is {name}<br />
-            children은 {children}입니다.
+            children은 {children}입니다.<br />
+            My FavoriteNumber is {favoriteNumber}
         </div>
     )
 }
@@ -16,7 +17,8 @@ MyComponent.defaultProps = {
 
 // name은 무조건 String으로 전달해야 함
 MyComponent.propTypes = {
-    name: PropTypes.string
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
 }
 
 export default MyComponent;
