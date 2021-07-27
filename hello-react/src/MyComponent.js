688f50bad918eb9418e36baff163fdee48330965
kiekk <dbstnsgh2@naver.com>
2021-07-27 23:15:11 +0900

    PropTypes 종류

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -21,4 +21,24 @@ MyComponent.propTypes = {
     favoriteNumber: PropTypes.number.isRequired
 }
 
+/*
+    PropType 종류
+    
+    ㆍ array: 배열
+    ㆍ arrayOf(다른 PropType): 특정 PropType으로 이루어진 배열을 의미합니다.
+       ex) arrayOf(PropTypes.number)는 숫자로 이루어진 배열입니다.
+    ㆍ bool: true or false
+    ㆍ func: 함수
+    ㆍ object: 객체
+    ㆍ string: 문자열
+    ㆍ symbol: ES6의 Symbol
+    ㆍ node: 렌더링 할 수 있는 모든 것(숫자, 문자열, JSX 코드)
+    ㆍ instanceOf(클래스): 특정 클래스의 인스턴스
+       ex) instanceOf(MyClass)
+    ㆍ oneOf(['dog', 'cat']): 주어진 배열 요소 값 중 하나
+    ㆍ oneOfType([React.PropTypes.string, PropTypes.number]): 주어진 배열 안의 종류 중 하나
+    ㆍ objectOf(React.PropTypes.number): 객체의 모든 키 값이 인자로 주어진 PropTypes인 객체
+    ㆍ shape({ name: PropTypes.string, num: PropTypes.number }): 주어진 스키마를 가진 객체
+    ㆍ any: 아무 종류
+ */
 export default MyComponent;
