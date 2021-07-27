fb684ff1224d414029f04350b427d0c24c296269
kiekk <dbstnsgh2@naver.com>
2021-07-27 23:07:55 +0900

    PropTypes로 props 검증(정상)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import MyComponent from './MyComponent'
 
 const App = () => {
     return (
-        <MyComponent name={3}>홍길동</MyComponent>
+        <MyComponent name="React">홍길동</MyComponent>
     );
 }
 
