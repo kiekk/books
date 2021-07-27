46516171dd043c457a9f5fe066b9d5a313f932da
kiekk <dbstnsgh2@naver.com>
2021-07-27 23:10:44 +0900

    isRequired를 이용하여 필수 propTyps 설정(정상)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import MyComponent from './MyComponent'
 
 const App = () => {
     return (
-        <MyComponent name="React">홍길동</MyComponent>
+        <MyComponent name="React" favoriteNumber={3}>홍길동</MyComponent>
     );
 }
 
