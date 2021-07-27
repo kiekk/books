e0b8b7dea0ed1b680e59734df7b404b704667c42
kiekk <dbstnsgh2@naver.com>
2021-07-27 23:07:25 +0900

    PropTypes로 props 검증(오류)
    지정한 타입이 아닌 다른 타입을 전달할 경우 하단 에러 발생
    
    Failed prop type: Invalid prop `name` of type `number` supplied to `MyComponent`, expected `string`.
        at MyComponent (http://localhost:3000/main.cad1216de486ceecd490.hot-update.js:24:3)
        at App

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import MyComponent from './MyComponent'
 
 const App = () => {
     return (
-        <MyComponent>홍길동</MyComponent>
+        <MyComponent name={3}>홍길동</MyComponent>
     );
 }
 
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const MyComponent = ({ name, children }) => {
     return (
         <div>
@@ -10,6 +12,11 @@ const MyComponent = ({ name, children }) => {
 
 MyComponent.defaultProps = {
     name: '김수박'
+}
+
+// name은 무조건 String으로 전달해야 함
+MyComponent.propTypes = {
+    name: PropTypes.string
 }
 
 export default MyComponent;
