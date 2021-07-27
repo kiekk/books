c172c27625e991533ff95358aca639fe78a9279f
kiekk <dbstnsgh2@naver.com>
2021-07-28 08:57:40 +0900

    useState 여러 번 사용

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -6,12 +6,17 @@ const Say = () => {
     const [ message, setMessage ] = useState('')
     const onClickEnter = () => setMessage('Hello!')
     const onClickLeave = () => setMessage('GoodBye!')
+
+    const [color, setColor] = useState('')
     
     return (
         <div>
             <button onClick={onClickEnter}>입장</button>
             <button onClick={onClickLeave}>퇴장</button>
-            <h1>{message}</h1>
+            <h1 style={{color}}>{message}</h1>
+            <button style={{color: 'red'}} onClick={() => setColor('red')}>빨간색</button>
+            <button style={{color: 'green'}} onClick={() => setColor('green')}>초록색</button>
+            <button style={{color: 'blue'}} onClick={() => setColor('blue')}>파랑색</button>
         </div>
     )
 }
