3093aabb13b127ded3a9a74186046138fb15f075
kiekk <dbstnsgh2@naver.com>
2021-07-28 08:50:22 +0900

    this.setState 끝난 후 특정 작업 실행 => 콜백 함수 설정

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -14,19 +14,15 @@ class Counter extends Component {
                 <h2>바뀌지 않는 값 : {fixedNumber}</h2>
                 <button
                     onClick={() => {
-                        // prevState: 기존 상태
-                        // props: 현재 지니고 있는 props
-                        this.setState(prevState => {
-                            return {
-                                number: prevState.number + 1
+                        this.setState(
+                            {
+                                number: number + 1
+                            },
+                            () => {
+                                console.log('setState 호출')
+                                console.log(this.state)
                             }
-                        })
-
-                        // 동일하게 동작
-                        // 아래 코드는 함수에서 바로 객체를 반환
-                        this.setState(prevState => ({
-                            number: prevState.number + 1
-                        }))
+                        )
                     }}
                 >
                     +1
