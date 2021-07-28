409bfedd1a7a2a7dc76a1dd7214eb1da5dc84234
kiekk <dbstnsgh2@naver.com>
2021-07-29 07:20:39 +0900

    state 초기화

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -18,11 +18,24 @@ class EventPractice extends Component {
                     type="text"
                     name="message"
                     placeholder="아무거나 입력해 보세요"
+                    value={this.state.message}
                     onChange={(e) => {
                         this.setState({
                             message: e.target.value
                         })
                     }}/>
+                <button
+                    onClick={
+                        () => {
+                            alert(this.state.message)
+                            this.setState({
+                                message: ''
+                            })
+                        }
+                    }
+                >
+                    확인
+                </button>
             </div>
         )
     }
