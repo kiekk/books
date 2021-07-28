5c5d2e0a377780c58503b48161eb4be4ba7669f0
kiekk <dbstnsgh2@naver.com>
2021-07-29 08:45:06 +0900

    map 함수로 반복
    *key 설정을 해줘야함

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,10 +1,9 @@
 const IterationSample = () => {
+    const names = ['눈사람', '얼음', '눈', '바람']
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>)
     return (
         <ul>
-            <li>눈사람</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람</li>
+            {nameList}
         </ul>
     )
 }
