400a95839b4a3191aa69676137c10293479a7147
kiekk <dbstnsgh2@naver.com>
2021-07-29 07:17:50 +0900

    이벤트 연결 후 state 저장

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -6,11 +6,23 @@ import { Component } from 'react'
     3. DOM 요소에만 이벤트 설정 가능
  */
 class EventPractice extends Component {
+    state = {
+        message: '',
+    }
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
-                <input type="text" name="message" placeholder="아무거나 입력해 보세요" onChange={(e) => {console.log(e.target.value)}}/>
+                <h2>{this.state.message}</h2>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해 보세요"
+                    onChange={(e) => {
+                        this.setState({
+                            message: e.target.value
+                        })
+                    }}/>
             </div>
         )
     }
