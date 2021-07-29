c9014f009d301ddc3eaaf75d0a4d1fc902e19300
kiekk <dbstnsgh2@naver.com>
2021-07-30 08:29:31 +0900

    컴포넌트 LifeCycleSample

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,12 +5,31 @@
 // import ValidationSample from './ValidationSample'
 import { Component } from 'react'
 // import ScrollBox from "./ScrollBox";
-import IterationSample from "./IterationSample";
+// import IterationSample from "./IterationSample";
+import LikeCycleSample from "./LikeCycleSample";
+
+// 랜덤 색상을 생성합니다.
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
 
 class App extends Component {
+    state = {
+        color: '#000000'
+    }
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor()
+        })
+    }
+
     render() {
         return (
-            <IterationSample />
+            <>
+                <button onClick={this.handleClick}>랜덤 생성</button>
+                <LikeCycleSample color={this.state.color} />
+            </>
         )
     }
 }
