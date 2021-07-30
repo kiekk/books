d127a49fa0898f3e10ba50858a2f4a4120749760
kiekk <dbstnsgh2@naver.com>
2021-07-30 21:40:31 +0900

    hook - useReducer - 1
    기존 Counter 컴포넌트를 useReducer를 이용하여 변경

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,18 +1,10 @@
-// import Counter from './Counter'
-import Info from './Info'
-import { useState } from 'react'
+import Counter from './Counter'
+// import Info from './Info'
 
 const App = () => {
-  const [visible, setVisible] = useState(false)
   return (
       <div>
-        <button onClick={() => {
-          setVisible(!visible)
-        }}>
-          {visible ? '숨기기' : '보이기'}
-        </button>
-        <hr />
-        {visible && <Info />}
+          <Counter />
       </div>
   )
 }
--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-/*
-    useState
-    가장 기본적인 Hook, 함수형 컴포넌트에서 가변 값을 가질 수 있게 해줌
- */
+import { useReducer } from 'react'
+
+function reducer(state, action) {
+    // action.type에 따라 다른 작업 수행
+    switch(action.type) {
+        case 'INCREMENT':
+            return { value: state.value + 1 }
+        case 'DECREMENT':
+            return { value: state.value - 1 }
+        default:
+            return state
+    }
+}
+
 const Counter = () => {
-    const [value, setValue] = useState(0)
+    // useReducer 첫 번째 파라미터 : reducer 함수
+    // 두 번째 파라미터 : 해당 reducer의 기본 값
+
+    // state: 현재 가리키고 있는 상태
+    // dispatch: 액션을 발생시키는 함수
+    const [state, dispatch] = useReducer(reducer, { value: 0 })
+
 
     return (
         <div>
-            <p>현재 카운터 값은 <b>{value}</b>입니다.</p>
-            <button onClick={() => setValue(value + 1)}>+1</button>
-            <button onClick={() => setValue(value - 1)}>-1</button>
+            <p>
+                현재 카운터 값은 <b>{state.value}</b>입니다.
+            </p>
+            <button onClick={() => dispatch({ type: 'INCREMENT' })}>+1</button>
+            <button onClick={() => dispatch({ type: 'DECREMENT' })}>-1</button>
         </div>
     )
 }
