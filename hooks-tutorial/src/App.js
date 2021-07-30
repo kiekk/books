28e5aaa4cd2b8b9489210eef1de55dc34498a8c6
kiekk <dbstnsgh2@naver.com>
2021-07-30 21:45:06 +0900

    hook - useReducer - 2
    여러 Input 상태 통합 관리
    기존 Info 컴포넌트를 useReducer를 이용하여 변경

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,10 @@
-import Counter from './Counter'
-// import Info from './Info'
+// import Counter from './Counter'
+import Info from './Info'
 
 const App = () => {
   return (
       <div>
-          <Counter />
+          <Info />
       </div>
   )
 }
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,44 +1,28 @@
-import { useState, useEffect } from 'react'
-/*
-    useState 함수는 하나의 상태 값만 관리,
-    관리해야 할 상태 값이 여러 개라면 useState를 여러 번 사용
+import { useReducer } from 'react'
 
-    useEffect는 리액트 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook
-    클래스형 컴포넌트의 componentDidMount + componentDidUpdate의 형태
- */
-const Info = () => {
-    const [name, setName] = useState('')
-    const [nickname, setNickname] = useState('')
-
-    // 두 번째 인자로 [](배열)을 넣어주면 마운트될 때만 실행
-    // [](배열)안에 변수를 넣으면 해당 변수가 바뀔때만 실행
-    useEffect(() => {
-        console.log('effect')
-        console.log(name)
-
-        // cleanup 함수
-        // 컴포넌트가 나타날 때 effect 호출, 사라질 때 cleanup 호출
-        return () => {
-            console.log('cleanup')
-            console.log(name)
-        }
-    }, [name]) 
-    // cleanup이 있는 상태에서 두 번째 인자로 [](배열)을 넣어주면 언마운트 될때만 cleanup 호출
-    // useEffect에서 state를 사용하는 경우 []안에 넣어줘야 함
-
-    const onChangeName = e => {
-        setName(e.target.value)
+function reducer(state, action) {
+    return {
+        ...state,
+        [action.name]: action.value
     }
+}
 
-    const onChangeNickname = e => {
-        setNickname(e.target.value)
+const Info = () => {
+    const [state, dispatch] = useReducer(reducer, {
+        name: '',
+        nickname: ''
+    })
+    const { name, nickname } = state
+
+    const onChange= e => {
+        dispatch(e.target)
     }
 
     return (
         <div>
             <div>
-                <input type="text" value={name} onChange={onChangeName}/>
-                <input type="text" value={nickname} onChange={onChangeNickname}/>
+                <input type="text" name="name" value={name} onChange={onChange}/>
+                <input type="text" name="nickname" value={nickname} onChange={onChange}/>
             </div>
             <div>
                 <div>
