e5a0a71f3aaf5168d1922a45efc44f9ca103e7cb
kiekk <dbstnsgh2@naver.com>
2021-08-01 01:31:30 +0900

    exact=true 적용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,13 +3,15 @@ import { Route } from 'react-router-dom'
 import About from './About'
 import Home from './Home'
 
+// '/about' 접속 시 '/' 규칙도 일치하기 때문에 Home, About 컴포넌트가 같이 출력
+// Home 컴포넌트에 exact=true 적용하여 '/about' 접속 시 About 컴포넌트만 출력
 const App = () => {
-  return (
-      <div>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </div>
-  )
+    return (
+        <div>
+            <Route path="/" component={Home} exact={true}/>
+            <Route path="/about" component={About} />
+        </div>
+    )
 }
 
 export default App
