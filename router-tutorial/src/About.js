8cc01196839266a14719623b53ed72795615d7c9
kiekk <dbstnsgh2@naver.com>
2021-08-01 01:29:21 +0900

    페이지 생성 및 라우터 적용

---
+++ router-tutorial/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>라우터 예제 프로젝트</p>
+        </div>
+    )
+}
+
+export default About
