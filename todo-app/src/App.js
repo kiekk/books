630da0afa87c9388927cb20b7843b74e5b2a5b36
kiekk <dbstnsgh2@naver.com>
2021-08-01 00:00:39 +0900

    React.memo 사용 (클래스형 컴포넌트의 shouldComponentUpdate) - 컴포넌트의 props가 바뀌지 않는다면 리렌더링 하지 않음
    todos 배열 업데이트시 onRemove, onToggle 함수로 새로 변경되지 않도록 수정
    - useState의 함수형 업데이트

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -27,28 +27,28 @@ const App = () => {
                 text,
                 checked: false,
             }
-            setTodos(todos.concat(todo))
+            setTodos(todos => todos.concat(todo))
             nextId.current += 1
         },
-        [todos],
+        [],
     )
 
     const onRemove = useCallback(
         id => {
-            setTodos(todos.filter(todo => todo.id !== id))
+            setTodos(todos => todos.filter(todo => todo.id !== id))
         },
-        [todos],
+        [],
     )
 
     const onToggle = useCallback(
         id => {
             setTodos(
-                todos.map(todo =>
+                todos => todos.map(todo =>
                     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
                 ),
             )
         },
-        [todos],
+        [],
     )
 
     return (
