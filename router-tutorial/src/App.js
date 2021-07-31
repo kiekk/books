a36ac64a4996e86da35a9c3f721b3422451c441f
kiekk <dbstnsgh2@naver.com>
2021-08-01 01:35:40 +0900

    Link 컴포넌트 사용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import About from './About'
 import Home from './Home'
 
-// '/about' 접속 시 '/' 규칙도 일치하기 때문에 Home, About 컴포넌트가 같이 출력
-// Home 컴포넌트에 exact=true 적용하여 '/about' 접속 시 About 컴포넌트만 출력
+/*
+    Link 컴포넌트
+    다른 주소로 이동시켜 주는 컴포넌트
+
+    *리액트 라우터를 사용할 때는 a 태그를 직접 사용하게 되면
+    페이지를 새로 불러오기 때문에 기존 상태들이 초기화 됩니다.
+
+    Link 컴포넌트를 사용하여 페이지를 전환하면,
+    페이지를 새로 불러오지 않고 애플리케이션은 그대로 유지한 상태에서
+    페이지의 주소만 변경해 줍니다.
+ */
 const App = () => {
     return (
         <div>
-            <Route path="/" component={Home} exact={true}/>
+            <ul>
+                <li>
+                    <Link to="/">홈</Link>
+                </li>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+            </ul>
+            <hr />
+            <Route path="/" component={Home} exact={true} />
             <Route path="/about" component={About} />
         </div>
     )
