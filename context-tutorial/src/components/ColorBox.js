568976025bcfad96ba2d9c55f5c8942ac23e6559
kiekk <dbstnsgh2@naver.com>
2021-08-02 08:58:27 +0900

    동적 context

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,23 +1,28 @@
 import React from 'react'
-import ColorContext from '../contexts/color'
+import { ColorConsumer } from '../contexts/color'
 
 const ColorBox = () => {
     return (
-        // props가 아닌 Consumer 컴포넌트를 통해 조회
-        // Consumer 사이에 중괄호를 열어 함수를 넣어줌
-        // Function as a child, Render Props
-        // 컴포넌트의 child 자리에 JSX, 함수 전달
-        <ColorContext.Consumer>
+        <ColorConsumer>
             {value => (
-                <div
-                    style={{
-                        width: '64px',
-                        height: '64px',
-                        background: value.color
-                    }}
-                />
+                <>
+                    <div
+                        style={{
+                            width: '64px',
+                            height: '64px',
+                            background: value.state.color
+                        }}
+                    />
+                    <div
+                        style={{
+                            width: '64px',
+                            height: '64px',
+                            background: value.state.subColor
+                        }}
+                    />
+                </>
             )}
-        </ColorContext.Consumer>
+        </ColorConsumer>
     )
 }
 
