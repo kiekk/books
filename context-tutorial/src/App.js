535b150172b0c481dfa137409df1622e402947eb
kiekk <dbstnsgh2@naver.com>
2021-08-02 08:43:58 +0900

    Provider 사용 - context value 변경

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ColorBox from './components/ColorBox'
+import ColorContext from './contexts/color'
 
 const App = () => {
-  return (
-      <div>
-        <ColorBox />
-      </div>
-  )
+    return (
+        <ColorContext.Provider value={{color: 'red'}}>
+            <div>
+                <ColorBox/>
+            </div>
+        </ColorContext.Provider>
+    )
 }
 
 export default App
