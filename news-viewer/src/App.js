4f428652f697b7150b14344108c9802ee022eded
kiekk <dbstnsgh2@naver.com>
2021-08-01 09:17:28 +0900

    newsapi 호출, 뉴스 목록 가져오기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -5,7 +5,8 @@ const App = () => {
     const [data, setData] = useState(null)
     const onClick = async () => {
         try {
-            const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            // https://newsapi.org/register 에서 API key 발급
+            const { data } = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apikey=aa592dd277fa4d5ab5b8e08ff5d4604c')
             setData(data)
         }catch(e) {
             console.log(e)
