af2f6df4e9a96898b4d2e41374612a2aaaa2b66a
kiekk <dbstnsgh2@naver.com>
2021-08-01 09:29:12 +0900

    뉴스 뷰어 레이아웃

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,8 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React from 'react'
+import NewsList from './components/NewsList'
 
 const App = () => {
-    const [data, setData] = useState(null)
-    const onClick = async () => {
-        try {
-            // https://newsapi.org/register 에서 API key 발급
-            const { data } = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apikey=aa592dd277fa4d5ab5b8e08ff5d4604c')
-            setData(data)
-        }catch(e) {
-            console.log(e)
-        }
-    }
-    
-    return (
-        <div>
-            <div>
-                <button onClick={onClick}>불러오기</button>
-            </div>
-            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-        </div>
-    )
+    return <NewsList />
 }
 
-export default App;
+export default App
