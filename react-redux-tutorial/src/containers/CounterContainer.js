ff290ecc1fb553f8778e5b56aff65b05627aec06
kiekk <dbstnsgh2@naver.com>
2021-08-02 23:35:22 +0900

    useSelector, useDispatch

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,22 +1,18 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import Counter from '../components/Counter'
 import {increase, decrease} from '../modules/counter'
 
-const CounterContainer = ({number, increase, decrease}) => {
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number)
+    const dispatch = useDispatch()
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+        <Counter
+            number={number}
+            onIncrease={() => dispatch(increase())}
+            onDecrease={() => dispatch(decrease())}
+        />
     )
 }
 
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    // bindActionCreators 생략 후 객체 형태로 넣어주면
-    // connect 함수에서 내부적으로 bindActionCreators 작업을 대신해줌
-    {
-        increase,
-        decrease
-    },
-)(CounterContainer)
+export default CounterContainer
