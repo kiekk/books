30744bfe08e22dd1df0f1adf59c31cce3b6319c6
shyoon <dbstnsgh2@naver.com>
2021-08-04 08:22:04 +0900

    redux-thunk 사용

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import rootReducer from './modules';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import rootReducer from './modules'
 // import loggerMiddleware from './lib/loggerMiddleware';
-import { createLogger } from 'redux-logger/src';
+import { createLogger } from 'redux-logger/src'
+import ReduxThunk from 'redux-thunk'
 
-const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const logger = createLogger()
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root'),
-);
+)
--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -6,14 +6,27 @@ const DECREASE = 'counter/DECREASE'
 export const increase = createAction(INCREASE)
 export const decrease = createAction(DECREASE)
 
-const initialState = 0;
+// 1초 뒤에 increase, decrease 함수 디스패치
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase())
+  }, 1000)
+}
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease())
+  }, 1000)
+}
+
+const initialState = 0
 
 const counter = handleActions(
-    {
-        [INCREASE]: state => state + 1,
-        [DECREASE]: state => state - 1
-    },
-    initialState
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  initialState,
 )
 
 export default counter
