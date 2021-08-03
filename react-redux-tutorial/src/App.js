5a7715e604a4ea4fc60d2a80050c8cfdfff212c4
kiekk <dbstnsgh2@naver.com>
2021-08-03 20:12:11 +0900

    prettier 적용

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import CounterContainer from './containers/CounterContainer'
-import TodosContainer from './containers/TodosContainer'
+import React from 'react';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
-    return (
-        <div>
-            <CounterContainer />
-            <hr />
-            <TodosContainer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
+    </div>
+  );
+};
 
-export default App
+export default App;
