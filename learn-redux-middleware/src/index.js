c166152274ddbfaf97d36d87793a697e131705a6
kiekk <dbstnsgh2@naver.com>
2021-08-03 09:04:12 +0900

    CounterContainer - 컨테이너 컴포넌트 Counter - 프레젠테이셔널 컴포넌트

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -4,7 +4,6 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules'
 
 const store = createStore(rootReducer)
@@ -15,8 +14,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
