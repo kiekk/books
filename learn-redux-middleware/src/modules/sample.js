1019b0658538465eedbf4531cdf8ddb23c711809
shyoon <dbstnsgh2@naver.com>
2021-08-04 09:03:03 +0900

    오타 수정

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -5,7 +5,7 @@ import * as api from '../lib/api'
 // 요청, 성공, 실패로 한 요청당 3개를 만들어야 함
 
 const GET_POST = 'sample/GET_POST'
-const GET_POST_SUCCESS = 'sample/GET_POST_SUCCES'
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS'
 const GET_POST_FAILURE = 'sample/GET_POST_FAILURE'
 
 const GET_USERS = 'sample/GET_USERS'
