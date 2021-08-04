2ca6586602443cacd6d748660b32ad6606a93b89
shyoon <dbstnsgh2@naver.com>
2021-08-04 09:20:26 +0900

    불필요 코드 제거

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -7,11 +7,9 @@ import createRequestThunk from '../lib/createRequestThunk'
 
 const GET_POST = 'sample/GET_POST'
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS'
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE'
 
 const GET_USERS = 'sample/GET_USERS'
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS'
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE'
 
 // thunk 함수 생성
 // thunk 함수 내부에서는 시작, 성공, 실패했을 때 다른 액션을 dispatch함
@@ -23,23 +21,12 @@ export const getUsers = createRequestThunk(GET_USERS, api.getUsers)
 // 요청 로딩 중 상태는 loading 객체에서 관리
 
 const initialState = {
-  loading: {
-    GET_POST: false,
-    GET_USERS: false,
-  },
   post: null,
   users: null,
 }
 
 const sample = handleActions(
   {
-    [GET_POST]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true, // 요청 시작
-      },
-    }),
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
       loading: {
@@ -48,20 +35,6 @@ const sample = handleActions(
       },
       post: action.payload,
     }),
-    [GET_POST_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false, // 요청 완료
-      },
-    }),
-    [GET_USERS]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: true, // 요청 시작
-      },
-    }),
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
       loading: {
@@ -69,13 +42,6 @@ const sample = handleActions(
         GET_USERS: false, // 요청 완료
       },
       users: action.payload,
-    }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false, // 요청 완료
-      },
     }),
   },
   initialState,
