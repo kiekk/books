dc656e57e981e937f49223db9f4e4d9d3dfdc2a8
shyoon <dbstnsgh2@naver.com>
2021-08-04 09:04:07 +0900

    반복되는 로직 개선 - 리팩토링 - createRequestThunk 유틸 함수 작성

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions'
 import * as api from '../lib/api'
+import createRequestThunk from '../lib/createRequestThunk'
 
 // 액션 타입 선언
 // 요청, 성공, 실패로 한 요청당 3개를 만들어야 함
@@ -15,41 +16,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE'
 // thunk 함수 생성
 // thunk 함수 내부에서는 시작, 성공, 실패했을 때 다른 액션을 dispatch함
 
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST })
-  try {
-    const response = await api.getPost(id)
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    }) // 요청 성공
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    }) // 에러 발생
-    throw e // 나중에 컴포넌트에서 에러 조회할 수 있도록 함
-  }
-}
-
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USERS })
-  try {
-    const response = await api.getUsers()
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    }) // 요청 성공
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    })
-    throw e
-  }
-}
+export const getPost = createRequestThunk(GET_POST, api.getPost)
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers)
 
 // 초기 상태
 // 요청 로딩 중 상태는 loading 객체에서 관리
