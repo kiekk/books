55a849206c7fea863b58bffe2253668a1d3c20b0
shyoon <dbstnsgh2@naver.com>
2021-08-05 22:57:19 +0900

    코드 스플리팅

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,18 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
-function App() {
-  const onClick = () => {
-    import("./notify").then((result) => result.default());
-  };
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    SplitMe: null,
+  }
+
+  handleClick = async () => {
+    const loadedModule = await import('./SplitMe')
+    this.setState({
+      SplitMe: loadedModule.default,
+    })
+  }
+
+  render() {
+    const { SplitMe } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
