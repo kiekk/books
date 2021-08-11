2190a16331688f452828149845a5ed8f036f6447
shyoon <dbstnsgh2@naver.com>
2021-08-12 08:21:44 +0900

    koa-router 적용

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,28 +1,20 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router()
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url)
-  console.log(1)
-
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401 // Unauthorized
-    return
-  }
-
-  await next()
-  console.log('END')
+// 라우터 설정
+router.get('/', (ctx) => {
+  ctx.body = '홈'
 })
 
-app.use((ctx, next) => {
-  console.log(2)
-  next()
+router.get('/about', (ctx) => {
+  ctx.body = '소개'
 })
 
-app.use((ctx) => {
-  ctx.body = 'hello world'
-})
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4000, () => {
   console.log('Listening to port 4000')
