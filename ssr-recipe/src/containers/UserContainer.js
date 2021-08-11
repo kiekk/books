9bbfdeb1e25ee881c4f8a34e4c562022ed2cb3b5
shyoon <dbstnsgh2@naver.com>
2021-08-11 23:40:26 +0900

    usePreloader 적용

--- ssr-recipe/src/containers/UserContainer.js
+++ ssr-recipe/src/containers/UserContainer.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import User from '../components/User'
-import { Preloader } from '../lib/PreloadContext'
+import { Preloader, usePreloader } from '../lib/PreloadContext'
 import { getUser } from '../modules/users'
 
 const UserContainer = ({ id }) => {
   const user = useSelector((state) => state.users.user)
   const dispatch = useDispatch()
 
+  usePreloader(() => dispatch(getUser(id)))
   useEffect(() => {
     if (user && user.id === parseInt(id, 10)) {
       return
@@ -16,7 +17,7 @@ const UserContainer = ({ id }) => {
   }, [dispatch, id, user])
 
   if (!user) {
-    return <Preloader resolve={() => dispatch(getUser(id))} />
+    return null
   }
   return <User user={user} />
 }
