825602c82eee2b20cdc25ba60e04b448e25b50e5
shyoon <dbstnsgh2@naver.com>
2021-08-11 23:29:48 +0900

    user component, container 수정

--- ssr-recipe/src/components/User.js
+++ ssr-recipe/src/components/User.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const User = ({ user }) => {
   const { email, name, username } = user
 
--- ssr-recipe/src/containers/UserContainer.js
+++ ssr-recipe/src/containers/UserContainer.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import User from '../components/User'
 import { Preloader } from '../lib/PreloadContext'
