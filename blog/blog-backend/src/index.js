f485a96cdfbfdd53dc534d62b0e1d5ac0135f1b4
shyoon <dbstnsgh2@naver.com>
2021-08-12 14:06:01 +0900

    .env 파일로 환경변수 정보 작성, dotenv의 config()를 호출한 후 process.env로 환경변수 값 조회

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT } = process.env
+
 const api = require('./api')
 
 const app = new Koa()
@@ -15,6 +20,7 @@ app.use(bodyParser())
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000')
+const port = PORT || 4000
+app.listen(port, () => {
+  console.log('Listening to port %d', port)
 })
