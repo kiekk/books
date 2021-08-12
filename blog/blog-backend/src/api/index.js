bb33ea769a955273c5c74b44ab50dd10ca65c61e
shyoon <dbstnsgh2@naver.com>
2021-08-12 17:20:57 +0900

    인증 api 구조 작성

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,8 +1,10 @@
 import Router from 'koa-router'
 import posts from './posts'
+import auth from './auth'
 
 const api = new Router()
 
 api.use('/posts', posts.routes())
+api.user('/auth', auth.routes())
 
 export default api
