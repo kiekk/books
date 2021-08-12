8e2112cad001a1ae6cc1a55b05510d208e56212f
shyoon <dbstnsgh2@naver.com>
2021-08-12 16:21:55 +0900

    post 추가 api - request body 검증

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/posts'
 import mongoose from 'mongoose'
+import Joi from '@hapi/joi'
 
 const { ObjectId } = mongoose.Types
 
@@ -22,6 +23,21 @@ export const checkObjectId = (ctx, next) => {
   }
  */
 export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), // required()가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
+  })
+
+  // 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body)
+  if (result.error) {
+    ctx.status = 400 // Bad Request
+    ctx.body = result.error
+    return
+  }
+
   const { title, body, tags } = ctx.request.body
   const post = new Post({
     title,
