ab14408682731b8a3e134ffb7928aa37799ed675
shyoon <dbstnsgh2@naver.com>
2021-08-12 15:40:30 +0900

    post 수정 api

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -66,4 +66,27 @@ export const remove = async (ctx) => {
   }
 }
 
-export const update = (ctx) => {}
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
+  }
+ */
+export const update = async (ctx) => {
+  const { id } = ctx.params
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환
+      // false는 업데이트되기 전의 데이터를 반환
+    })
+    if (!post) {
+      ctx.status = 400
+      return
+    }
+    ctx.body = post
+  } catch (e) {
+    ctx.throw(500, e)
+  }
+}
