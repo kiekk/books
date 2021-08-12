0a67978f0beffce13f1303306904fa74db289134
shyoon <dbstnsgh2@naver.com>
2021-08-12 16:15:10 +0900

    요청시 id 검증 middleware 작성

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 import Post from '../../models/posts'
+import mongoose from 'mongoose'
+
+const { ObjectId } = mongoose.Types
+
+// 요청시 id 검증 middleware
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400 // Bad Request
+    return
+  }
+  return next()
+}
 
 /*
   POST /api/posts
