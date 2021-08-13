aa78f4b4aad6f5aad041f56d1fa4f59f27dc2733
shyoon <dbstnsgh2@naver.com>
2021-08-14 08:12:24 +0900

    Button style 적용

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -49,6 +49,10 @@ const Footer = styled.div`
   }
 `
 
+const ButtonWithMarginTop = styled(Button)`
+  margin-top: 1rem;
+`
+
 const AuthForm = () => {
   return (
     <AuthFormBlock>
@@ -65,7 +69,9 @@ const AuthForm = () => {
           placeholder="비밀번호"
           type="password"
         />
-        <Button>로그인</Button>
+        <ButtonWithMarginTop cyan fullWidth>
+          로그인
+        </ButtonWithMarginTop>
       </form>
       <Footer>
         <Link to="/register">회원가입</Link>
