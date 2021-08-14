5dbb89d322851d92072e4afe8a373e6d45401d33
shyoon <dbstnsgh2@naver.com>
2021-08-14 10:10:31 +0900

    회원가입 기능

--- blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,12 +1,14 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { changeField, initializeForm } from '../../modules/auth'
+import { changeField, initializeForm, register } from '../../modules/auth'
 import AuthForm from '../../components/auth/AuthForm'
 
 const RegisterForm = () => {
   const dispatch = useDispatch()
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
   }))
   // input change event handler
   const onChange = (e) => {
@@ -22,12 +24,33 @@ const RegisterForm = () => {
   // form register event handler
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const { username, password, passwordConfirm } = form
+
+    if (password !== passwordConfirm) {
+      // TODO: 오류 처리
+      return
+    }
+    dispatch(register({ username, password }))
   }
 
   // 컴포넌트 처음 렌더링될 때 form 초기화
   useEffect(() => {
     dispatch(initializeForm('register'))
   }, [dispatch])
+
+  // 회원가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생')
+      console.log(authError)
+      return
+    }
+    if (auth) {
+      console.log('회원가입 성공')
+      console.log(auth)
+    }
+  }, [auth, authError])
 
   return (
     <AuthForm
