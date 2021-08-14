0ada419dc60c54d729af5296f1494f0d4c621af0
shyoon <dbstnsgh2@naver.com>
2021-08-14 09:25:04 +0900

    loading module 작성

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { changeField, initializeForm } from '../../module/auth'
+import { changeField, initializeForm } from '../../modules/auth'
 import AuthForm from '../../components/auth/AuthForm'
 
 const LoginForm = () => {
