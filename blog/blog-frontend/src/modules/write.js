2509152145785447f3196813477b29f20f2d122d
shyoon <dbstnsgh2@naver.com>
2021-08-14 21:47:59 +0900

    컨테이너 컴포넌트 EditorContainer 작성

--- blog/blog-frontend/src/modules/write.js
+++ blog/blog-frontend/src/modules/write.js
@@ -1,11 +1,10 @@
 import { createAction, handleActions } from 'redux-actions'
-import { changeField } from './auth'
 
 const INITIALIZE = 'write/INITIALIZE' // 모든 내용 초기화
 const CHANGE_FIELD = 'write/CHANGE_FIELD' // 특정 key 값 바꾸기
 
 export const initialize = createAction(INITIALIZE)
-export const chageField = createAction(CHANGE_FIELD, ({ key, value }) => ({
+export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
   key,
   value,
 }))
