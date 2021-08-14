ad73dbd5ba6ee524410911eb4c99d3ba92eeb0d0
shyoon <dbstnsgh2@naver.com>
2021-08-14 10:15:48 +0900

    user module 작성

--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import auth, { authSaga } from './auth'
 import loading from './loading'
 import { all } from 'redux-saga/effects'
+import user, { userSaga } from './users'
 
 const rootReducer = combineReducers({
   auth,
   loading,
+  user,
 })
 
 export function* rootSaga() {
