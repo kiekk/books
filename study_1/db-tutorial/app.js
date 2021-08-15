b85f620c933d5497c2a65aec6e261838fbc93b51
shyoon <dbstnsgh2@naver.com>
2021-08-15 09:35:08 +0900

    DB 연결

--- study_1/db-tutorial/app.js
+++ study_1/db-tutorial/app.js
@@ -4,3 +4,10 @@ const ejs = require('ejs')
 const mysql = require('mysql')
 const express = require('express')
 const bodyParser = require('body-parser')
+
+// 데이터베이스와 연결
+const client = mysql.createConnection({
+  user: 'root',
+  password: '비밀번호', // root 계정에 설정했던 비밀번호 입력
+  database: 'Company'
+})
