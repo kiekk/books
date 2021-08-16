ad56a0cab78dca1bb142204dfea328f5575aefc5
shyoon <dbstnsgh2@naver.com>
2021-08-16 09:33:50 +0900

    socket io

---
+++ study_1/socket-tutorial/app.js
@@ -0,0 +1,26 @@
+// 모듈 추출
+const socketIo = require("socket.io");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIo.listen(server);
+
+// 미들웨어 설정
+app.use(express.static(`${__dirname}/public`));
+
+// 웹 소켓을 설정
+io.sockets.on("connection", (socket) => {
+  socket.on("print", (data) => {
+    console.log("Client Send Data:", data);
+
+    socket.emit("smart", data);
+  });
+});
+
+// 서버 실행
+server.listen(52273, function () {
+  console.log("Server Running at http://127.0.0.1:52273");
+});
