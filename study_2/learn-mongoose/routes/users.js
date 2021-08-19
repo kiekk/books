48140c8f2e3ed02572eaee5f4e743669bf4b7c94
shyoon <dbstnsgh2@naver.com>
2021-08-19 17:55:58 +0900

    users routes 작성

--- study_2/learn-mongoose/routes/users.js
+++ study_2/learn-mongoose/routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
+var User = require("../schemas/user");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const user = await new User({
+      name: req.body.name,
+      age: req.body.age,
+      married: req.body.married,
+    });
+    const result = await user.save();
+    console.log(result);
+    res.status(201).json(result);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
 });
 
 module.exports = router;
