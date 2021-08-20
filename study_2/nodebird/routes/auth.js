ec0208939dea7891e1dc0ba915331f5f576d2b41
shyoon <dbstnsgh2@naver.com>
2021-08-20 23:44:13 +0900

    auth routes 작성

---
+++ study_2/nodebird/routes/auth.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const { User } = require("../models");
+
+const router = express.Router();
+
+router.post("/join", isNotLoggedIn, async (req, res, next) => {
+  const { email, nick, password } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      req.flash("joinError", "이미 가입된 메일입니다.");
+      return res.redirect("/join");
+    }
+
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email,
+      nick,
+      password: hash,
+    });
+    return res.redirect("/");
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+});
+
+router.post("/login", isNotLoggedIn, (req, res, next) => {
+  passport.authenticate("local", (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      req.flash("loginError", info.message);
+      return res.redirect("/");
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next); // 미들웨어 내의 미들웨어는 (req, res, next)를 붙임
+});
+
+router.get("/logout", isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect("/");
+});
+
+module.exports = router;
