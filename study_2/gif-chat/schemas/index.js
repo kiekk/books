93981c24c46534b25ae8949758bb5f0f9c461279
shyoon <dbstnsgh2@naver.com>
2021-08-23 07:51:22 +0900

    오타 수정, db 연결 url 수정

--- study_2/gif-chat/schemas/index.js
+++ study_2/gif-chat/schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const { MONGO_ID, MONGO_PASSWORD, NODE_ENV } = mongoose;
-const MONGO_URL = `mongodb://${MONGO_ID}:${MONGO_PASSWORD}@localhost:27017/admin`;
+const { NODE_ENV } = mongoose;
+const MONGO_URL = `mongodb://localhost:27017/admin`;
 
 module.exports = () => {
   const connect = () => {
@@ -22,18 +22,17 @@ module.exports = () => {
       }
     );
   };
-};
-
-connect();
-
-mongoose.connection.on("error", (error) => {
-  console.error("몽고디비 연결 에러", error);
-});
-
-mongoose.connection.on("disconnected", () => {
-  console.error("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
   connect();
-});
 
-require("./chat");
-require("./room");
+  mongoose.connection.on("error", (error) => {
+    console.error("몽고디비 연결 에러", error);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.error("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
+    connect();
+  });
+
+  require("./chat");
+  require("./room");
+};
