4c6b1807288df1b670a76836c8e45327be22235e
shyoon <dbstnsgh2@naver.com>
2021-08-22 15:28:53 +0900

    게시글 조회, 해시태그 검색 API 추가

--- study_2/nodebird-call/routes/index.js
+++ study_2/nodebird-call/routes/index.js
@@ -2,6 +2,55 @@ const express = require("express");
 const axios = require("axios");
 
 const router = express.Router();
+const URL = "http://localhost:8002/v1";
+
+axios.defaults.headers.origin = "http://localhost:8003";
+
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) {
+      // 세션에 토큰이 없으면
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      // 세션에 토큰 저장
+      req.session.jwt = tokenResult.data.token;
+    }
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt },
+    }); // API 요청
+  } catch (e) {
+    console.error(e);
+    if (e.response.status < 500) {
+      return e.response;
+    }
+    throw e;
+  }
+};
+
+router.get("/mypost", async (req, res, next) => {
+  try {
+    const result = await request(req, "/posts/my");
+    res.json(result.data);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+router.get("/search/:hashtag", async (req, res, next) => {
+  try {
+    console.log("params", req.params.hashtag);
+    const result = await request(
+      req,
+      `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`
+    );
+    res.json(result.data);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 
 router.get("/test", async (req, res, next) => {
   try {
