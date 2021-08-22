1f7306003db4c5631218329334bcbbfd39c59b62
shyoon <dbstnsgh2@naver.com>
2021-08-22 16:38:22 +0900

    호스트, 비밀키가 모두 일치할 때만 cors 허용

--- study_2/nodebird-api/routes/v2.js
+++ study_2/nodebird-api/routes/v2.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const url = require("url");
 
 const { verifyToken, apiLimiter } = require("./middlewares");
 const { Domain, User, Post, HashTag } = require("../models");
@@ -8,11 +9,52 @@ const { hash } = require("bcrypt");
 
 const router = express.Router();
 
-router.use(cors());
+router.use(async (req, res, next) => {
+  const domain = await Domain.findOne({
+    where: { host: url.parse(req.get("origin")).host },
+  });
+  console.log("domain", domain);
+  console.log("origin", req.get("origin"));
+  console.log("host", url.parse(req.get("origin")).host);
+  if (domain) {
+    cors({ origin: req.get("origin") })(req, res, next);
+  } else {
+    next();
+  }
+});
 
 router.post("/token", apiLimiter, async (req, res) => {
   const { clientSecret } = req.body;
   try {
+    const domain = await Domain.findOne({
+      where: { clientSecret },
+      include: {
+        model: User,
+        attribute: ["nick", "id"],
+      },
+    });
+    if (!domain) {
+      return res.status(401).json({
+        code: 401,
+        message: "등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요",
+      });
+    }
+    const token = jwt.sign(
+      {
+        id: domain.user.id,
+        nick: domain.user.nick,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "30m",
+        issuer: "nodebird",
+      }
+    );
+    return res.json({
+      code: 200,
+      message: "토큰이 발급되었습니다",
+      token,
+    });
   } catch (e) {
     console.error(e);
     return res.status(500).json({
