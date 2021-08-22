21497d2b097e37fddde7797f44da732078066724
shyoon <dbstnsgh2@naver.com>
2021-08-22 15:47:17 +0900

    v2 routes 작성

---
+++ study_2/nodebird-api/routes/v2.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const { verifyToken, apiLimiter } = require("./middlewares");
+const { Domain, User, Post, HashTag } = require("../models");
+const { hash } = require("bcrypt");
+
+const router = express.Router();
+
+router.post("/token", apiLimiter, async (req, res) => {
+  const { clientSecret } = req.body;
+  try {
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      code: 500,
+      message: "서버 에러",
+    });
+  }
+});
+
+router.get("/test", verifyToken, apiLimiter, (req, res) => {
+  res.json(req.decoded);
+});
+
+router.get("/posts/my", apiLimiter, verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } })
+    .then((posts) => {
+      console.log("posts", posts);
+      res.json({
+        code: 200,
+        payload: posts,
+      });
+    })
+    .catch((e) => {
+      console.error(e);
+      return res.status(500).json({
+        code: 500,
+        message: "서버 에러",
+      });
+    });
+});
+
+router.get(
+  "/posts/hashtag/:title",
+  verifyToken,
+  apiLimiter,
+  async (req, res) => {
+    try {
+      const hashtag = await HashTag.findOne({
+        where: { title: req.params.title },
+      });
+      if (!hashtag) {
+        return res.status(404).json({
+          code: 404,
+          message: "검색 결과가 없습니다.",
+        });
+      }
+      const posts = await hashtag.getPosts();
+      return res.json({
+        code: 200,
+        payload: posts,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+);
+
+module.exports = router;
