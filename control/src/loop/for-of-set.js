df001268b8399daf32d85f7006c15acb660ce71c
shyoon <dbstnsgh2@naver.com>
2021-08-27 22:34:11 +0900

    ES5로 컴파일 할 경우 Set은 for문으로 변환

---
+++ control/src/loop/for-of-set.js
@@ -0,0 +1,12 @@
+var itSet = new Set(['a', 'b', 'c', 'd', 'a', 'b', 'c']);
+for (var _i = 0, itSet_1 = itSet; _i < itSet_1.length; _i++) {
+    var value = itSet_1[_i];
+    console.log(value);
+}
+/*
+    실행 결과
+    a
+    b
+    c
+    d
+ */
