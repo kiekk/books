266582c4b6f5f3c1434fc42a891806260a14fa3c
shyoon <dbstnsgh2@naver.com>
2021-08-27 22:07:39 +0900

    switch

---
+++ control/dist/switch.js
@@ -0,0 +1,10 @@
+let command = 'hi';
+switch (command) {
+    case 'hi':
+        console.log('hi');
+        break;
+    case 0:
+        console.log(0);
+        break;
+}
+// any 타입일 경우 case 절 값의 타입을 제한하지 않을 수 있습니다.
