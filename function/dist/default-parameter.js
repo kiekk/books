e149f76f993a039f8528dfe8f7f2426dc6aaa818
shyoon <dbstnsgh2@naver.com>
2021-08-28 08:46:43 +0900

    default parameter

---
+++ function/dist/default-parameter.js
@@ -0,0 +1,10 @@
+function pow(x, n = 2) {
+    return Math.pow(x, n);
+}
+console.log(pow(10));
+console.log(pow(10, 3));
+/*
+    실행 결과
+    100
+    1000
+ */
---
+++ function/src/basic/default-parameter.js
@@ -0,0 +1,11 @@
+function pow(x, n) {
+    if (n === void 0) { n = 2; }
+    return Math.pow(x, n);
+}
+console.log(pow(10));
+console.log(pow(10, 3));
+/*
+    실행 결과
+    100
+    1000
+ */
