e17002a9257411492b852f818b525e71f8ba68ca
shyoon <dbstnsgh2@naver.com>
2021-08-27 21:47:53 +0900

    tuple 타입

---
+++ variable/dist/tuple.js
@@ -0,0 +1,9 @@
+let x = ['tuple', 100];
+console.log(typeof x, typeof x[0], typeof x[1]);
+console.log(x[0].substr(0, 2), x[1].toFixed(2));
+// 튜플에 선언된 타입 수와 할당될 배열의 요소 수가 정확히 일치해야 합니다.
+/*
+    실행 결과
+    object string number
+    tu 100.00
+ */ 
