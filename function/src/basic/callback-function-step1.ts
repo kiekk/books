fb7733a08bb820f664387a5e492295cf7147291d
shyoon <dbstnsgh2@naver.com>
2021-08-28 18:33:17 +0900

    callback function - 2

--- function/src/basic/callback-function-step1.ts
+++ function/src/basic/callback-function-step1.ts
@@ -8,7 +8,7 @@ console.log(responseMessage);
 
 // 2
 // 콜백 함수의 선언을 외부로 분리하여 함수를 더 간결하게 호출
-type EchoCallbackType = (message: string) => void
-let callbackEcho: EchoCallbackType = message => message
-responseMessage = echoFunction('happy world', callbackEcho);
-console.log(responseMessage);
+// type EchoCallbackType = (message: string) => void
+// let callbackEcho: EchoCallbackType = message => message
+// responseMessage = echoFunction('happy world', callbackEcho);
+// console.log(responseMessage);
