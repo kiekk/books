5f7cdd2cad094d39809553cec4f5052f430ef370
shyoon <dbstnsgh2@naver.com>
2021-09-02 21:32:43 +0900

    hoisting - 4 function

---
+++ src/hosting/function.js
@@ -0,0 +1,48 @@
+// console.log(sum(1, 2))
+// console.log(multiply(1, 2))
+//
+// function sum(x, y) {
+//   return x + y
+// }
+//
+// var multiply = function (x, y) {
+//   return x * y
+// }
+
+/*
+  에러 발생
+  console.log(multiply(1, 2))
+            ^
+  TypeError: multiply is not a function
+
+ */
+
+/*
+var sum = function (x, y) {
+  return x + y
+}
+
+var multiply
+
+console.log(sum(1, 2))
+console.log(multiply(1, 2))
+
+multiply = function (x, y) {
+  return x * y
+}
+*/
+
+/*
+   정상 작동
+console.log(sum(1, 2))
+
+function sum(x, y) {
+  return x + y
+}
+
+var multiply = function (x, y) {
+  return x * y
+}
+
+console.log(multiply(1, 2))
+*/
