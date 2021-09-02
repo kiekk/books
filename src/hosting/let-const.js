2106c0ba0663e24e6e57074c00eac7cfcc6fc25f
shyoon <dbstnsgh2@naver.com>
2021-09-02 23:19:40 +0900

    let, const의 hoisting 여부

--- src/hosting/let-const.js
+++ src/hosting/let-const.js
@@ -5,3 +5,19 @@ let a = 2
 
   let a = 2
 })()
+
+// let, const는 hoisting이 되지 않는듯 하다
+// hoisting이 되면 c가 undefined로 출력되야 하는데 에러가 발생한다.
+function b() {
+  console.log(c)
+
+  let c = 2
+}
+b()
+
+function d() {
+  console.log(e)
+
+  const e = 3
+}
+d()
