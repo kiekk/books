08624fa242b12d4a59d2c510f3d6c50d42efde33
shyoon <dbstnsgh2@naver.com>
2021-09-03 22:53:06 +0900

    함수 선언식 대신 표현식 사용하기

---
+++ src/hosting/good-code-function.js
@@ -0,0 +1,17 @@
+var sum = function (x, y) {
+  return x + y
+}
+
+console.log(sum(1, 2))
+
+var sum = function (x, y) {
+  return `${x + y}입니다.`
+}
+
+console.log(sum(3, 4))
+
+/*
+  실행 결과
+  3
+  7입니다.
+ */
