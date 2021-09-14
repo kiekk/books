c9b27c23468f73a385ce05ba95f9da61ad7dcaab
shyoon <dbstnsgh2@naver.com>
2021-09-14 22:52:42 +0900

    jpa 매핑 객체 연관 관계

--- ch05-jpa-start3/src/main/java/jpabook/start/Member.java
+++ ch05-jpa-start3/src/main/java/jpabook/start/Member.java
@@ -2,12 +2,21 @@ package jpabook.start;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "MEMBER")
 public class Member {
+    @Id
+    @Column(name = "MEMBER_ID")
     private String id;
     private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     public Member(String id, String username) {
--- ch05-jpa-start3/src/main/java/jpabook/start/Team.java
+++ ch05-jpa-start3/src/main/java/jpabook/start/Team.java
@@ -1,15 +1,23 @@
 package jpabook.start;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
+@Entity
+@Table(name = "TEAM")
+@NoArgsConstructor
 public class Team {
+    @Id
+    @Column(name = "TEAM_ID")
     private String id;
+
     private String name;
 }
