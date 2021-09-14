18360d6439545e74cc6a0a32f2d2f55a7f9f26bf
shyoon <dbstnsgh2@naver.com>
2021-09-14 22:44:05 +0900

    add class Team

---
+++ ch05-jpa-start3/src/main/java/jpabook/start/Team.java
@@ -0,0 +1,15 @@
+package jpabook.start;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Team {
+    private String id;
+    private String name;
+}
