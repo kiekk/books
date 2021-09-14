c19fb763d19974ba2c1901701342de6a555a2684
shyoon <dbstnsgh2@naver.com>
2021-09-14 22:08:39 +0900

    add class Member

---
+++ ch04-jpa-start2/src/main/java/jpabook/start/Member.java
@@ -0,0 +1,36 @@
+package jpabook.start;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+}
