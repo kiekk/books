e7616712bbde6854f1029d148ec48f2cc4c245b5
shyoon <dbstnsgh2@naver.com>
2021-09-14 20:56:58 +0900

    add jpa entity mapping to Member class

--- ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -4,11 +4,25 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @ToString
 @Getter
 @Setter
+@Entity
+@Table(name = "MEMBER")
 public class Member {
+    @Id
+    @Column(name = "ID")
     private String id;
+    
+    @Column(name = "NAME")
     private String username;
+    
+    // mapping이 없는 field
+    // field명으로 column을 매핑
     private Integer age;
 }
