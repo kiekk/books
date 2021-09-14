e747112ab156fa554c0e48629c798c833a4efb3a
shyoon <dbstnsgh2@naver.com>
2021-09-14 23:01:52 +0900

    jpa mapping example - persist Member & Team

--- ch05-jpa-start3/src/main/java/jpabook/start/JpaMain.java
+++ ch05-jpa-start3/src/main/java/jpabook/start/JpaMain.java
@@ -1,16 +1,46 @@
 package jpabook.start;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
     public static void main(String[] args) {
-        Member member1 = new Member("member1", "회원1");
-        Member member2 = new Member("member2", "회원2");
+        //엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
 
+        EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
+
+        try {
+            tx.begin(); //트랜잭션 시작
+            testSave(em);  //비즈니스 로직
+            tx.commit();//트랜잭션 커밋
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback(); //트랜잭션 롤백
+        } finally {
+            em.close(); //엔티티 매니저 종료
+        }
+
+        emf.close(); //엔티티 매니저 팩토리 종료
+    }
+
+    private static void testSave(EntityManager em) {
+
+        // 팀1 저장
         Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
 
-        member1.setTeam(team1);
-        member2.setTeam(team1);
+        // 회원1 저장
+        Member member1 = new Member("member1", "회원1");
+        member1.setTeam(team1); // 연관관계 설정
+        em.persist(member1);
 
-        Team findTeam = member1.getTeam();
-
+        // 회원2 저장
+        Member member2 = new Member("member2", "회원2");
+        member2.setTeam(team1); // 연관관계 설정
+        em.persist(member2);
     }
 }
