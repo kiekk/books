e9745f7532fa34fc9ae35a44db83d190e064bd90
shyoon <dbstnsgh2@naver.com>
2021-09-15 21:19:08 +0900

    양방향 연관관계 잘못된 케이스

--- ch05-jpa-start3/src/main/java/jpabook/start/JpaMain.java
+++ ch05-jpa-start3/src/main/java/jpabook/start/JpaMain.java
@@ -16,7 +16,7 @@ public class JpaMain {
 
         try {
             tx.begin(); //트랜잭션 시작
-            testSave(em);
+            testSaveNonOwner(em);
             queryLogicJoin(em);
             tx.commit();//트랜잭션 커밋
         } catch (Exception e) {
@@ -100,5 +100,26 @@ public class JpaMain {
         Member member2 = new Member("member2", "회원2");
         member2.setTeam(team1);
         em.persist(member2);
+    }
+
+    private static void testSaveNonOwner(EntityManager em) {
+        // 양방향 연관관계 주의점
+        // 연관관계 주인이 아닌 곳에서 값을 입력할 경우
+        // 반영되지 않음
+
+        // 회원1 저장
+        Member member1 = new Member("member1", "회원1");
+        em.persist(member1);
+
+        // 회원2 저장
+        Member member2 = new Member("member2", "회원2");
+        em.persist(member2);
+
+        Team team1 = new Team("team1", "팀1");
+        // 주인이 아닌 곳만 연관관계 설정
+        team1.getMembers().add(member1);
+        team1.getMembers().add(member2);
+
+        em.persist(team1);
     }
 }
