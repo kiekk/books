2f011b095aabfcbc4698ff4b62eb1f63eb2db5f1
shyoon <dbstnsgh2@naver.com>
2021-09-15 21:37:47 +0900

    기존 관계 제거

--- ch05-jpa-start3/src/main/java/jpabook/start/Member.java
+++ ch05-jpa-start3/src/main/java/jpabook/start/Member.java
@@ -26,6 +26,10 @@ public class Member {
 
     // 연관관계 편의 메소드
     public void setTeam(Team team) {
+        // 기존 팀이 있을 경우 기존 팀과 관계 제거
+        if(this.team != null) {
+            this.team.getMembers().remove(this);
+        }
         this.team = team;
         team.getMembers().add(this);
     }
