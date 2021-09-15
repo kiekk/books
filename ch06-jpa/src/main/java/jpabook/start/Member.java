bce3cfd837954d710bc7ee7ff92d9bc6dcc6c219
shyoon <dbstnsgh2@naver.com>
2021-09-15 22:23:30 +0900

    일대일 단방향 - 주 테이블에 외래 키 설정

--- ch06-jpa/src/main/java/jpabook/start/Member.java
+++ ch06-jpa/src/main/java/jpabook/start/Member.java
@@ -19,20 +19,11 @@ public class Member {
 
     private String username;
 
-    /*
-        일대다 양방향 관계 설정
-        다대일 쪽을 읽기 전용(insertable, updatable = false)으로 만들어줌
-        이럴바에는 다대일 양방향 매핑을 사용하자.
-     */
-    @ManyToOne
-    @JoinColumn(name = "TEAM_Id", insertable = false, updatable = false)
-    private Team team;
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public Member(String username) {
         this.username = username;
     }
-
-    /*
-        Member 클래스에서는 team을 참조할 수 없다. (단방향)
-     */
 }
