f7cfa3a1a148393be6c9200614cb07a5de9e732b
shyoon <dbstnsgh2@naver.com>
2021-09-21 08:59:37 +0900

    add MemberServiceTest

---
+++ ch11-jpa-shop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.transaction.Transactional;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:appConfig.xml")
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    // 회원 가입 테스트
+    @Test
+    public void join() throws Exception {
+        // Given
+        Member member = new Member();
+        member.setName("kim");
+
+        // When
+        Long saveId = memberService.join(member);
+
+        // Then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+}
