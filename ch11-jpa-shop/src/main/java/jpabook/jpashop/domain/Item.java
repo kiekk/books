0910f454a21989668d46eec0ea8725c0bdaa0048
shyoon <dbstnsgh2@naver.com>
2021-09-21 09:06:30 +0900

    add methods addStock, removeStock to Item Class

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Item.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -22,4 +23,17 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
