8ce84c98e9ca34f55603f792d1aee2b274a4a7c3
shyoon <dbstnsgh2@naver.com>
2021-09-21 09:23:26 +0900

    add Business, Create Logic to Order, OrderItem Class

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Order.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.enums.DeliveryStatus;
 import jpabook.jpashop.enums.OrderStatus;
 import lombok.Data;
 
@@ -33,6 +34,42 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태
 
+    // 생성 메소드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for(OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(new Date());
+        return order;
+    }
+    
+    // 비즈니스 로직
+    // 주문 취소 로직
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new RuntimeException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+    // 조회 로직
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+    
     // 연관 관계 메소드
     public void setMember(Member member) {
         this.member = member;
