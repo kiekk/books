dab8c101ade5cca4db3c037af1352c2044385303
shyoon <dbstnsgh2@naver.com>
2021-09-21 09:10:12 +0900

    modify import @Transactional

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -5,8 +5,8 @@ import jpabook.jpashop.repository.MemberRepository;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
