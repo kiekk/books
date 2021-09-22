bc3b31c7e037ed99f91b0217d584d02bd97c0f3a
shyoon <dbstnsgh2@naver.com>
2021-09-22 20:03:59 +0900

    add RequestMapping - "/items"

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/web/ItemController.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/web/ItemController.java
@@ -1,12 +1,25 @@
 package jpabook.jpashop.web;
 
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class ItemController {
 
     @Autowired
     ItemService itemService;
+
+    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
 }
