f0a7e171a96e6cb9706d9992b33c569b35555186
shyoon <dbstnsgh2@naver.com>
2021-09-22 20:36:00 +0900

    add RequestMapping - "/members"

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/web/MemberController.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/web/MemberController.java
@@ -6,8 +6,11 @@ import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/members")
@@ -30,5 +33,12 @@ public class MemberController {
         member.setAddress(address);
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
