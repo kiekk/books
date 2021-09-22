d6dda18d8cb37f4fab37482cb7c7411664d12877
shyoon <dbstnsgh2@naver.com>
2021-09-22 20:20:13 +0900

    modify ItemController

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/web/ItemController.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/web/ItemController.java
@@ -14,37 +14,38 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "items")
 public class ItemController {
 
     @Autowired
     ItemService itemService;
 
-    @RequestMapping(value = "/items/new", method = RequestMethod.GET)
+    @RequestMapping(value = "new", method = RequestMethod.GET)
     public String createForm() {
         return "items/createItemForm";
     }
 
-    @RequestMapping(value = "/items/new", method = RequestMethod.POST)
+    @RequestMapping(value = "new", method = RequestMethod.POST)
     public String create(Book item) {
         itemService.saveItem(item);
         return "redirect:/items";
     }
 
-    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String list(Model model) {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
     }
 
-    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.GET)
+    @RequestMapping(value = "{itemId}/edit", method = RequestMethod.GET)
     public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
         Item item = itemService.findOne(itemId);
         model.addAttribute("item", item);
         return "items/updateItemForm";
     }
 
-    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.POST)
+    @RequestMapping(value = "{itemId}/edit", method = RequestMethod.POST)
     public String updateItem(@ModelAttribute("item") Book item) {
         itemService.saveItem(item);
         return "redirect:/items";
