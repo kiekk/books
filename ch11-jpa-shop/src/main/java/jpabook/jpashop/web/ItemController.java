e159bd1f0fde4b97c08cbe58d118f377cff667d3
shyoon <dbstnsgh2@naver.com>
2021-09-22 20:14:35 +0900

    add RequestMapping - "/items/{itemId}/edit"

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/web/ItemController.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/web/ItemController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,5 +35,18 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.GET)
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemService.findOne(itemId);
+        model.addAttribute("item", item);
+        return "items/updateItemForm";
+    }
+
+    @RequestMapping(value = "/items/{itemId}/edit", method = RequestMethod.POST)
+    public String updateItem(@ModelAttribute("item") Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
     }
 }
