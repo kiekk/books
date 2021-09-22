168ad8aa690bcdf412401107f8b55e8380f959e1
shyoon <dbstnsgh2@naver.com>
2021-09-22 19:23:40 +0900

    add Constructor to Address Class

--- ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Address.java
+++ ch11-jpa-shop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,10 +1,14 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 public class Address {
     private String city;
