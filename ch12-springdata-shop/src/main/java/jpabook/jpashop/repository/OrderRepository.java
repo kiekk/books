f71110aeb70f622649e99be8eb593eb4aee54201
shyoon <dbstnsgh2@naver.com>
2021-09-23 09:30:29 +0900

    add implements CustomRepository to OrderRepository

--- ch12-springdata-shop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ ch12-springdata-shop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,8 +1,9 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.custom.CustomOrderRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order>, CustomOrderRepository {
 }
