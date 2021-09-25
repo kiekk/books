ce94159af25160f74150b4c63a927fe61087f531
shyoon <dbstnsgh2@naver.com>
2021-09-25 10:00:50 +0900

    ch02 디렉토리 & recipe-2-1-1 프로젝트 생성

---
+++ ch02/recipe-2-1-1/src/main/java/com/example/recipe211/Recipe211Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe211Application.class, args);
+    }
+
+}
