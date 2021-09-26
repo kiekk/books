6245debb73e58dad1db479f34e6846755136f6b2
shyoon <dbstnsgh2@naver.com>
2021-09-26 21:27:01 +0900

    recipe2-2 프로젝트 생성

---
+++ ch02/recipe-2-2/src/main/java/com/example/recipe22/Recipe22Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe22Application.class, args);
+    }
+
+}
