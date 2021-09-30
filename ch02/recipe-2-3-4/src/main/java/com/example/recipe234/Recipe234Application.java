14c0449ab81259339e0a8cb183e4f515e815ec10
shyoon <dbstnsgh2@naver.com>
2021-09-30 22:56:38 +0900

    recipe-2-3-4 프로젝트 생성

---
+++ ch02/recipe-2-3-4/src/main/java/com/example/recipe234/Recipe234Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe234;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe234Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe234Application.class, args);
+    }
+
+}
