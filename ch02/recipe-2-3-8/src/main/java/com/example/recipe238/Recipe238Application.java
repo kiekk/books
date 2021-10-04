cb79858befff89c52173a4352ab97ebeca0ada6e
shyoon <dbstnsgh2@naver.com>
2021-10-04 21:47:23 +0900

    recipe-2-3-8 프로젝트 생성

---
+++ ch02/recipe-2-3-8/src/main/java/com/example/recipe238/Recipe238Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe238;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe238Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe238Application.class, args);
+    }
+
+}
---
+++ ch02/recipe-2-3-8/src/test/java/com/example/recipe238/Recipe238ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe238;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe238ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
