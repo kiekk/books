9795b3fc44fc6d3832a2de614ec864de46942015
shyoon <dbstnsgh2@naver.com>
2021-10-09 23:20:36 +0900

    recipe-2-4-4 프로젝트 생성

---
+++ ch02/recipe-2-4-4/src/main/java/com/example/recipe244/Recipe244Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe244;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe244Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe244Application.class, args);
+    }
+
+}
---
+++ ch02/recipe-2-4-4/src/test/java/com/example/recipe244/Recipe244ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe244;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe244ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
