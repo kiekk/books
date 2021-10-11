60b9b5c63d3bda556c9bc981f3fe2def33af31c2
shyoon <dbstnsgh2@naver.com>
2021-10-11 22:22:35 +0900

    recipe-2-4-6 프로젝트 생성

---
+++ ch02/recipe-2-4-6/src/test/java/com/example/recipe246/Recipe246ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe246;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe246ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
