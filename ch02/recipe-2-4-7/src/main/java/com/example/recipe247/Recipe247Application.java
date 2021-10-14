32d8dd76051fea9e8d07b8fab83b83db38ff9d22
shyoon <dbstnsgh2@naver.com>
2021-10-14 21:09:57 +0900

    recipe-2-4-7 프로젝트 생성

---
+++ ch02/recipe-2-4-7/src/main/java/com/example/recipe247/Recipe247Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe247;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe247Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe247Application.class, args);
+    }
+
+}
---
+++ ch02/recipe-2-4-7/src/test/java/com/example/recipe247/Recipe247ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe247;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe247ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
