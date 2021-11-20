d76f97fb3de9c2c9cb6fd63e4ceabb85f7cea452
shyoon <dbstnsgh2@naver.com>
2021-11-20 17:26:18 +0900

    @Autowired로 의존성 주입 - Constructor

--- ch02/recipe-2-3/src/main/java/com/example/recipe23/sequence/service/SequenceService.java
+++ ch02/recipe-2-3/src/main/java/com/example/recipe23/sequence/service/SequenceService.java
@@ -2,13 +2,15 @@ package com.example.recipe23.sequence.service;
 
 import com.example.recipe23.sequence.dao.SequenceDao;
 import com.example.recipe23.sequence.entity.Sequence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SequenceService {
 
-    private final SequenceDao sequenceDao;
+    private SequenceDao sequenceDao;
 
+    @Autowired
     public SequenceService(SequenceDao sequenceDao) {
         this.sequenceDao = sequenceDao;
     }
