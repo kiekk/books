fb6dd9177977514ad195547376020f4f59977f0d
shyoon <dbstnsgh2@naver.com>
2021-11-22 22:45:40 +0900

    @PropertySource로 외부 리소스 파일 데이터 가져오기

---
+++ ch02/recipe-2-6-1/src/test/java/com/example/recipe261/Recipe261ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe261;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe261ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
