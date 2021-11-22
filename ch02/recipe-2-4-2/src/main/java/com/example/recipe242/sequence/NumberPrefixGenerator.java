192b3f1134b46404edabdc3bec0f7753c7a32f22
shyoon <dbstnsgh2@naver.com>
2021-11-22 22:16:46 +0900

    @Inject, Custom Annotation 사용

---
+++ ch02/recipe-2-4-2/src/main/java/com/example/recipe242/sequence/NumberPrefixGenerator.java
@@ -0,0 +1,14 @@
+package com.example.recipe242.sequence;
+
+import java.util.Random;
+
+@NumberPrefixAnnotation
+public class NumberPrefixGenerator implements PrefixGenerator {
+
+    @Override
+    public String getPrefix() {
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(100);
+        return String.format("%03d", randomInt);
+    }
+}
