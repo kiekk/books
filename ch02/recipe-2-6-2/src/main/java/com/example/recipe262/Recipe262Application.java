ac48ea6dedcd82513d737d984aa5ef17835c6c26
shyoon <dbstnsgh2@naver.com>
2021-11-22 22:55:25 +0900

    @PropertySource로 외부 리소스 파일 데이터 사용 - Custom Banner

---
+++ ch02/recipe-2-6-2/src/main/java/com/example/recipe262/Recipe262Application.java
@@ -0,0 +1,43 @@
+package com.example.recipe262;
+
+import com.example.recipe262.shop.Product;
+import com.example.recipe262.shop.ShoppingCart;
+import com.example.recipe262.shop.config.ShopConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@SpringBootApplication
+public class Recipe262Application {
+
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ShopConfiguration.class);
+
+        Product aaa = context.getBean("aaa", Product.class);
+        Product cdrw = context.getBean("cdrw", Product.class);
+        Product dvdrw = context.getBean("dvdrw", Product.class);
+
+        ShoppingCart cart1 = context.getBean("shoppingCart", ShoppingCart.class);
+        cart1.addItem(aaa);
+        cart1.addItem(cdrw);
+        System.out.println("Shopping cart 1 contains " + cart1.getItems());
+
+        ShoppingCart cart2 = context.getBean("shoppingCart", ShoppingCart.class);
+        cart2.addItem(dvdrw);
+        System.out.println("Shopping cart 2 contains " + cart2.getItems());
+
+        Resource resource = new ClassPathResource("discounts.properties");
+        Properties props = PropertiesLoaderUtils.loadProperties(resource);
+        System.out.println("And don't forget our discounts!");
+        System.out.println(props);
+    }
+
+}
