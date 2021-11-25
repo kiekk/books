1e498332e64065e2a1fec17cb203118cadd773b9
shyoon <dbstnsgh2@naver.com>
2021-11-25 21:44:28 +0900

    @PreDestroy, @PostConstruct 사용(단 Component로 등록 되어 있어야 가능)

---
+++ ch02/recipe-2-8-2/src/test/java/com/example/recipe282/Recipe282ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe282;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe282ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
