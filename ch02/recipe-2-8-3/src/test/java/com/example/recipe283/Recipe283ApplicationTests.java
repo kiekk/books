a90fb39367ae7bacc480b16c31886ae12513397f
shyoon <dbstnsgh2@naver.com>
2021-11-25 22:05:32 +0900

    @Lazy로 Bean 생성 시점 늦추기

---
+++ ch02/recipe-2-8-3/src/test/java/com/example/recipe283/Recipe283ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe283;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe283ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
