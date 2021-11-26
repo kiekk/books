9efaff2bcf260344295fe825442e406c36031185
shyoon <dbstnsgh2@naver.com>
2021-11-26 20:54:22 +0900

    특정 bean만 후처리기 실행

---
+++ ch02/recipe-2-9-2/src/test/java/com/example/recipe292/Recipe292ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe292;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe292ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
