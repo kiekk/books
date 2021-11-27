58f8b0efaed2f545edb27238a74dc2703b4fb170
shyoon <dbstnsgh2@naver.com>
2021-11-27 17:51:14 +0900

    instance factory method 사용

---
+++ ch02/recipe-2-10-2/src/test/java/com/example/recipe2102/Recipe2102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
