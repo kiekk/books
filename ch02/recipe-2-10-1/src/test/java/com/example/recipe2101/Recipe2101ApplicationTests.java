a94c5030331c4d1a69894d7185f7b023765bc48a
shyoon <dbstnsgh2@naver.com>
2021-11-27 17:33:56 +0900

    static factory method 사용

---
+++ ch02/recipe-2-10-1/src/test/java/com/example/recipe2101/Recipe2101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
