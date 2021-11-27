e387e9250c6397ff897e3fbb9a23afb29a7cf875
shyoon <dbstnsgh2@naver.com>
2021-11-27 18:09:01 +0900

    spring factory bean으로 bean 생성

---
+++ ch02/recipe-2-10-3/src/main/java/com/example/recipe2103/shop/config/ShopConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.recipe2103.shop.config;
+
+import com.example.recipe2103.shop.Battery;
+import com.example.recipe2103.shop.Disc;
+import com.example.recipe2103.shop.DiscountFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.recipe2103.shop")
+public class ShopConfiguration {
+
+    @Bean
+    public Battery aaa() {
+        Battery aaa = new Battery("AAA", 2.5);
+        return aaa;
+    }
+
+    @Bean
+    public Disc cdrw() {
+        Disc aaa = new Disc("CD-RW", 1.5);
+        return aaa;
+    }
+
+    @Bean
+    public Disc dvdrw() {
+        Disc aaa = new Disc("DVD-RW", 3.0);
+        return aaa;
+    }
+
+    @Bean
+    public DiscountFactoryBean discountFactoryBeanAAA() {
+        DiscountFactoryBean factory = new DiscountFactoryBean();
+        factory.setProduct(aaa());
+        factory.setDiscount(0.2);
+        return factory;
+    }
+
+    @Bean
+    public DiscountFactoryBean discountFactoryBeanCDRW() {
+        DiscountFactoryBean factory = new DiscountFactoryBean();
+        factory.setProduct(cdrw());
+        factory.setDiscount(0.1);
+        return factory;
+    }
+
+    @Bean
+    public DiscountFactoryBean discountFactoryBeanDVDRW() {
+        DiscountFactoryBean factory = new DiscountFactoryBean();
+        factory.setProduct(dvdrw());
+        factory.setDiscount(0.1);
+        return factory;
+    }
+}
