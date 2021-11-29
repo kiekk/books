7d277655d639378762533be42bc5906ea30bed88
shyoon <dbstnsgh2@naver.com>
2021-11-29 22:34:53 +0900

    recipe-2-12 프로젝트 생성

---
+++ ch02/recipe-2-12/src/main/java/com/example/recipe212/Recipe212Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe212Application.class, args);
+    }
+
+}
