bc2d77cc80907717a3fa74b2d7fe2ffddc10715f
shyoon <dbstnsgh2@naver.com>
2021-11-30 23:20:52 +0900

    AOP 사용 - @EnableAspectJAutoProxy aspect 스캐닝
    @Aspect 적용한 클래스에 공통 기능, PointCut을 설정

---
+++ ch02/recipe-2-13-1/src/main/java/com/example/recipe2131/Recipe2131Application.java
@@ -0,0 +1,30 @@
+package com.example.recipe2131;
+
+import com.example.recipe2131.calculator.ArithmeticCalculator;
+import com.example.recipe2131.calculator.CalculatorConfiguration;
+import com.example.recipe2131.calculator.UnitCalculator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Recipe2131Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(CalculatorConfiguration.class);
+
+        ArithmeticCalculator arithmeticCalculator =
+                context.getBean("arithmeticCalculator", ArithmeticCalculator.class);
+        arithmeticCalculator.add(1, 2);
+        arithmeticCalculator.sub(4, 3);
+        arithmeticCalculator.mul(2, 3);
+        arithmeticCalculator.div(4, 2);
+
+        UnitCalculator unitCalculator = context.getBean("unitCalculator", UnitCalculator.class);
+        unitCalculator.kilogramToPound(10);
+        unitCalculator.kilometerToMile(5);
+    }
+
+}
---
+++ ch02/recipe-2-13-1/src/test/java/com/example/recipe2131/Recipe2131ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2131;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2131ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
