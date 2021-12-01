8dc9974a60d64ed8a79298233a20dfbbeb4b5b02
shyoon <dbstnsgh2@naver.com>
2021-12-01 22:50:41 +0900

    타입 시그니처 패턴

---
+++ ch02/recipe-2-17-2/src/test/java/com/example/recipe2172/Recipe2172ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2172;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2172ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
