9516b697dfec5f21e0e4839a646483f2d6e164f7
shyoon <dbstnsgh2@naver.com>
2021-12-01 22:30:59 +0900

    @Order로 aspect 우선 순위 설정

---
+++ ch02/recipe-2-15-1/src/test/java/com/example/recipe2151/Recipe2151ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2151;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2151ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
