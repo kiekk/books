2cc1170bfa17ba0e2d0d14caa58d2306f614cbaf
shyoon <dbstnsgh2@naver.com>
2021-12-01 22:37:27 +0900

    @PointCut 사용 - point cut 재사용

---
+++ ch02/recipe-2-16-1/src/main/java/com/example/recipe2161/Recipe2161Application.java
@@ -0,0 +1,30 @@
+package com.example.recipe2161;
+
+import com.example.recipe2161.calculator.ArithmeticCalculator;
+import com.example.recipe2161.calculator.UnitCalculator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+@SpringBootApplication
+public class Recipe2161Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.example.recipe2161.calculator");
+
+        ArithmeticCalculator arithmeticCalculator =
+                (ArithmeticCalculator) context.getBean("arithmeticCalculator");
+        arithmeticCalculator.add(1, 2);
+        arithmeticCalculator.sub(4, 3);
+        arithmeticCalculator.mul(2, 3);
+        arithmeticCalculator.div(4, 2);
+
+        UnitCalculator unitCalculator =
+                (UnitCalculator) context.getBean("unitCalculator");
+        unitCalculator.kilogramToPound(10);
+        unitCalculator.kilometerToMile(5);
+    }
+
+}
---
+++ ch02/recipe-2-16-1/src/test/java/com/example/recipe2161/Recipe2161ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2161;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2161ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
