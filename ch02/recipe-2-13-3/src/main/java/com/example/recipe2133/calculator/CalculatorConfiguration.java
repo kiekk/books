0e23ce58ad41213180446ca0f8399ba2a1c7539f
shyoon <dbstnsgh2@naver.com>
2021-12-01 22:09:30 +0900

    @AfterReturning 어드바이스 사용

---
+++ ch02/recipe-2-13-3/src/main/java/com/example/recipe2133/calculator/CalculatorConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.recipe2133.calculator;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan
+public class CalculatorConfiguration {
+}
