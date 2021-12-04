1eefc6c40661374c8cfa2906c0b5989ca5a2c952
shyoon <dbstnsgh2@naver.com>
2021-12-04 20:38:40 +0900

    recipe-2-20-1 프로젝트 생성

---
+++ ch02/recipe-2-20-1/src/main/java/com/example/recipe2201/Recipe2201Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe2201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe2201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe2201Application.class, args);
+    }
+
+}
---
+++ ch02/recipe-2-20-1/src/test/java/com/example/recipe2201/Recipe2201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
