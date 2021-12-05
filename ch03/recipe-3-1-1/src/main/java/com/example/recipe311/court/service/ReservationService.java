02418e03f0090c919b42ca1e1df14b145a90148e
shyoon <dbstnsgh2@naver.com>
2021-12-05 18:51:33 +0900

    spring mvc application setting
    issue: add dependency tomcat-jasper, jstl

---
+++ ch03/recipe-3-1-1/src/main/java/com/example/recipe311/court/service/ReservationService.java
@@ -0,0 +1,11 @@
+package com.example.recipe311.court.service;
+
+import com.example.recipe311.court.domain.Reservation;
+
+import java.util.List;
+
+public interface ReservationService {
+
+    List<Reservation> query(String courtName);
+
+}
