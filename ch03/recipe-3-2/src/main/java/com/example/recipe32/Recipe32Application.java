c8513a142e8e43c411308b2d4fb63830687095ef
shyoon <dbstnsgh2@naver.com>
2021-12-05 19:22:32 +0900

    @RequestMapping 사용

---
+++ ch03/recipe-3-2/src/main/java/com/example/recipe32/Recipe32Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe32Application.class, args);
+    }
+
+}
---
+++ ch03/recipe-3-2/src/test/java/com/example/recipe32/Recipe32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
