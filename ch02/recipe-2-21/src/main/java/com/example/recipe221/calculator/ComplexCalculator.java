dbf4d56ee11eebb7adcf6978ae08e1493715a098
shyoon <dbstnsgh2@naver.com>
2021-12-05 15:50:16 +0900

    aspectj 구성

---
+++ ch02/recipe-2-21/src/main/java/com/example/recipe221/calculator/ComplexCalculator.java
@@ -0,0 +1,8 @@
+package com.example.recipe221.calculator;
+
+public interface ComplexCalculator {
+
+    Complex add(Complex a, Complex b);
+
+    Complex sub(Complex a, Complex b);
+}
---
+++ ch02/recipe-2-21/src/test/java/com/example/recipe221/Recipe221ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe221;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe221ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
