d1f798629643461229d6a6747b7c58f0e4459eed
shyoon <dbstnsgh2@naver.com>
2021-12-05 16:14:08 +0900

    TaskExecutor

---
+++ ch02/recipe-2-23-1/src/test/java/com/example/recipe2231/Recipe2231ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2231;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2231ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
