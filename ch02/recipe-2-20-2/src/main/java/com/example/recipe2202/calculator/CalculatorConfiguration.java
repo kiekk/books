42a6ffe4c8a75792fb737acb85f0c089f916238d
shyoon <dbstnsgh2@naver.com>
2021-12-05 15:32:17 +0900

    spring load time weaver로 로드 타임 위빙

---
+++ ch02/recipe-2-20-2/src/main/java/com/example/recipe2202/calculator/CalculatorConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.recipe2202.calculator;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class CalculatorConfiguration {
+}
