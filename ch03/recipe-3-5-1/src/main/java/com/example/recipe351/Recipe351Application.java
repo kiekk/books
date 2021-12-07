17e3108b194808cfb4c9d50cb63fab7037aae58e
shyoon <dbstnsgh2@naver.com>
2021-12-07 23:04:44 +0900

    MessageSource로 다국어 처리

---
+++ ch03/recipe-3-5-1/src/main/java/com/example/recipe351/Recipe351Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe351;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe351Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe351Application.class, args);
+    }
+
+}
---
+++ ch03/recipe-3-5-1/src/main/java/com/example/recipe351/court/web/CourtWebApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.example.recipe351.court.web;
+
+import com.example.recipe351.court.config.CourtConfiguration;
+import com.example.recipe351.court.config.InterceptorConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class CourtWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{CourtConfiguration.class, InterceptorConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
