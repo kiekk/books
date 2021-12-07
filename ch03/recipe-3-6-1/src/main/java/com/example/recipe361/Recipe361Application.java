d7794dedcc5c583b878e2028591f437d439357fc
shyoon <dbstnsgh2@naver.com>
2021-12-07 23:36:25 +0900

    XmlViewResolver 사용 - xml 파일로 뷰 해석

---
+++ ch03/recipe-3-6-1/src/main/java/com/example/recipe361/Recipe361Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe361;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe361Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe361Application.class, args);
+    }
+
+}
---
+++ ch03/recipe-3-6-1/src/test/java/com/example/recipe361/Recipe361ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe361;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe361ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
