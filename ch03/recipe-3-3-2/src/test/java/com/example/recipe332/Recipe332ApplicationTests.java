5b3a7ce57fb1e5b7ea1077ed7face13a3dfd9a83
shyoon <dbstnsgh2@naver.com>
2021-12-07 22:22:26 +0900

    Interceptor 사용 - WebMvcConfigurer : addInterceptors 사용 - addPathPatterns로 url 매핑

---
+++ ch03/recipe-3-3-2/src/test/java/com/example/recipe332/Recipe332ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe332;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe332ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
