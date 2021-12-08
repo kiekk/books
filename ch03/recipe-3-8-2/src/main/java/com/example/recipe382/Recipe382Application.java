7dfd231706e2207f1aa2e7456e390253eaf1e6f9
shyoon <dbstnsgh2@naver.com>
2021-12-08 22:43:14 +0900

    @ExceptionHandler로 예외 매핑

---
+++ ch03/recipe-3-8-2/src/main/java/com/example/recipe382/Recipe382Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe382;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe382Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe382Application.class, args);
+    }
+
+}
---
+++ ch03/recipe-3-8-2/src/main/java/com/example/recipe382/court/web/ExceptionHandlingAdvice.java
@@ -0,0 +1,19 @@
+package com.example.recipe382.court.web;
+
+import com.example.recipe382.court.service.ReservationNotAvailableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlingAdvice {
+
+    @ExceptionHandler(ReservationNotAvailableException.class)
+    public String handle(ReservationNotAvailableException ex) {
+        return "reservationNotAvailable";
+    }
+
+    @ExceptionHandler
+    public String handleDefault(Exception e) {
+        return "error";
+    }
+}
