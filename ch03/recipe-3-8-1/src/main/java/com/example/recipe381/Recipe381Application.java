16a62f8837197a1ca98b6b14bb4d7bddce1ce5b6
shyoon <dbstnsgh2@naver.com>
2021-12-08 22:34:49 +0900

    SimpleMappingExceptionResolver 사용

---
+++ ch03/recipe-3-8-1/src/main/java/com/example/recipe381/Recipe381Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe381;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe381Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe381Application.class, args);
+    }
+
+}
---
+++ ch03/recipe-3-8-1/src/test/java/com/example/recipe381/Recipe381ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe381;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe381ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
