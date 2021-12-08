0a173b956cf47c3322d428c4b3031ca6b24c0ff8
shyoon <dbstnsgh2@naver.com>
2021-12-08 23:18:29 +0900

    Converter, Fomatter 사용 - 커스텀 타입 프로퍼티 바인딩

---
+++ ch03/recipe-3-9-2/src/main/java/com/example/recipe392/court/web/ExceptionHandlingAdvice.java
@@ -0,0 +1,19 @@
+package com.example.recipe392.court.web;
+
+import com.example.recipe392.court.service.ReservationNotAvailableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlingAdvice {
+
+    @ExceptionHandler(ReservationNotAvailableException.class)
+    public String handle(ReservationNotAvailableException ex) {
+        return "reservationNotAvailable";
+    }
+
+    @ExceptionHandler
+    public String handleDefault(Exception e) {
+        return "error";
+    }
+}
