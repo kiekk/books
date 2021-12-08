6eac83bc6cf3770ff1e6725b1b1815b7a7724080
shyoon <dbstnsgh2@naver.com>
2021-12-08 22:52:57 +0900

    스프링 폼 태그 사용

---
+++ ch03/recipe-3-9-1/src/test/java/com/example/recipe391/Recipe391ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe391;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe391ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
