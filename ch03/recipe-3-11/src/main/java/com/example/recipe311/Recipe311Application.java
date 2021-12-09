8e4002106c0dcedde4fee38ac3c58ca58dfad41c
shyoon <dbstnsgh2@naver.com>
2021-12-09 21:20:57 +0900

    bean validation 어노테이션으로 bean 검증

---
+++ ch03/recipe-3-11/src/main/java/com/example/recipe311/Recipe311Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe311Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe311Application.class, args);
+    }
+
+}
