81153d7c8a6d990b41fbffa8d953d3eca4a725a7
shyoon <dbstnsgh2@naver.com>
2021-12-10 20:54:15 +0900

    recipe-4-1-1 프로젝트 생성

---
+++ ch03/recipe-4-1-1/src/test/java/com/example/recipe411/Recipe411ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe411;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe411ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
