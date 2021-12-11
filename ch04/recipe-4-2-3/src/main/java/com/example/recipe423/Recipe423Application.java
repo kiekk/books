38842c3444836d965101cfa514546e052797a4e0
shyoon <dbstnsgh2@naver.com>
2021-12-11 10:43:30 +0900

    ResponseBody, ResponseEntity로 json 데이터, Http 응답 코드 반환하기

---
+++ ch04/recipe-4-2-3/src/main/java/com/example/recipe423/Recipe423Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe423;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe423Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe423Application.class, args);
+    }
+
+}
