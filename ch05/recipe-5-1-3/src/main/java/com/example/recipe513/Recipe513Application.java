1c8c23a871767ad76031d34585ff2d4c99623d84
shyoon <dbstnsgh2@naver.com>
2021-12-12 23:17:31 +0900

    CompleteFuture 사용, TaskExecutor로 request 비동기 처리

---
+++ ch05/recipe-5-1-3/src/main/java/com/example/recipe513/Recipe513Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe513;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe513Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe513Application.class, args);
+    }
+
+}
