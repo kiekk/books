bdf7a2e6e596eb2c2b3eb69c55efae4ecddf9743
shyoon <dbstnsgh2@naver.com>
2021-12-12 23:01:39 +0900

    TaskExecutor로 request 비동기 처리

---
+++ ch05/recipe-5-1-1/src/main/java/com/example/recipe511/Recipe511Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe511;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe511Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe511Application.class, args);
+    }
+
+}
