f9b525adb3d9b4c08e29a92c9de41c781a00c7f9
shyoon <dbstnsgh2@naver.com>
2021-12-13 22:27:44 +0900

    ResponseEntity로 status, header 전달

---
+++ ch05/recipe-5-2-2/src/main/java/com/example/recipe522/court/service/ReservationService.java
@@ -0,0 +1,25 @@
+package com.example.recipe522.court.service;
+
+import com.example.recipe522.court.domain.PeriodicReservation;
+import com.example.recipe522.court.domain.Reservation;
+import com.example.recipe522.court.domain.SportType;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReservationService {
+
+    List<Reservation> query(String courtName);
+
+    void make(Reservation reservation)
+            throws ReservationNotAvailableException;
+
+    List<SportType> getAllSportTypes();
+
+    SportType getSportType(int sportTypeId);
+
+    List<Reservation> findByDate(LocalDate summaryDate);
+
+    void makePeriodic(PeriodicReservation periodicReservation)
+            throws ReservationNotAvailableException;
+}
