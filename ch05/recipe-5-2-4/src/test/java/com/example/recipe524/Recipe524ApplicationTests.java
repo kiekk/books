ec3cc9a21241dcac80549244fa1d1229f86ba461
shyoon <dbstnsgh2@naver.com>
2021-12-13 22:46:47 +0900

    SseEventBuilder를 사용하여 id 추가하기

---
+++ ch05/recipe-5-2-4/src/test/java/com/example/recipe524/Recipe524ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe524;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe524ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
