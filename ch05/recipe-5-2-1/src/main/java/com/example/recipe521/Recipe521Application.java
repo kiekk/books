f42d42b2000bccaf5e2a134b5617b35d6c3e8dee
shyoon <dbstnsgh2@naver.com>
2021-12-13 22:14:44 +0900

    ResponseBodyEmitter로 응답을 청크로 나눠 보내기

---
+++ ch05/recipe-5-2-1/src/main/java/com/example/recipe521/Recipe521Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe521Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe521Application.class, args);
+    }
+
+}
