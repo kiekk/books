768f278ee2ec83f5556144f93214d22b698bccf3
shyoon <dbstnsgh2@naver.com>
2021-12-13 22:38:05 +0900

    SseEmitter를 사용하여 결과를 이벤트 형태로 보내기

---
+++ ch05/recipe-5-2-3/src/main/java/com/example/recipe523/Recipe523Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe523;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe523Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe523Application.class, args);
+    }
+
+}
