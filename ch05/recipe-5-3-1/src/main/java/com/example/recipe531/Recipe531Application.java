da810044067a7858bcb19f4b897859b49d39e9a3
shyoon <dbstnsgh2@naver.com>
2021-12-14 23:33:55 +0900

    AsyncHandlerInterceptor를 사용하여 비동기 인터셉터

---
+++ ch05/recipe-5-3-1/src/main/java/com/example/recipe531/Recipe531Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe531;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe531Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe531Application.class, args);
+    }
+
+}
---
+++ ch05/recipe-5-3-1/src/test/java/com/example/recipe531/Recipe531ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe531;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe531ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
