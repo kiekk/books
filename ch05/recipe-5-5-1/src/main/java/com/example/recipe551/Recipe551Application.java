75b5282c442c5bff65e7d95e14c8c5c98bd53f38
shyoon <dbstnsgh2@naver.com>
2021-12-16 21:06:01 +0900

    recipe-5-5-1 프로젝트 생성

---
+++ ch05/recipe-5-5-1/src/main/java/com/example/recipe551/Recipe551Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe551;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe551Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe551Application.class, args);
+    }
+
+}
---
+++ ch05/recipe-5-5-1/src/test/java/com/example/recipe551/Recipe551ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe551;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe551ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
