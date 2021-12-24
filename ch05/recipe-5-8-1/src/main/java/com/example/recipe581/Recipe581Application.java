74f1a999e17de9b30226ec8c51f5127db1021f39
shyoon <dbstnsgh2@naver.com>
2021-12-25 08:01:06 +0900

    async web client

---
+++ ch05/recipe-5-8-1/src/main/java/com/example/recipe581/Recipe581Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe581;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe581Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe581Application.class, args);
+    }
+
+}
---
+++ ch05/recipe-5-8-1/src/main/java/com/example/recipe581/reactive/court/SportType.java
@@ -0,0 +1,26 @@
+package com.example.recipe581.reactive.court;
+
+public class SportType {
+
+    private int id;
+    private String name;
+
+    public SportType() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
