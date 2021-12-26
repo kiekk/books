4d084254654f9f869c90b3f8142b8a4a5c011724
shyoon <dbstnsgh2@naver.com>
2021-12-26 18:21:17 +0900

    Spring Security csrf 비활성, user별 권한 설정

---
+++ ch07/recipe-7-1-2/src/main/java/com/example/recipe712/board/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.recipe712.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+
+}
