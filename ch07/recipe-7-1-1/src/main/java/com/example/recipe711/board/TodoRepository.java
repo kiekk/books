7e2dc4193c502223ac1c06b9504ccafd24378235
shyoon <dbstnsgh2@naver.com>
2021-12-26 18:06:19 +0900

    Spring Security Login 적용 (URL 접근 보안)

---
+++ ch07/recipe-7-1-1/src/main/java/com/example/recipe711/board/TodoRepository.java
@@ -0,0 +1,15 @@
+package com.example.recipe711.board;
+
+import java.util.List;
+
+public interface TodoRepository {
+
+    List<Todo> findAll();
+    Todo findOne(long id);
+    
+    void remove(long id);
+    Todo save(Todo todo);
+
+    List<Todo> findByOwner(String author);
+
+}
---
+++ ch07/recipe-7-1-1/src/main/java/com/example/recipe711/board/TodoServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.recipe711.board;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+class TodoServiceImpl implements TodoService {
+
+    private final TodoRepository todoRepository;
+
+    TodoServiceImpl(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @Override
+    public List<Todo> listTodos() {
+        return todoRepository.findAll();
+    }
+
+    @Override
+    public void save(Todo todo) {
+        this.todoRepository.save(todo);
+    }
+
+    @Override
+    public void complete(long id) {
+        Todo todo = findById(id);
+        todo.setCompleted(true);
+        todoRepository.save(todo);
+    }
+
+    @Override
+    public void remove(long id) {
+        todoRepository.remove(id);
+    }
+
+    @Override
+    public Todo findById(long id) {
+        return todoRepository.findOne(id);
+    }
+}
+
