a26985c731eec922b9d81900318307634ae20636
shyoon <dbstnsgh2@naver.com>
2021-12-27 22:17:23 +0900

    Spring Security - Remember me, logout, headers()

---
+++ ch07/recipe-7-2-2/src/main/java/com/example/recipe722/Recipe722Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe722;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe722Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe722Application.class, args);
+    }
+
+}
