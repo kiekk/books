5f07ca584670845887df2d5f668ee818ec9f4b3b
shyoon <dbstnsgh2@naver.com>
2021-12-27 21:53:26 +0900

    Spring Security - Custom Login Page

---
+++ ch07/recipe-7-2-1/src/main/java/com/example/recipe721/Recipe721Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe721;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe721Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe721Application.class, args);
+    }
+
+}
---
+++ ch07/recipe-7-2-1/src/test/java/com/example/recipe721/Recipe721ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe721;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe721ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
