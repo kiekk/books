3b36cedde364f97490b5bf92b4f957a7a6c9b487
shyoon <dbstnsgh2@naver.com>
2021-12-28 21:23:59 +0900

    recipe-7-3-1 프로젝트 생성

---
+++ ch07/recipe-7-3-1/src/main/java/com/example/recipe731/Recipe731Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe731;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe731Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe731Application.class, args);
+    }
+
+}
