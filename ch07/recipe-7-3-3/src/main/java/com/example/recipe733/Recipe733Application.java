370bb6d4bfb06c88fda59b2c5e91a5e30bb4557b
shyoon <dbstnsgh2@naver.com>
2022-01-02 19:17:36 +0900

    usersByUsernameQuery(), authoritiesByUsernameQuery() 사용 - 유저 기본 정보, 권한 조회

---
+++ ch07/recipe-7-3-3/src/main/java/com/example/recipe733/Recipe733Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe733;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe733Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe733Application.class, args);
+    }
+
+}
---
+++ ch07/recipe-7-3-3/src/main/java/com/example/recipe733/board/web/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.recipe733.board.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Repository("")
+public class LoginController {
+
+    @RequestMapping("login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("logout-success")
+    public String logoutSuccess() {
+        return "logout-success";
+    }
+}
