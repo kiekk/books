9a56e28de930516bb4605572264463f8f9724097
shyoon <dbstnsgh2@naver.com>
2022-01-02 18:43:50 +0900

    .jdbcAuthentication().dataSource() 사용 - DB 저장 방식으로 유저 인증

---
+++ ch07/recipe-7-3-2/src/main/java/com/example/recipe732/Recipe732Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe732;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe732Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe732Application.class, args);
+    }
+
+}
