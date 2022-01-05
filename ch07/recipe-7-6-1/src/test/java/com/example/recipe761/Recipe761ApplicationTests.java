2dc34c1963d33180061be5893bc11dc4b4e5ddcc
shyoon <dbstnsgh2@naver.com>
2022-01-05 22:16:47 +0900

    Spring Security Tag (sec) 사용 - View에서 보안 처리

---
+++ ch07/recipe-7-6-1/src/test/java/com/example/recipe761/Recipe761ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe761;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe761ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
