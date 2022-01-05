5ec80f0b03353d43bc6adf4cf1abc9a0bb3e7ce4
shyoon <dbstnsgh2@naver.com>
2022-01-05 21:48:47 +0900

    표현식에 스프링 빈을 넣어 접근 통제 결정

---
+++ ch07/recipe-7-4-3/src/main/java/com/example/recipe743/board/security/TodoSecurityInitializer.java
@@ -0,0 +1,10 @@
+package com.example.recipe743.board.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class TodoSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    public TodoSecurityInitializer() {
+        super(TodoSecurityConfig.class);
+    }
+}
