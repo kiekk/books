1d84c678f8cfaedafbb32bdd104b3e53d0431984
shyoon <dbstnsgh2@naver.com>
2022-01-05 21:42:25 +0900

    표현식을 이용해 접근 통제 결정

---
+++ ch07/recipe-7-4-2/src/test/java/com/example/recipe742/Recipe742ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe742;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe742ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
