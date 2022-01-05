320f52bebe8eea3452ba124ce40cebeedb6d0c0e
shyoon <dbstnsgh2@naver.com>
2022-01-05 21:55:04 +0900

    @Secured 어노테이션으로 메서드 보안

---
+++ ch07/recipe-7-5-1/src/main/java/com/example/recipe751/Recipe751Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe751;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe751Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe751Application.class, args);
+    }
+
+}
