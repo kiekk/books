4fccdeb115327f7a1e6df470cd15f016003df67b
shyoon <dbstnsgh2@naver.com>
2022-01-06 22:31:55 +0900

    요청 헤더의 User-Agent로 접속한 기기 감지

---
+++ ch08/recipe-8-1-1/src/main/java/com/example/recipe811/Recipe811Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe811;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe811Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe811Application.class, args);
+    }
+
+}
---
+++ ch08/recipe-8-1-1/src/test/java/com/example/recipe811/Recipe811ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe811;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe811ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
