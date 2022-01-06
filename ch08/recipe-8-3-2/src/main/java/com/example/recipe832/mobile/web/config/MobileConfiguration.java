f91c767d829b9f2d2359d3b2801a5e9735f784a4
shyoon <dbstnsgh2@naver.com>
2022-01-06 23:14:05 +0900

    SitePreferenceHandlerInterceptor() 사용

---
+++ ch08/recipe-8-3-2/src/main/java/com/example/recipe832/mobile/web/config/MobileConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.recipe832.mobile.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerInterceptor;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.example.recipe832.mobile.web")
+public class MobileConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public DeviceResolverHandlerInterceptor
+    deviceResolverHandlerInterceptor() {
+        return new DeviceResolverHandlerInterceptor();
+    }
+
+    @Bean
+    public DeviceHandlerMethodArgumentResolver
+    deviceHandlerMethodArgumentResolver() {
+        return new DeviceHandlerMethodArgumentResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(deviceResolverHandlerInterceptor());
+        registry.addInterceptor(new SitePreferenceHandlerInterceptor());
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setOrder(2);
+        return viewResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(deviceHandlerMethodArgumentResolver());
+    }
+}
---
+++ ch08/recipe-8-3-2/src/test/java/com/example/recipe832/Recipe832ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe832;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe832ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
