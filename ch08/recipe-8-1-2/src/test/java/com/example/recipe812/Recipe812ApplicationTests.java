f9c6b6ebd0adcb27b2b7faee16cf3395167e36db
shyoon <dbstnsgh2@naver.com>
2022-01-06 22:43:25 +0900

    spring-mobile-device 사용하여 접속한 기기 감지

---
+++ ch08/recipe-8-1-2/src/test/java/com/example/recipe812/Recipe812ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe812;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe812ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
