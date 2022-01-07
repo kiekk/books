80080705918fdbc65a703cd202c915333f7a207a
shyoon <dbstnsgh2@naver.com>
2022-01-07 23:39:26 +0900

    HikariCP 사용 - datasource 구성

---
+++ ch08/recipe-9-0-2/src/test/java/com/example/recipe902/Recipe902ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe902;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe902ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
