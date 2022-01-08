dbe33bfb8e89c1b308bb1c820cc71e5b1578365e
shyoon <dbstnsgh2@naver.com>
2022-01-08 09:28:50 +0900

    jdbctemplate - 매개변수 바인딩으로 DB 수정

---
+++ ch08/recipe-9-1-3/src/test/java/com/example/recipe913/Recipe913ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe913;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe913ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
