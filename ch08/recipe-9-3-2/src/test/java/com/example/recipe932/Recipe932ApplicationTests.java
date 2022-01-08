62dba49ba2865681abb34719860a0f8b11c53154
shyoon <dbstnsgh2@naver.com>
2022-01-08 18:42:46 +0900

    JdbcDaoSupport 상속

---
+++ ch08/recipe-9-3-2/src/test/java/com/example/recipe932/Recipe932ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe932;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe932ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
