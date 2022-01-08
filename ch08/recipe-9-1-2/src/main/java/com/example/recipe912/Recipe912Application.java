3d49cca85240a3332abaf899056ce58d5cd77c35
shyoon <dbstnsgh2@naver.com>
2022-01-08 09:23:18 +0900

    jdbctemplate - statement setter로 DB 수정

---
+++ ch08/recipe-9-1-2/src/main/java/com/example/recipe912/Recipe912Application.java
@@ -0,0 +1,27 @@
+package com.example.recipe912;
+
+import com.example.recipe912.vehicle.Vehicle;
+import com.example.recipe912.vehicle.VehicleDao;
+import com.example.recipe912.vehicle.config.VehicleConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Recipe912Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(VehicleConfiguration.class);
+
+        VehicleDao vehicleDao = context.getBean(VehicleDao.class);
+        Vehicle vehicle = new Vehicle("TEM0013", "Red", 4, 4);
+        vehicleDao.insert(vehicle);
+
+        vehicle = vehicleDao.findByVehicleNo("TEM0013");
+        System.out.println("Vehicle No: " + vehicle.getVehicleNo());
+        System.out.println("Color: " + vehicle.getColor());
+        System.out.println("Wheel: " + vehicle.getWheel());
+        System.out.println("Seat: " + vehicle.getSeat());
+    }
+
+}
---
+++ ch08/recipe-9-1-2/src/main/java/com/example/recipe912/vehicle/config/VehicleConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.recipe912.vehicle.config;
+
+import com.example.recipe912.vehicle.JdbcVehicleDao;
+import com.example.recipe912.vehicle.VehicleDao;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class VehicleConfiguration {
+
+    @Bean
+    public VehicleDao vehicleDao() {
+        return new JdbcVehicleDao(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("password");
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/vehicle");
+        dataSource.setMinimumIdle(2);
+        dataSource.setMaximumPoolSize(5);
+        return dataSource;
+    }
+}
