1a8f63b2b3147b6428410bf2b1c348d88d841d7e
shyoon <dbstnsgh2@naver.com>
2022-01-08 18:46:44 +0900

    jdbctemplate - 기명 매개변수 사용

---
+++ ch08/recipe-9-4-1/src/test/java/com/example/recipe941/Recipe941ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe941;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe941ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
