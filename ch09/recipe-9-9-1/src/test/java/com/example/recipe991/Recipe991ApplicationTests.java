149b419d80466677483881802de9219ec51364e4
shyoon <dbstnsgh2@naver.com>
2022-01-09 10:47:39 +0900

    jpa context 주입하여 객체 저장

---
+++ ch09/recipe-9-9-1/src/test/java/com/example/recipe991/Recipe991ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe991;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe991ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
