794a6c24673cd4f0199a15081d58c9a8a7a5c5bc
shyoon <dbstnsgh2@naver.com>
2022-01-10 22:06:02 +0900

    recipe-10-1-1 프로젝트 생성

---
+++ ch09/recipe-10-1-1/src/main/java/com/example/recipe1011/Recipe1011Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe1011;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe1011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe1011Application.class, args);
+    }
+
+}
