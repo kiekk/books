8d0cdf8ef51efc62a82fa8187e4778b7edcc405c
shyoon <dbstnsgh2@naver.com>
2022-01-11 22:32:34 +0900

    jdbc commit / rollback 사용하여 트랜잭션 관리

---
+++ ch10/recipe-10-1-2/src/main/java/com/example/recipe1012/bookshop/config/BookstoreConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.recipe1012.bookshop.config;
+
+import com.example.recipe1012.bookshop.BookShop;
+import com.example.recipe1012.bookshop.JdbcBookShop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BookstoreConfiguration {
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(org.postgresql.Driver.class.getName());
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/bookstore");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("password");
+        return dataSource;
+    }
+
+    @Bean
+    public BookShop bookShop(DataSource dataSource) {
+        JdbcBookShop bookShop = new JdbcBookShop();
+        bookShop.setDataSource(dataSource);
+        return bookShop;
+    }
+}
