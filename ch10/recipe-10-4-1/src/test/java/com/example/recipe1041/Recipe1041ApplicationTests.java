effdaf638478f2ddfefaf2ca0b0bbea77580e3c9
shyoon <dbstnsgh2@naver.com>
2022-01-11 22:50:02 +0900

    TransactionTemplate 를 사용하여 트랜잭션 관리

---
+++ ch10/recipe-10-4-1/src/test/java/com/example/recipe1041/Recipe1041ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe1041;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe1041ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
