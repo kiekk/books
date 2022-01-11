e79edcd5d56a21d4326027ee22a07cf4e8f271e5
shyoon <dbstnsgh2@naver.com>
2022-01-11 22:38:29 +0900

    DataSourceTransactionManager(트랜잭션 관리자) 로 트랜잭션 관리

---
+++ ch10/recipe-10-3-1/src/main/java/com/example/recipe1031/Recipe1031Application.java
@@ -0,0 +1,19 @@
+package com.example.recipe1031;
+
+import com.example.recipe1031.bookshop.BookShop;
+import com.example.recipe1031.bookshop.config.BookstoreConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Recipe1031Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BookstoreConfiguration.class);
+
+        BookShop bookShop = context.getBean(BookShop.class);
+        bookShop.purchase("0001", "user1");
+    }
+
+}
