c9e5dba61889e53f53aa000e8fd4372059fdd9d1
shyoon <dbstnsgh2@naver.com>
2022-01-12 16:25:10 +0900

    트랜잭션 격리 속성

---
+++ ch10/recipe-10-7-2/src/test/java/com/example/recipe1072/Recipe1072ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe1072;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe1072ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
