f160d15545f79174f7212709d04323aa5e24322d
shyoon <dbstnsgh2@naver.com>
2022-01-12 16:46:10 +0900

    트랜잭션 관리 - 로드타임 위빙

---
+++ ch10/recipe-10-10-1/src/test/java/com/example/recipe10101/Recipe10101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe10101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe10101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
