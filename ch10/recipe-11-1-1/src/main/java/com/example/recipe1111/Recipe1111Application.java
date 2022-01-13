f8d65eca7a58d2bb950eb55f860872f390c7bf46
shyoon <dbstnsgh2@naver.com>
2022-01-13 21:53:29 +0900

    recipe-11-1-1 프로젝트 생성

---
+++ ch10/recipe-11-1-1/src/main/java/com/example/recipe1111/Recipe1111Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe1111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe1111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe1111Application.class, args);
+    }
+
+}
---
+++ ch10/recipe-11-1-1/src/test/java/com/example/recipe1111/Recipe1111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe1111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe1111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
