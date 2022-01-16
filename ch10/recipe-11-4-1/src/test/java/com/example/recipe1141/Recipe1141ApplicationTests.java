19b11b5d15a7f6f9d34777df7b5d08264e0051ad
shyoon <dbstnsgh2@naver.com>
2022-01-16 21:43:40 +0900

    데이터 출력 전, 입력 데이터 가공, 처리 하기

---
+++ ch10/recipe-11-4-1/src/test/java/com/example/recipe1141/Recipe1141ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe1141;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe1141ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
