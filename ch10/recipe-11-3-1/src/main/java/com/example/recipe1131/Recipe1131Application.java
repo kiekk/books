e0fcb7b03ac2f049517f69503b9bfcfb5ff97f49
shyoon <dbstnsgh2@naver.com>
2022-01-16 21:38:05 +0900

    Custom ItemReader / ItemWriter 작성

---
+++ ch10/recipe-11-3-1/src/main/java/com/example/recipe1131/Recipe1131Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe1131;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe1131Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe1131Application.class, args);
+    }
+
+}
