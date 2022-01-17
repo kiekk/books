23bd20a30eff2efe61d35c00ca5a6d45813db130
shyoon <dbstnsgh2@naver.com>
2022-01-17 22:16:45 +0900

    spring batch 재시도 스텝 구성

---
+++ ch10/recipe-11-6-1/src/test/java/com/example/recipe1161/Recipe1161ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe1161;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe1161ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
