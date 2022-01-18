a627d341ebacd7ed5852c6f7538109c14d1f28b1
shyoon <dbstnsgh2@naver.com>
2022-01-18 21:07:40 +0900

    BatchConfigurer를 직접 구현 - TaskExecutor 구성 후 SimpleJobLauncher에 추가

---
+++ ch10/recipe-11-7-3/src/test/java/com/example/recipe1173/Recipe1173ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe1173;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe1173ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
