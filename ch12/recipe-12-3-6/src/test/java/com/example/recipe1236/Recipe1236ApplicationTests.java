b877a6fbf17ff8bd06a54b34b52cadf1f9128421
shyoon <dbstnsgh2@naver.com>
2022-01-30 08:27:48 +0900

    Neo4j Repository 생성

---
+++ ch12/recipe-12-3-6/src/test/java/com/example/recipe1236/Recipe1236ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe1236;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe1236ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
