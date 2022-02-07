0376f1fa2a9361295b3e0a84e64ef0e461fb9b23
shyoon <dbstnsgh2@naver.com>
2022-02-07 22:17:05 +0900

    ch03 - v-bind, v-model

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <DeclarativeRendering/>
-  <v-html/>
-  <v-pre/>
+  <reactive-variable/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import DeclarativeRendering from "./components/DeclarativeRendering.vue";
-import VHtml from "./components/v-html.vue";
-import VPre from "./components/v-pre.vue";
+import ReactiveVariable from "./components/ReactiveVariable.vue";
 
 export default {
   name: 'App',
   components: {
-    VPre,
-    VHtml,
-    DeclarativeRendering,
+    ReactiveVariable,
     HelloWorld
   },
 }
