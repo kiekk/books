b75b4c9a5b619199618b14bfddc1ed5b2b75bd6e
shyoon <dbstnsgh2@naver.com>
2022-02-07 22:09:40 +0900

    ch03 - v-html

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -2,15 +2,18 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <DeclarativeRendering/>
+  <v-html/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import DeclarativeRendering from "./components/DeclarativeRendering.vue";
+import VHtml from "./components/v-html.vue";
 
 export default {
   name: 'App',
   components: {
+    VHtml,
     DeclarativeRendering,
     HelloWorld
   },
---
+++ ch03/3-3/gramma/src/components/v-html.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-html="tag"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tag: '<i>HTML TEXT</i>'
+    }
+  }
+}
+</script>
