5bce461f0a423c000c13b6f73d65382693d8a1e3
shyoon <dbstnsgh2@naver.com>
2022-02-07 22:23:09 +0900

    ch03 - v-on:click or @click

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <reactive-variable/>
+  <event-listener/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import ReactiveVariable from "./components/ReactiveVariable.vue";
+import EventListener  from "./components/EventListener.vue";
 
 export default {
   name: 'App',
   components: {
-    ReactiveVariable,
-    HelloWorld
+    HelloWorld,
+    EventListener
   },
 }
 </script>
