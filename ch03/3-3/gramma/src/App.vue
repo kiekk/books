55cc691a0d2252725625bffe300d3bb26f109b69
shyoon <dbstnsgh2@naver.com>
2022-02-07 22:11:37 +0900

    ch03 - v-pre

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -3,16 +3,19 @@
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <DeclarativeRendering/>
   <v-html/>
+  <v-pre/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import DeclarativeRendering from "./components/DeclarativeRendering.vue";
 import VHtml from "./components/v-html.vue";
+import VPre from "./components/v-pre.vue";
 
 export default {
   name: 'App',
   components: {
+    VPre,
     VHtml,
     DeclarativeRendering,
     HelloWorld
