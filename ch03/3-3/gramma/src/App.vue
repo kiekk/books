a77fc9f6b1329f2c9b7c13ba8ad8e161c9bdb224
shyoon <dbstnsgh2@naver.com>
2022-02-08 21:22:03 +0900

    ch03 - props

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <watch-watch-effect />
+  <props title="Component 1" :data="[2,4,6]" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import WatchWatchEffect from "./components/WatchWatchEffect.vue";
+import props from "./components/props.vue";
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    WatchWatchEffect
+    props
   },
 }
 </script>
---
+++ ch03/3-3/gramma/src/components/props.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3 v-text="title"/>
+  <span v-for="i in data" :key="i">{{ i }}</span>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    return { count }
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+      validator: function (value) {
+        return typeof value == String
+      },
+    },
+    data: {
+      type: Object,
+      default: () => {
+        return []
+      },
+    },
+  },
+}
+</script>
