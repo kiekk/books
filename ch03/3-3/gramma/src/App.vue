a07a5078175d34f4cf2e259852d7f0be517d9d49
shyoon <dbstnsgh2@naver.com>
2022-02-09 21:56:33 +0900

    ch03 - Customer Event & v-model

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <non-props msg="RootNode Example" />
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <HelloWorld msg="Hello Vue 3.0 + Vite"/>
+  <p>{{ `[${msg}] : ${msg2}` }}</p>
+  <customer-event v-model:msg="msg" v-model:msg2="msg2"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import NonProps from './components/non-props.vue'
+import CustomerEvent from "./components/CustomerEvent.vue";
 
 export default {
   name: 'App',
   components: {
+    CustomerEvent,
     HelloWorld,
-    NonProps
+  },
+  data() {
+    return {
+      msg: '초기 메시지',
+      msg2: '초기 메시지',
+    }
   },
 }
 </script>
