2f9774b31ebcfa17d31468071cc5e6758ee06358
shyoon <dbstnsgh2@naver.com>
2022-02-09 22:02:35 +0900

    ch03 - slots

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png"/>
   <HelloWorld msg="Hello Vue 3.0 + Vite"/>
-  <p>{{ `[${msg}] : ${msg2}` }}</p>
-  <customer-event v-model:msg="msg" v-model:msg2="msg2"/>
+  <slots>
+    <template v-slot="slotProps"> ✔️ {{ slotProps.item }} </template>
+  </slots>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import CustomerEvent from "./components/CustomerEvent.vue";
+import Slots from "./components/Slots.vue";
 
 export default {
   name: 'App',
   components: {
-    CustomerEvent,
+    Slots,
     HelloWorld,
-  },
-  data() {
-    return {
-      msg: '초기 메시지',
-      msg2: '초기 메시지',
-    }
   },
 }
 </script>
