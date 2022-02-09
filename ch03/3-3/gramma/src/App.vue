df53f5fc1be5d9685873bf015f6db65eea19c2b5
shyoon <dbstnsgh2@naver.com>
2022-02-09 22:19:37 +0900

    ch03 - Mixins

--- ch03/3-3/gramma/src/App.vue
+++ ch03/3-3/gramma/src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <div style="height:1000px">
-    <p v-notification:top="100">고정 자리에 표시하기</p>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <HelloWorld msg="Hello Vue 3.0 + Vite"/>
+  <p>Total : {{ total }}</p>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+const exMixin = {
+  data() {
+    return {
+      total: 5,
+      data: [1, 2, 3, 4, 5],
+    }
+  },
+}
+
 export default {
   name: 'App',
+  mixins: [exMixin],
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      // mixins의 변수명과 겹칠 경우 data의 변수가 먼저 적용
+      total: 10,
+    }
   },
 }
 </script>
--- ch03/3-3/gramma/src/main.js
+++ ch03/3-3/gramma/src/main.js
@@ -2,11 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-const app = createApp(App);
-
-app.directive('notification', (el, binding, vnode, prevNode) => {
-  el.style.position = 'fixed'
-  el.style[binding.arg || 'top'] = binding.value + 'px'
-})
-
-app.mount('#app')
+createApp(App).mount('#app')
