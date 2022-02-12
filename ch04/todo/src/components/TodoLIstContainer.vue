1be0254bdb0567333ac5523066647edd08554d05
shyoon <dbstnsgh2@naver.com>
2022-02-12 20:36:46 +0900

    ch04 - TodoLIstContainer.vue 작성

---
+++ ch04/todo/src/components/TodoLIstContainer.vue
@@ -0,0 +1,26 @@
+<template></template>
+
+<script>
+import { ref, readonly, provide } from 'vue'
+
+export default {
+  name: 'TodoListContainer',
+  setup() {
+    const todos = ref([])
+
+    provide('todos', readonly(todos))
+
+    const initTodos = (init_todos) => {
+      todos.value = init_todos
+    }
+
+    const addTodo = (job, date) => {}
+    const removeTodo = (id) => {}
+    const completeTodo = (id) => {}
+
+    provide('addTodo', addTodo)
+    provide('removeTodo', removeTodo)
+    provide('completeTodo', completeTodo)
+  },
+}
+</script>
