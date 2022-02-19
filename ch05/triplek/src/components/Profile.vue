5d382b75c4e1979f03a8f27a842a479d4bcfc6d9
shyoon <dbstnsgh2@naver.com>
2022-02-19 13:44:37 +0900

    ch05 - profile component 작성

---
+++ ch05/triplek/src/components/Profile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container pt-5">
+    <div class="row no-gutters"
+         v-for="(d, index) in user_data.resume"
+         :key="index"
+    >
+      <Card :top="!index" :resume="d" :show="index % 2 === 1"/>
+      <div class="col-sm-2"></div>
+      <Card :top="!index" :resume="d" :show="index % 2 === 0"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+const Car = defineComponent({
+  props: {
+    top: Boolean,
+    resume: Object,
+    show: Boolean,
+  },
+  setup(props) {
+  },
+  template: `
+    <div v-if="show" class="col-sm py-2">
+    <div class="card" :class="{'border-primary bg-primary text-light': top, shadow: top}">
+      <div class="card-body">
+        <h4 class="card-title" :class="[top ? 'text-light' : 'text-muted']">
+          {{ resume.title }}
+        </h4>
+        <hr/>
+        <p class="card-text">
+          {{ resume.content }}
+        </p>
+        <a v-if="resume.URL != 'null'" :href="resume.URL" target="_blank" :class="[top ? 'text-light': 'text-primary']">link</a>
+      </div>
+    </div>
+    </div>
+    <div v-if="!show" class="col-sm"></div>
+  `,
+})
+</script>
