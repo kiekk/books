b79984576ff64b045d9227f6aa6157c62a7188a6
shyoon <dbstnsgh2@naver.com>
2022-02-24 20:01:32 +0900

    ch05 - route 수정 (admin 추가)

--- ch05/triplek/src/router/routes.js
+++ ch05/triplek/src/router/routes.js
@@ -1,6 +1,7 @@
 import Home from '/@components/Home.vue'
 import Profile from '/@components/Profile.vue'
 import Application from '/@components/Application.vue'
+import Admin from '/@components/Admin.vue'
 import { defineComponent } from 'vue'
 
 const NotFound = defineComponent({
@@ -10,8 +11,9 @@ const NotFound = defineComponent({
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/home', name: 'home', component: Home },
-  { path: '/profile', name: 'profile', component: Profile },
   { path: '/application', name: 'application', component: Application },
+  { path: '/profile', name: 'profile', component: Profile },
+  { path: '/admin', name: 'admin', component: Admin },
   { path: '/:catchAll(.*)+', component: NotFound },
 ]
 
