f8802d5c694bb430f663864e24ac2506697a1cf4
shyoon <dbstnsgh2@naver.com>
2022-02-25 21:34:01 +0900

    ch05 - Blog component 수정(markdown 데이터 적용)

--- ch05/triplek/src/components/Blog.vue
+++ ch05/triplek/src/components/Blog.vue
@@ -5,39 +5,40 @@
         <h3 class="pb-4 mb-4 font-italic border-bottom">Blog</h3>
 
         <article
-            class="blog-post"
-            v-for="article in sliced_posts"
-            :key="article.id"
-            :id="'article-' + article.id"
+          class="blog-post"
+          v-for="article in sliced_posts"
+          :key="article.id"
+          :id="'article-' + article.id"
         >
           <h2 class="blog-post-title">{{ article.title }}</h2>
           <p class="blog-post-meta">{{ article.date }}</p>
-          <div v-html="article.post" />
+          <div v-if="article.type == 'html'" v-html="article.post" />
+          <markdown v-else-if="(type = 'md')" id="md" :source="article.post" />
         </article>
 
         <nav class="blog-pagination" aria-label="Pagination">
           <a
-              class="btn"
-              :class="[
+            class="btn"
+            :class="[
               page == 1
                 ? 'btn-outline-secondary disabled'
                 : 'btn-outline-primary',
             ]"
-              @click="page--"
-              href="#"
-          >Older</a
+            @click="page--"
+            href="#"
+            >Older</a
           >
           {{ page }}
           <a
-              class="btn"
-              :class="[
+            class="btn"
+            :class="[
               page == total_pages
                 ? 'btn-outline-secondary disabled'
                 : 'btn-outline-primary',
             ]"
-              @click="page++"
-              href="#"
-          >Newer</a
+            @click="page++"
+            href="#"
+            >Newer</a
           >
         </nav>
       </div>
@@ -57,19 +58,19 @@
           <ol class="list-unstyled mb-0">
             <li v-for="(archive, index) in archives" :key="index">
               <a
-                  data-bs-toggle="collapse"
-                  :data-bs-target="'#archive-' + index"
-                  href="#"
-              >{{ archive.key }}</a
+                data-bs-toggle="collapse"
+                :data-bs-target="'#archive-' + index"
+                href="#"
+                >{{ archive.key }}</a
               >
               <div class="collapse" :id="'archive-' + index">
                 <ol class="list-unstyled ms-3">
                   <li v-for="post in archive.posts" :key="post.id">
                     {{ post.date.substring(0, 10) + ': ' }}
                     <a
-                        :href="'#article-' + post.id"
-                        @click="onArchive($event, post.id)"
-                    >{{ post.title.substring(0, 20) }}</a
+                      :href="'#article-' + post.id"
+                      @click="onArchive($event, post.id)"
+                      >{{ post.title.substring(0, 20) }}</a
                     >
                   </li>
                 </ol>
@@ -95,6 +96,7 @@
 
 <script>
 import { ref, reactive, onMounted, computed } from 'vue'
+import Markdown from 'vue3-markdown-it'
 import useAxios from '/@app_modules/axios.js'
 export default {
   setup() {
@@ -111,18 +113,18 @@ export default {
 
       const temp_group = posts.reduce((accumulator, currentValue) => {
         ;(accumulator[currentValue['date'].substring(0, 7)] =
-            accumulator[currentValue['date'].substring(0, 7)] || []).push(
-            currentValue
+          accumulator[currentValue['date'].substring(0, 7)] || []).push(
+          currentValue
         )
         return accumulator
       }, {})
 
       Object.assign(
-          archives,
-          Object.keys(temp_group).map((key) => ({
-            key: key,
-            posts: temp_group[key],
-          }))
+        archives,
+        Object.keys(temp_group).map((key) => ({
+          key: key,
+          posts: temp_group[key],
+        }))
       )
     }
 
@@ -151,6 +153,9 @@ export default {
       archives,
       onArchive,
     }
+  },
+  components: {
+    Markdown,
   },
 }
 </script>
