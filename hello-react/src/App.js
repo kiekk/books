448c3378c1a8acb730e2b9cdf89a0347e4975aa7
shyoon <dbstnsgh2@naver.com>
2022-03-12 22:54:40 +0900

    ch01 - undefined 반환 - 1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 
 function App() {
-  const name = 'React';
-  return (
-    <div>
-      {name === 'React' && <h1>Hello React!</h1>}
-    </div>
-  );
+  const name = undefined;
+  return name;
 }
+/*
 
+함수에서 바로 undefined를 반환할 경우 아뢔와 같은 에러가 발생한다.
+Uncaught Error: App(...): Nothing was returned from render. This usually means a return statement is missing. Or, to render nothing, return null.
+ */
 export default App;
