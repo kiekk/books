e1c6e9e1ba10cf5e4986a18fcc9d0b1939cdb1d6
shyoon <dbstnsgh2@naver.com>
2022-03-24 21:10:14 +0900

    ch03 - component props - defaultProps

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,4 +2,8 @@ const MyComponent = props => {
   return <div>My First Component, prop: {props.name}</div>
 }
 
+MyComponent.defaultProps = {
+  name: 'Default Props Name, React'
+}
+
 export default MyComponent;
