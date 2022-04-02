bcb4e1fbcadd712060a3663e1bf36681be4475d5
shyoon <dbstnsgh2@naver.com>
2022-04-02 20:42:48 +0900

    ch03 - PropTypes (isRequired 속성)

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       My First Component, prop: {name}<br/>
       children name is {children}
+      <br />
+      My FavoriteNumber is {favoriteNumber}
     </div>
   );
 }
@@ -14,7 +16,8 @@ MyComponent.defaultProps = {
 }
 
 MyComponent.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired
 }
 
 export default MyComponent;
