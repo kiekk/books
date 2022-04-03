97b4c4f797a413e7f1b990f82c107fe6adcff1ab
shyoon <dbstnsgh2@naver.com>
2022-04-03 21:35:45 +0900

    ch03 - class component props 사용

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,14 +1,18 @@
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      My First Component, prop: {name}<br/>
-      children name is {children}
-      <br />
-      My FavoriteNumber is {favoriteNumber}
-    </div>
-  );
+class MyComponent extends Component {
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        My First Component, prop: {name}<br/>
+        children name is {children}
+        <br />
+        My FavoriteNumber is {favoriteNumber}
+      </div>
+    )
+  }
 }
 
 MyComponent.defaultProps = {
