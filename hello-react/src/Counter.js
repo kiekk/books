3d2c97b262ec6fcb54fd5a536b32ed15239448a2
shyoon <dbstnsgh2@naver.com>
2022-04-08 21:18:50 +0900

    ch03 - class형 component - constructor에서 state 분리

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,18 +1,17 @@
 import {Component} from 'react'
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0
-    }
+  state = {
+    number: 0,
+    fixedNumber: 0
   }
 
   render() {
-    const {number} = this.state
+    const {number, fixedNumber} = this.state
     return (
       <div>
         <h1>{number}</h1>
+        <h1>{fixedNumber}</h1>
         <button
           onClick={() => {
             this.setState({number: number + 1})
