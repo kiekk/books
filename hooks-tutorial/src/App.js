199720da86f506755b4b741e687fef65e33bee50
shyoon <dbstnsgh2@naver.com>
2022-04-10 21:01:29 +0900

    ch09 - Hook - useReducer - 여러 상태 관리하기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,8 @@
-import Counter from "./Counter";
+import Info from "./Info";
 
 function App() {
   return (
-    <Counter />
+    <Info />
   );
 }
 
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,31 +1,27 @@
-import {useState, useEffect} from "react";
+import {useReducer} from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value
+  }
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log('effect')
-    console.log(name);
-    return () => {
-      console.log('cleanup')
-      console.log(name);
-    }
-  }, [])
-
-
-  const onChangeName = e => {
-    setName(e.target.value);
-  }
-
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: ''
+  })
+  const {name, nickname} = state;
+  const onChange = e => {
+    dispatch(e.target);
   }
 
   return (
     <div>
       <div>
-        <input type="text" value={name} onChange={onChangeName}/>
-        <input type="text" value={nickname} onChange={onChangeNickname}/>
+        <input type="text" value={name} onChange={onChange}/>
+        <input type="text" value={nickname} onChange={onChange}/>
       </div>
       <div>
         <div>
