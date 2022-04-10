9c86ea298a6a6b4a142148d4ad19779b9854bee8
shyoon <dbstnsgh2@naver.com>
2022-04-10 15:43:15 +0900

    ch04 - state 사용하여 onchange event  변경 값 저장

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,12 +1,17 @@
 import {Component} from "react";
 
 class EventPractice extends Component {
+
+  state = {
+    message: ''
+  }
+
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
-        <input type="text" name="message" onChange={(e) => {
-          console.log(e.target.value);
+        <input type="text" name="message" value={this.state.message} onChange={(e) => {
+          this.setState({message: e.target.value})
         }}/>
       </div>
     )
