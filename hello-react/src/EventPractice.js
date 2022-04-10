61631d19f7983a500648103cc43e8a4da958330c
shyoon <dbstnsgh2@naver.com>
2022-04-10 15:41:05 +0900

    ch04 - 이벤트 핸들링 연습용 EventPractice component 생성

---
+++ hello-react/src/EventPractice.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input type="text" name="message" onChange={(e) => {
+          console.log(e);
+        }}/>
+      </div>
+    )
+  }
+}
+
+export default EventPractice;
