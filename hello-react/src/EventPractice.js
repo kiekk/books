a661046e55bb3d1a5befa832a00af57777ce74db
shyoon <dbstnsgh2@naver.com>
2022-04-10 15:50:57 +0900

    ch04 - 메서드를 만들어 event에 전달

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -6,19 +6,31 @@ class EventPractice extends Component {
     message: ''
   }
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    })
+  }
+
+  handleClick() {
+    this.setState({
+      message: ''
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
-        <input type="text" name="message" value={this.state.message} onChange={(e) => {
-          this.setState({message: e.target.value})
-        }}/>
+        <input type="text" name="message" value={this.state.message} onChange={this.handleChange}/>
         <h2>{this.state.message}</h2>
-        <button onClick={() => {
-          this.setState({
-            message: ''
-          })
-        }}>
+        <button onClick={this.handleClick}>
           Reset
         </button>
       </div>
