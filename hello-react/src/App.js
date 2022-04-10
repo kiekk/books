6b32c829ef8b746fae40790435079442557cf36d
shyoon <dbstnsgh2@naver.com>
2022-04-10 16:42:26 +0900

    ch05 - ref 학습용 ValidationSample component 생성

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,14 @@
+import {Component} from "react";
 import './App.css';
-import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
 
-const App = () => {
-  return <EventPractice />
+class App extends Component {
+  render() {
+    return (
+      <ValidationSample />
+    )
+  }
 }
 
+// 추후 ref를 사용할 예정이기 때문에 미리 class component로 변경
 export default App;
