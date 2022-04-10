4ea6e653e84cea1ca63b9f7c479f1f586ead9e7a
shyoon <dbstnsgh2@naver.com>
2022-04-10 15:59:59 +0900

    ch04 - babel의 transform-class-properties 문법 사용하여 메서드 바인딩 로직 수정

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -8,17 +8,15 @@ class EventPractice extends Component {
 
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
       message: e.target.value
     })
   }
 
-  handleClick() {
+  handleClick = () => {
     this.setState({
       message: ''
     })
