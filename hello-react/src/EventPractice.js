332a5a98a2431b7fec7e64d5e42e56ff7839b704
shyoon <dbstnsgh2@naver.com>
2022-04-10 16:12:14 +0900

    ch04 - 함수형 컴포넌트로 event handling

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,42 +1,36 @@
-import {Component} from "react";
+import {useState} from "react";
 
-class EventPractice extends Component {
-
-  state = {
-    username: '',
-    message: ''
+const EventPractice = () => {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = e => setUsername(e.target.value);
+  const onChangeMessage = e => setMessage(e.target.value);
+  const onClick = () => {
+    setUsername('');
+    setMessage('');
+  }
+  const onKeyPress = e => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
   }
 
-  constructor(props) {
-    super(props);
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  handleClick = () => {
-    this.setState({
-      username: '',
-      message: '',
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-        <input type="text" name="message" value={this.state.message} onChange={this.handleChange}/>
-        <h2>{this.state.username}: {this.state.message}</h2>
-        <button onClick={this.handleClick}>
-          Reset
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input type="text"
+      name="username"
+      value={username}
+      onChange={onChangeUsername}/>
+      <input type="text"
+      name="message"
+      value={message}
+      onChange={onChangeMessage}
+      onKeyPress={onKeyPress}/>
+      <button onClick={onClick}>Reset</button>
+      <h2>{username}: {message}</h2>
+    </div>
+  )
 }
 
 export default EventPractice;
