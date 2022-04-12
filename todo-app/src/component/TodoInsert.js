52489ec4059641cebebc89d7c6d957a02cb48e0e
shyoon <dbstnsgh2@naver.com>
2022-04-13 07:10:45 +0900

    ch10 - TodoInsert 작성

---
+++ todo-app/src/component/TodoInsert.js
@@ -0,0 +1,18 @@
+import { MdAdd } from 'react-icons/md'
+import './TodoInsert.scss'
+
+const TodoInsert = () => {
+    return (
+        <form className="TodoInsert">
+            <input
+                type="text"
+                placeholder="할 일을 입력하세요"
+            />
+            <button type="submit">
+                <MdAdd />
+            </button>
+        </form>
+    )
+}
+
+export default TodoInsert
