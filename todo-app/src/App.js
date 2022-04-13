ccd83302f371a7fdf0ec35f0235a089eb2add134
shyoon <dbstnsgh2@naver.com>
2022-04-13 21:38:09 +0900

    ch10 - 할 일 추가 기능

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useRef, useCallback} from 'react';
 import TodoTemplate from "./component/TodoTemplate";
 import TodoInsert from "./component/TodoInsert";
 import TodoList from "./component/TodoList";
@@ -22,9 +22,23 @@ function App() {
     }
   ]);
 
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(text => {
+      const todo = {
+        id: nextId.current,
+        text: text,
+        checked: false
+      }
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
+      <TodoInsert onInsert={onInsert}/>
       <TodoList todos={todos}/>
     </TodoTemplate>
   );
