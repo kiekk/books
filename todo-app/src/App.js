3be11c7494287d187e50b917d2f3f4bf17913a7b
shyoon <dbstnsgh2@naver.com>
2022-04-14 20:48:59 +0900

    ch11 - bulk data 추가로 성능 저하 확인

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,26 +3,39 @@ import TodoTemplate from "./component/TodoTemplate";
 import TodoInsert from "./component/TodoInsert";
 import TodoList from "./component/TodoList";
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false
-    }
-  ]);
+function createBulkTodos() {
+  const array = [];
+  for (let i=0;i<=2500;i++) {
+    array.push({
+      id: i,
+      text: `할 일${i}`,
+      checked: false,
+    })
+  }
+  return array;
+}
 
-  const nextId = useRef(4);
+function App() {
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트의 기초 알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링해 보기',
+  //     checked: true
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어 보기',
+  //     checked: false
+  //   }
+  // ]);
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(text => {
       const todo = {
