1b095db8c0914c2e0bece737bae34375d1eb02a1
shyoon <dbstnsgh2@naver.com>
2022-04-16 08:54:28 +0900

    ch13 - Link 컴포넌트 사용 (페이지 이동 없이 페이지 변경)

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,10 +1,22 @@
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route, BrowserRouter, Link} from 'react-router-dom';
 import About from "./About";
 import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">
+            홈
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            소개
+          </Link>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
