70cb4a8ff3250d5bfed26a73c4d69ea54ecd3a90
shyoon <dbstnsgh2@naver.com>
2022-04-16 22:24:33 +0900

    ch14 - Categories component 생성

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,7 +1,14 @@
 import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
+import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
-  return <NewsList />
+  return (
+    <BrowserRouter>
+      <Categories />
+      <NewsList />
+    </BrowserRouter>
+  )
 }
 
 export default App;
