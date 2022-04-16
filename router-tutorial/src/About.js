a399e496fbb458f0d30b04bdd5b607e62c987227
shyoon <dbstnsgh2@naver.com>
2022-04-16 09:52:40 +0900

    ch13 - location 으로 query string 받기 (v6 부터는 에러 발생)

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,8 +1,19 @@
-const About = () => {
+import qs from "qs";
+
+const About = ({ location }) => {
+  // locaiton.search 역시 v6 부터는 사용할 수 없기 때문에 에러 발생
+  // v6 부터는 params. location. history 를 사용할 수 없다고 한다.
+
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true// 이 설정을 통해 문자열 맨 앞의 ? 를 생략합니다/
+  })
+  const showDetail = query.detail === 'true' // 쿼리의 파싱 결과 값은 boolean이 아니라 문자열
+
   return (
     <div>
       <h1>소개</h1>
       <p>about 페이지 입니다.</p>
+      <p>{showDetail && <p>detail 값을 true로 설정</p>}</p>
     </div>
   )
 }
