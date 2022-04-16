c5540ff9850461e2ba8db2c7ad7db9c453a6f166
shyoon <dbstnsgh2@naver.com>
2022-04-16 21:36:13 +0900

    ch14 - newsapi 에서 뉴스 목록 데이터 가져오기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=aa592dd277fa4d5ab5b8e08ff5d4604c')
 
       setData(response.data);
     } catch (e) {
