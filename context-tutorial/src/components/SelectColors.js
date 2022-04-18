5c11206651e39dd92f9fbadfa9d0b3a1b73aa898
shyoon <dbstnsgh2@naver.com>
2022-04-19 07:08:43 +0900

    ch15 - SelectColors component -> 클래스형 컴포넌트로 변환(statis contextType 사용 예정)

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,37 +1,29 @@
-import {ColorConsumer} from "../contexts/color";
+import {Component} from "react";
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-const SelectColors = () => {
-  return (
-    <div>
-      <h2>색상을 입력하세요.</h2>
-      <ColorConsumer>
-        {({actions}) => (
-          <div style={{display: 'flex'}}>
-            {colors.map(color => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: '24px',
-                  height: '24px',
-                  cursor: 'pointer'
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={e => {
-                  e.preventDefault();
-                  actions.setSubColor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr/>
-    </div>
-
-  )
+class SelectColors extends Component {
+  render() {
+    return (
+      <div>
+        <h2>색상을 입력하세요.</h2>
+        <div style={{display: 'flex'}}>
+          {colors.map(color => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer'
+              }}
+            />
+          ))}
+        </div>
+        <hr/>
+      </div>
+    )
+  }
 }
 
 export default SelectColors;
