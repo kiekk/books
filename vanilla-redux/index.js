5ba0c2c2581c8360b6f587fa3c478b1dca6543b1
shyoon <dbstnsgh2@naver.com>
2022-04-20 07:18:44 +0900

    ch16 - click event 연결 및 index.js 추가

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -59,3 +59,16 @@ const render = () => {
 
 render()
 store.subscribe(render)
+
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch())
+}
+
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1))
+}
+
+btnDecrease.onclick = () => {
+  store.dispatch(decrease())
+}
