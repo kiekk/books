4d16a2d96a77dde37be97a2d439f94c7a6edff47
shyoon <dbstnsgh2@naver.com>
2022-04-21 22:13:45 +0900

    ch17 - mapStateToProps, mapDispatchToProps 익명 함수로 작성

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -6,20 +6,16 @@ const CounterContainer = ({ number, increase, decrease }) => {
   return <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
 }
 
-const mapStateToProps = state => ({
-  number: state.counter.number
-})
-
-const mapDispatchToProps = dispatch => ({
-  increase: () => {
-    dispatch(increase())
-  },
-  decrease: () => {
-    dispatch(decrease())
-  }
-})
-
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  state => ({
+    number: state.counter.number
+  }),
+  dispatch => ({
+    increase: () => {
+      dispatch(increase())
+    },
+    decrease: () => {
+      dispatch(decrease())
+    }
+  })
 )(CounterContainer);
