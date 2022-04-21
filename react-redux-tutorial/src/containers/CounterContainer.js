7a9edfaad2408aa4ffbc76c3d27eb17f67ac263e
shyoon <dbstnsgh2@naver.com>
2022-04-22 07:01:10 +0900

    ch17 - useCallback 사용하여 성능 최적화

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,3 +1,4 @@
+import {useCallback} from "react";
 import Counter from "../components/Counter";
 import {useDispatch, useSelector} from "react-redux";
 import {increase, decrease} from "../modules/counter";
@@ -5,11 +6,13 @@ import {increase, decrease} from "../modules/counter";
 const CounterContainer = () => {
   const number = useSelector(state => state.counter.number)
   const dispatch = useDispatch()
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch])
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch])
 
   return <Counter
     number={number}
-    onIncrease={() => dispatch(increase())}
-    onDecrease={() => dispatch(decrease())}
+    onIncrease={onIncrease}
+    onDecrease={onDecrease}
   />
 }
 
