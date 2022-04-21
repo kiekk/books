ba0e798f82aab5d9a8c0ca4cd6f0eca02771d1ab
shyoon <dbstnsgh2@naver.com>
2022-04-22 06:55:17 +0900

    ch17 - todos 모듈 - immer 적용

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import {createAction, handleActions} from 'redux-actions'
+import produce from 'immer'
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'
 const INSERT = 'todos/INSERT'
@@ -12,10 +13,11 @@ let id = 3
 export const insert = createAction(INSERT, text => ({
   id: id++,
   text,
-  done: false
+  done: false,
 }))
 
 export const toggle = createAction(TOGGLE, id => id)
+
 export const remove = createAction(REMOVE, id => id)
 
 const initialState = {
@@ -37,18 +39,25 @@ const initialState = {
 const todos = handleActions(
   {
     [CHANGE_INPUT]: (state, {payload: input}) =>
-      ({...state, input}),
+      produce(state, draft => {
+        draft.input = input
+      }),
     [INSERT]: (state, {payload: todo}) =>
-      ({...state, todos: state.todos.concat(todo)}),
+      produce(state, draft => {
+        draft.todos.push(todo)
+      }),
     [TOGGLE]: (state, {payload: id}) =>
-      ({
-        ...state,
-        todos: state.todos.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo)
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id === id)
+        todo.done = !todo.done
       }),
     [REMOVE]: (state, {payload: id}) =>
-      ({...state, todos: state.todos.filter(todo => todo.id !== id)}),
+      produce(state, draft => {
+        const index = draft.todos.findIndex(todo => todo.id === id)
+        draft.todos.splice(index, 1)
+      }),
   }
   ,
   initialState
 )
-export default todos;
+export default todos
