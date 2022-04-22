142f2fcb8c701a1393011db321e36d8220dbee91
shyoon <dbstnsgh2@naver.com>
2022-04-22 22:11:19 +0900

    ch18 - loading 상태 관리 및 코드 리팩토링

--- redux-middleware-tutorial/src/modules/sample.js
+++ redux-middleware-tutorial/src/modules/sample.js
@@ -18,59 +18,19 @@ export const getUsers = createRequestThunk(GET_USERS, api.getUsers)
 
 // 초기 상태 선언
 const initialState = {
-  loading: {
-    GET_POST: false,
-    GET_USERS: false
-  },
   post: null,
   users: null
 }
 
 const sample = handleActions(
   {
-    [GET_POST]: state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true
-      }
-    }),
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false
-      },
       post: action.payload
-    }),
-    [GET_POST_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false
-      }
-    }),
-    [GET_USERS]: state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: true
-      }
     }),
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false
-      },
       users: action.payload
-    }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false
-      }
     })
   },
   initialState
