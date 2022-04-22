f9e3e6a14fbafa47c39882a9ae043defafa9c249
shyoon <dbstnsgh2@naver.com>
2022-04-23 08:50:00 +0900

    ch19 - React.lazy, Suspense 사용하여 코드 스플리팅 리팩토링

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, Suspense} from "react";
 import './App.css';
+import logo from './logo.svg';
+
+const SplitMe = React.lazy(() => import('./SplitMe'))
 
 function App() {
+  const [visible, setVisible] = useState(false)
+
   const onClick = () => {
-    import('./notify').then(result => result.default())
+    setVisible(true)
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
