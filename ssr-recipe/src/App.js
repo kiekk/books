30a1c1f6978c630ee3b1ee6cc2ac12fd66b1f6ce
shyoon <dbstnsgh2@naver.com>
2022-04-24 16:19:30 +0900

    ch20 - build 버전 수정

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -1,18 +1,17 @@
+import React from 'react'
 import Menu from "./components/Menu";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route} from "react-router-dom";
 import RedPage from "./pages/RedPage";
 import BluePage from "./pages/BluePage";
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
+    <div>
       <Menu/>
       <hr/>
-      <Routes>
-        <Route path="/red" element={<RedPage/>}/>
-        <Route path="/blue" element={<BluePage/>}/>
-      </Routes>
-    </BrowserRouter>
+      <Route path="/red" component={RedPage}/>
+      <Route path="/blue" component={BluePage}/>
+    </div>
   )
 }
 
--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import express from 'express'
-import {StaticRouter} from 'react-router-dom'
-import App from './App'
+import { StaticRouter } from 'react-router-dom'
+import App from "./App";
 
 const app = express()
 
@@ -10,12 +10,12 @@ const app = express()
 const serverRender = (req, res, next) => {
   // 이 함수는 404 에러가 발생해야 하는 상황에 404를 발생하지 않고 서버 사이드 렌더링을 해 줍니다.
 
-  const context = {}
+  const context = {};
   const jsx = (
     <StaticRouter location={req.url} context={context}>
       <App />
     </StaticRouter>
-  )
+  );
   const root = ReactDOMServer.renderToString(jsx)
   res.send(root)
 }
