fe14ed3766f83c30626646e26a35800962addde3
shyoon <dbstnsgh2@naver.com>
2022-04-25 06:59:25 +0900

    ch20 - UsersPage에 렌더링 추가

--- ssr-recipe/src/pages/UsersPage.js
+++ ssr-recipe/src/pages/UsersPage.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import UsersContainer from '../containers/UsersContainer'
+import {Route} from "react-router-dom";
 
 const UsersPage = () => {
-  return <UsersContainer />
+  return (
+    <>
+      <UsersContainer/>
+      <Route path="/users/:id" render={({match}) => <UsersContainer id={match.params.id}/>}/>
+    </>
+  )
 }
 
 export default UsersPage
