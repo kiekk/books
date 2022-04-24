e596e7961333e866f8f71eb2723bc83aa0bb1e15
shyoon <dbstnsgh2@naver.com>
2022-04-24 16:55:02 +0900

    ch20 - index.server.js - 정적 소스 로딩 추가

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -3,8 +3,45 @@ import ReactDOMServer from 'react-dom/server'
 import express from 'express'
 import { StaticRouter } from 'react-router-dom'
 import App from "./App";
+import path from 'path'
+import fs from 'fs'
 
 const app = express()
+
+// asset-manifest.json 에서 파일 경로들을 조회합니다.
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf-8')
+)
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key))  // chunk.js로 끝나는 키를 찾음
+  .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환
+  .join('')
+
+function createPage(root) {
+  return `
+   <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <meta name="theme-color" content="#000000" />
+            <title>React App</title>
+            <link href="${manifest.files['main.css']}" rel="stylesheet" />
+          </head>
+          <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root">
+              ${root}
+            </div>
+            <script src="${manifest.files['runtime~main.js']}"></script>
+            ${chunks}
+            <script src="${manifest.files['main.js']}"></script>
+          </body>
+        </html>
+  `
+}
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수
 const serverRender = (req, res, next) => {
@@ -17,9 +54,14 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx)
-  res.send(root)
+  res.send(createPage(root))
 }
 
+const serve = express.static(path.resolve('./build'), {
+  index: false  // "/" 경로에서 index.html을 보여주지 않도록 설정
+})
+
+app.use(serve)  // serverRender 보다 먼저 위치해야 합니다.
 app.use(serverRender)
 
 app.listen(5000, () => {
