bf8148d6878c439c14bcc409b23ffd50ef619cc8
shyoon <dbstnsgh2@naver.com>
2022-04-25 21:42:21 +0900

    ch20 - loadableReady 사용하여 성능 최적화

--- ssr-recipe/src/index.js
+++ ssr-recipe/src/index.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware} from "redux";
-import {Provider} from 'react-redux'
-import thunk from "redux-thunk";
-import rootReducer, {rootSaga} from "./modules";
-import createSagaMiddleware from "redux-saga";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer, { rootSaga } from './modules'
+import createSagaMiddleware from 'redux-saga'
+import { loadableReady } from '@loadable/component'
 
 const sagaMiddleware = createSagaMiddleware()
 
-const store = createStore(rootReducer, window.__PRELOAD_STATE__, applyMiddleware(thunk, sagaMiddleware))
+const store = createStore(
+  rootReducer,
+  window.__PRELOADED_STATE__, // 이 값을 초기 상태로 사용
+  applyMiddleware(thunk, sagaMiddleware),
+)
 
 sagaMiddleware.run(rootSaga)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>
-);
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = document.getElementById('root')
+
+if (process.env.NODE_ENV === 'production') {
+  loadableReady(() => {
+    ReactDOM.hydrate(<Root />, root)
+  })
+} else {
+  ReactDOM.render(<Root />, root)
+}
