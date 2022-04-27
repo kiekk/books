b949ff11e1079b7854a9ab95310314c3e98a820c
shyoon <dbstnsgh2@naver.com>
2022-04-27 21:15:41 +0900

    ch21 - 특정 상황일 경우 미들웨어 처리하도록 로직 작성

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -5,6 +5,12 @@ const app = new Koa();
 app.use((ctx, next) => {
   console.log(ctx.url)
   console.log(1)
+
+  if(ctx.query.authorized !== '1') {
+    ctx.status = 401
+    return;
+  }
+
   next()
 })
 
