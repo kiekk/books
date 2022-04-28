d9b8a30da41272d54b5157104ddc314c10bca48f
shyoon <dbstnsgh2@naver.com>
2022-04-29 07:03:28 +0900

    ch23 - user schema/model bcrypt 적용

--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,9 +1,18 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcrypt'
 
 const UserSchema = new Schema({
   username: String,
   hashedPassword: String
 })
+
+UserSchema.methods.setPassword = async function(password) {
+  this.hashedPassword = await bcrypt.hash(password, 10)
+}
+
+UserSchema.methods.checkPassword = async function(password) {
+  return await bcrypt.compare(password, this.hashedPassword)
+}
 
 const User = mongoose.model('User', UserSchema);
 
