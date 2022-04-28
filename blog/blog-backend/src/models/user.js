682190da519cc3779e951f2ff843376830fb24e5
shyoon <dbstnsgh2@naver.com>
2022-04-29 07:07:52 +0900

    ch23 - static 메소드 작성

--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import bcrypt from 'bcrypt'
 
 const UserSchema = new Schema({
@@ -6,12 +6,16 @@ const UserSchema = new Schema({
   hashedPassword: String
 })
 
-UserSchema.methods.setPassword = async function(password) {
+UserSchema.methods.setPassword = async function (password) {
   this.hashedPassword = await bcrypt.hash(password, 10)
 }
 
-UserSchema.methods.checkPassword = async function(password) {
+UserSchema.methods.checkPassword = async function (password) {
   return await bcrypt.compare(password, this.hashedPassword)
+}
+
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({username})
 }
 
 const User = mongoose.model('User', UserSchema);
