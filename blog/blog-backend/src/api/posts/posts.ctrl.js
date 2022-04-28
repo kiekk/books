b10042f8c500293e73ef091c83fea9fcabe8ab59
shyoon <dbstnsgh2@naver.com>
2022-04-28 22:25:44 +0900

    ch22 - 페이징 기능 구현

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -44,8 +44,20 @@ export const write = async ctx => {
 }
 
 export const list = async ctx => {
+
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    ctx.body = await Post.find().sort({_id: -1}).limit(10).exec();
+    ctx.body = await Post.find()
+      .sort({_id: -1})
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
   } catch (e) {
     ctx.throw(500, e);
   }
