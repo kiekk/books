dddb029a30adcd4f96e94e6192db5974aa75e71a
shyoon <dbstnsgh2@naver.com>
2022-05-01 21:11:04 +0900

    ch24 - 로그인 기능 구현

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,12 +1,17 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {changeField, initializeForm} from "../../modules/auth";
+import {changeField, initializeForm, login} from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
+import {withRouter} from 'react-router-dom'
+import {check} from "../../modules/user";
 
-const LoginForm = () => {
+const LoginForm = ({history}) => {
   const dispatch = useDispatch();
-  const {form} = useSelector(({auth}) => ({
-    form: auth.login
+  const {form, auth, authError, user} = useSelector(({auth, user}) => ({
+    form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user
   }))
 
   const onChange = e => {
@@ -24,12 +29,32 @@ const LoginForm = () => {
   const onSubmit = e => {
     e.preventDefault();
 
-    // TODO: 구현 예정
+    const {username, password} = form;
+    dispatch(login({username, password}))
   }
 
   useEffect(() => {
     dispatch(initializeForm('login'))
   }, [dispatch])
+
+  useEffect(() => {
+    if(authError) {
+      console.log('오류 발생')
+      console.log(authError)
+      return;
+    }
+
+    if(auth) {
+      console.log('로그인 성공')
+      dispatch(check())
+    }
+  }, [auth, authError, dispatch])
+
+  useEffect(() => {
+    if(user) {
+      history.push('/')
+    }
+  }, [history, user])
 
   return (
     <AuthForm
@@ -41,4 +66,4 @@ const LoginForm = () => {
   )
 }
 
-export default LoginForm;
+export default withRouter(LoginForm);
