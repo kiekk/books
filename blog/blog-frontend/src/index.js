8fd7b86a55a347af697153f5bc5af18f1761ddd6
shyoon <dbstnsgh2@naver.com>
2022-05-01 16:53:57 +0900

    ch24 - Provider import 오류 수정

--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-import {Provider} from 'react-router-dom'
+import {Provider} from 'react-redux'
 import {createStore} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import rootReducer from "./modules";
