c5a5d03985b99048059a7cd482ef94450cb6c353
shyoon <dbstnsgh2@naver.com>
2022-05-01 18:12:58 +0900

    ch24 - reducer 오류 수정

--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from "redux";
 import auth from "./auth";
 
-const rootReducer = combineReducers(auth)
+const rootReducer = combineReducers({auth})
 
 export default rootReducer;
