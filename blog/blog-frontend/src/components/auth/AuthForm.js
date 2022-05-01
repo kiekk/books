fb0859422a1264ba84591a0f6d3022945a77cdce
shyoon <dbstnsgh2@naver.com>
2022-05-01 17:23:46 +0900

    ch24 - page 별 AuthForm type 적용

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -43,17 +43,35 @@ const ButtonWitMarginTop = styled(Button)`
   margin-top: 1rem;
 `
 
-const AuthForm = () => {
+const textMap = {
+  login: '로그인',
+  register: '회원가입',
+}
+
+const AuthForm = ({type}) => {
+  const text = textMap[type];
+
   return (
     <AuthFormBlock>
-      <h3>로그인</h3>
+      <h3>{text}</h3>
       <form>
         <StyledInput autoComplete="username" name="username" placeholder="아이디"/>
         <StyledInput autoComplete="new-password" name="password" placeholder="비밀번호" type="password"/>
-        <ButtonWitMarginTop cyan fullWidth>로그인</ButtonWitMarginTop>
+        {type === 'register' && (
+          <StyledInput
+            autoComplete="new-password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            type="password"/>
+        )}
+        <ButtonWitMarginTop cyan fullWidth>{text}</ButtonWitMarginTop>
       </form>
       <Footer>
-        <Link to="/register">회원가입</Link>
+        {type === 'login' ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
       </Footer>
     </AuthFormBlock>
   )
