ae66f164c95d155c82a0064e1d458c092ea77fd5
shyoon <dbstnsgh2@naver.com>
2022-05-02 21:58:01 +0900

    ch25 - 포스트 작성 기능 추가

---
+++ blog/blog-frontend/src/containers/write/WriteActionButtonsContainer.js
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import WriteActionButtons from '../../components/write/WriteActionButtons'
+import { useSelector, useDispatch } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { writePost } from '../../modules/write'
+
+const WriteActionButtonsContainer = ({ history }) => {
+  const dispatch = useDispatch()
+  const { title, body, tags, post, postError } = useSelector(
+    ({ write }) => ({
+      title: write.title,
+      body: write.body,
+      tags: write.tags,
+      post: write.post,
+      postError: write.postError,
+      originalPostId: write.originalPostId,
+    }),
+  )
+
+  // 포스트 등록
+  const onPublish = () => {
+    dispatch(
+      writePost({
+        title,
+        body,
+        tags,
+      }),
+    )
+  }
+
+  // 취소
+  const onCancel = () => {
+    history.goBack()
+  }
+
+  // 성공 / 실패 시 작업
+  useEffect(() => {
+    if (post) {
+      const { _id, user } = post
+      history.push(`/@${user.username}/${_id}`)
+    }
+    if (postError) {
+      console.error(postError)
+    }
+  }, [history, post, postError])
+
+  return (
+    <WriteActionButtons
+      onPublish={onPublish}
+      onCancel={onCancel}
+    />
+  )
+}
+
+export default withRouter(WriteActionButtonsContainer)
