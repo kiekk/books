8885e73dc02e7fae2bfce3918c629184b35e749d
soonho <dbstnsgh2@naver.com>
2022-05-05 23:48:33 +0900

    ch27 - 기존 포스트 정보 기억할 수 있도록 write 모듈에 action 추가

--- blog/blog-frontend/src/modules/write.js
+++ blog/blog-frontend/src/modules/write.js
@@ -1,63 +1,73 @@
-import { createAction, handleActions } from 'redux-actions'
+import {createAction, handleActions} from 'redux-actions'
 import createRequestSaga, {
-  createRequestActionTypes,
+    createRequestActionTypes,
 } from '../lib/createRequestSaga'
 import * as postsAPI from '../lib/api/posts'
-import { takeLatest } from 'redux-saga/effects'
+import {takeLatest} from 'redux-saga/effects'
 
 const INITIALIZE = 'write/INITIALIZE';
 const CHANGE_FIELD = 'write/CHANGE_FIELD';
 const [WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_FAILURE] =
-  createRequestActionTypes('write/WRITE_POST') // 포스트 작성
+    createRequestActionTypes('write/WRITE_POST') // 포스트 작성
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST'
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({key, value}) => ({
-  key,
-  value,
+    key,
+    value,
 }))
-export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
-  title,
-  body,
-  tags,
+export const writePost = createAction(WRITE_POST, ({title, body, tags}) => ({
+    title,
+    body,
+    tags,
 }))
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post)
 
 // 사가 생성
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost)
 
 export function* writeSaga() {
-  yield takeLatest(WRITE_POST, writePostSaga)
+    yield takeLatest(WRITE_POST, writePostSaga)
 }
 
 const initialState = {
-  title: '',
-  body: '',
-  tags: [],
-  post: null,
-  postError: null
+    title: '',
+    body: '',
+    tags: [],
+    post: null,
+    postError: null,
+    originalPostId: null,
 }
 
 const write = handleActions(
-  {
-    [INITIALIZE]: state => initialState,
-    [CHANGE_FIELD]: (state, {payload: {key, value}}) => ({
-      ...state,
-      [key]: value
-    }),
-    [WRITE_POST]: (state) => ({
-      ...state,
-      post: null,
-      postError: null,
-    }),
-    [WRITE_POST_SUCCESS]: (state, { payload: post }) => ({
-      ...state,
-      post,
-    }),
-    [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
-      ...state,
-      postError,
-    }),
-  },
-  initialState
+    {
+        [INITIALIZE]: state => initialState,
+        [CHANGE_FIELD]: (state, {payload: {key, value}}) => ({
+            ...state,
+            [key]: value
+        }),
+        [WRITE_POST]: (state) => ({
+            ...state,
+            post: null,
+            postError: null,
+        }),
+        [WRITE_POST_SUCCESS]: (state, {payload: post}) => ({
+            ...state,
+            post,
+        }),
+        [WRITE_POST_FAILURE]: (state, {payload: postError}) => ({
+            ...state,
+            postError,
+        }),
+        [SET_ORIGINAL_POST]: (state, {payload: post }) => ({
+            ...state,
+            title: post.title,
+            body: post.body,
+            tags: post.tags,
+            originalPostId: post._id
+        })
+    },
+    initialState
 )
 
 export default write;
