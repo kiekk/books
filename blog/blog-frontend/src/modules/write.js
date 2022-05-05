25da08afc5ce601f59b84be6b6346dbd7f2370f7
soonho <dbstnsgh2@naver.com>
2022-05-06 06:42:31 +0900

    ch27 - update action, saga 추가

--- blog/blog-frontend/src/modules/write.js
+++ blog/blog-frontend/src/modules/write.js
@@ -10,6 +10,7 @@ const CHANGE_FIELD = 'write/CHANGE_FIELD';
 const [WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_FAILURE] =
     createRequestActionTypes('write/WRITE_POST') // 포스트 작성
 const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST'
+const [UPDATE_POST, UPDATE_POST_SUCCESS, UPDATE_POST_FAILURE] = createRequestActionTypes('write/UPDATE_POST')
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({key, value}) => ({
@@ -22,12 +23,22 @@ export const writePost = createAction(WRITE_POST, ({title, body, tags}) => ({
     tags,
 }))
 export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post)
+export const updatePost = createAction(UPDATE_POST,
+    ({ id, title, body, tags}) => ({
+        id,
+        title,
+        body,
+        tags
+    })
+)
 
 // 사가 생성
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost)
+const updatePostSaga = createRequestSaga(UPDATE_POST, postsAPI.updatePost)
 
 export function* writeSaga() {
     yield takeLatest(WRITE_POST, writePostSaga)
+    yield takeLatest(UPDATE_POST, updatePostSaga)
 }
 
 const initialState = {
@@ -65,6 +76,14 @@ const write = handleActions(
             body: post.body,
             tags: post.tags,
             originalPostId: post._id
+        }),
+        [UPDATE_POST_SUCCESS]: (state, {payload: post}) => ({
+            ...state,
+            post
+        }),
+        [UPDATE_POST_FAILURE]: (state, {payload: postError}) => ({
+            ...state,
+            postError
         })
     },
     initialState
