570ccd4e9ecb44b63e70ae9e289df9df268a86df
soonho <dbstnsgh2@naver.com>
2022-05-08 19:06:02 +0900

    edit entity - Item
    ##
    jpa setting

--- src/main/java/com/shop/shoppingmall/entity/Item.java
+++ src/main/java/com/shop/shoppingmall/entity/Item.java
@@ -5,20 +5,39 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "item")
 @Getter
 @Setter
 @ToString
 public class Item {
 
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false, length = 50)
     private String name;
+
+    @Column(name = "price", nullable = false)
     private int price;
+
+    @Column(nullable = false)
     private String stockNumber;
+
+    @Lob
+    @Column(nullable = false)
     private String detail;
+
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus sellStatus;
+
     private LocalDateTime regTime;
+
     private LocalDateTime updateTime;
 
 }
