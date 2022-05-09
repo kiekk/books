4e94865dc04c5ca9b3cd1ee3b586d7aefe4da440
soonho <dbstnsgh2@naver.com>
2022-05-09 20:01:30 +0900

    security - add service MemberService

---
+++ src/main/java/com/shop/shoppingmall/service/MemberService.java
@@ -0,0 +1,29 @@
+package com.shop.shoppingmall.service;
+
+import com.shop.shoppingmall.entity.Member;
+import com.shop.shoppingmall.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+
+        if (findMember != null) {
+            throw new IllegalArgumentException("이미 가입된 회원입니다.");
+        }
+    }
+}
