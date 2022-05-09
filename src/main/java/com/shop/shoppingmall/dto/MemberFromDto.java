0ce0f0f9f549628ce879e131acb5cc948dccfc7f
soonho <dbstnsgh2@naver.com>
2022-05-09 19:55:27 +0900

    security - add dto MemberFromDto

---
+++ src/main/java/com/shop/shoppingmall/dto/MemberFromDto.java
@@ -0,0 +1,15 @@
+package com.shop.shoppingmall.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberFromDto {
+
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    
+}
