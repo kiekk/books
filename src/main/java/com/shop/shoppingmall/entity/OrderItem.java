5638470e4264add9843acb3a06ca6cb43fd88ed3
soonho <dbstnsgh2@naver.com>
2022-05-10 23:05:01 +0900

    jpa - add entity OrderItem & @ManyToOne setting

---
+++ src/main/java/com/shop/shoppingmall/entity/OrderItem.java
@@ -0,0 +1,35 @@
+package com.shop.shoppingmall.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ordre_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    
+    private int count;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+}
