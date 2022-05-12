41c840db7261e6f406506226893b8ba2c92a59ab
soonho <dbstnsgh2@naver.com>
2022-05-12 21:37:00 +0900

    item-register: add repository ItemRepository

---
+++ src/main/java/com/shop/shoppingmall/repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package com.shop.shoppingmall.repository;
+
+import com.shop.shoppingmall.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+}
