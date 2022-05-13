3d98ee9c24e57c24bf2fd1cb52bd8f86c6b01c09
soonho <dbstnsgh2@naver.com>
2022-05-14 07:05:47 +0900

    item-update : add GetMapping "/admin/item/{itemId}" to ItemController

--- src/main/java/com/shop/shoppingmall/controller/ItemController.java
+++ src/main/java/com/shop/shoppingmall/controller/ItemController.java
@@ -6,12 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -49,6 +47,20 @@ public class ItemController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/admin/item/{itemId}")
+    public String itemDtl(@PathVariable("itemId") Long itemId, Model model) {
+        try {
+            ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+            model.addAttribute("itemFormDto", itemFormDto);
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("errorMessage", "존재하지 않는 상품입니다.");
+            model.addAttribute("itemFormDto", new ItemFormDto());
+            return "item/itemForm";
+        }
+
+        return "item/itemForm";
     }
 
 }
