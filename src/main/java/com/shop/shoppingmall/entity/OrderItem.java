341f3fa33c340e222a5c348052af6d874d347036
soonho <dbstnsgh2@naver.com>
2022-05-15 22:29:01 +0900

    order: add createOrderItem, getTotalPrice to OrderItem

--- src/main/java/com/shop/shoppingmall/entity/OrderItem.java
+++ src/main/java/com/shop/shoppingmall/entity/OrderItem.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -27,5 +26,19 @@ public class OrderItem extends BaseEntity {
     private int orderPrice;
     
     private int count;
+
+    public static OrderItem createOrderItem(Item item, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setCount(count);
+        orderItem.setOrderPrice(item.getPrice());
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
 
 }
