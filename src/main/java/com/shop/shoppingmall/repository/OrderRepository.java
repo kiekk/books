473d8ed049e4a007435a8cfad9bda04270ddff0d
soonho <dbstnsgh2@naver.com>
2022-05-16 06:58:01 +0900

    order: change import Pagable

--- src/main/java/com/shop/shoppingmall/repository/OrderRepository.java
+++ src/main/java/com/shop/shoppingmall/repository/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.shop.shoppingmall.repository;
 
 import com.shop.shoppingmall.entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
