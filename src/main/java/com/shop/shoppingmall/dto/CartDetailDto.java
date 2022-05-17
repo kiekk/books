91155fc4cab240e2516b2af3b49d0d2d35dc4105
soonho <dbstnsgh2@naver.com>
2022-05-18 06:10:53 +0900

    cart: add dto CartItemDto

---
+++ src/main/java/com/shop/shoppingmall/dto/CartDetailDto.java
@@ -0,0 +1,28 @@
+package com.shop.shoppingmall.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartDetailDto {
+
+    private Long cartItemId;
+
+    private String itemName;
+
+    private int price;
+
+    private int count;
+
+    private String imageUrl;
+
+    public CartDetailDto(Long cartItemId, String itemName, int price, int count, String imageUrl) {
+        this.cartItemId = cartItemId;
+        this.itemName = itemName;
+        this.price = price;
+        this.count = count;
+        this.imageUrl = imageUrl;
+    }
+
+}
