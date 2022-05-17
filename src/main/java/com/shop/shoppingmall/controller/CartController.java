86d89ded3f7239b754b70ef8cec545a647d9ad99
soonho <dbstnsgh2@naver.com>
2022-05-18 06:15:41 +0900

    cart: add request mapping '/cart' to Cartcontroller

--- src/main/java/com/shop/shoppingmall/controller/CartController.java
+++ src/main/java/com/shop/shoppingmall/controller/CartController.java
@@ -1,13 +1,16 @@
 package com.shop.shoppingmall.controller;
 
+import com.shop.shoppingmall.dto.CartDetailDto;
 import com.shop.shoppingmall.dto.CartItemDto;
 import com.shop.shoppingmall.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,6 +50,14 @@ public class CartController {
         }
 
         return new ResponseEntity<>(cartItemId, HttpStatus.OK);
+    }
+
+    @GetMapping("/cart")
+    public String orderHist(Principal principal, Model model) {
+        List<CartDetailDto> cartDetailDtoList = cartService.getCartList(principal.getName());
+
+        model.addAttribute("cartItems", cartDetailDtoList);
+        return "cart/cartList";
     }
 
 }
