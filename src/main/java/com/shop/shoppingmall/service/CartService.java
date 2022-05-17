eec0eb17550da7b616d23ed3c0b70188534f4e6c
soonho <dbstnsgh2@naver.com>
2022-05-18 06:39:07 +0900

    cart: add orderCartItem to CartService

--- src/main/java/com/shop/shoppingmall/service/CartService.java
+++ src/main/java/com/shop/shoppingmall/service/CartService.java
@@ -2,6 +2,8 @@ package com.shop.shoppingmall.service;
 
 import com.shop.shoppingmall.dto.CartDetailDto;
 import com.shop.shoppingmall.dto.CartItemDto;
+import com.shop.shoppingmall.dto.CartOrderDto;
+import com.shop.shoppingmall.dto.OrderDto;
 import com.shop.shoppingmall.entity.Cart;
 import com.shop.shoppingmall.entity.CartItem;
 import com.shop.shoppingmall.entity.Item;
@@ -29,6 +31,7 @@ public class CartService {
     private final MemberRepository memberRepository;
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final OrderService orderService;
 
     public Long addCart(CartItemDto cartItemDto, String email) {
         Item item = itemRepository.findById(cartItemDto.getItemId()).orElseThrow(EntityExistsException::new);
@@ -87,5 +90,26 @@ public class CartService {
     public void deleteCartItem(Long cartItemId) {
         CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
         cartItemRepository.delete(cartItem);
+    }
+
+    public Long orderCartItem(List<CartOrderDto> cartOrderDtoList, String email) {
+        List<OrderDto> orderDtoList = new ArrayList<>();
+
+        for (CartOrderDto cartOrderDto : cartOrderDtoList) {
+            CartItem cartItem = cartItemRepository.findById(cartOrderDto.getCartItemId()).orElseThrow(EntityNotFoundException::new);
+            OrderDto orderDto = new OrderDto();
+            orderDto.setItemId(cartItem.getItem().getId());
+            orderDto.setCount(cartItem.getCount());
+            orderDtoList.add(orderDto);
+        }
+
+        Long orderId = orderService.orders(orderDtoList, email);
+
+        for (CartOrderDto cartOrderDto : cartOrderDtoList) {
+            CartItem cartItem = cartItemRepository.findById(cartOrderDto.getCartItemId()).orElseThrow(EntityNotFoundException::new);
+            cartItemRepository.delete(cartItem);
+        }
+
+        return orderId;
     }
 }
