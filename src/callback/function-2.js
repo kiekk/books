4b2659e97a886ffc2c9c4d7cd29eece3253d0078
soonho <dbstnsgh2@naver.com>
2022-06-01 22:48:43 +0900

    callback 함수 재사용

--- src/callback/function-2.js
+++ src/callback/function-2.js
@@ -13,4 +13,17 @@ var obj1 = {
 }
 
 var callback = obj1.func()
-setTimeout(callback, 1000)
+setTimeout(callback, 1000)  // obj1
+
+var obj2 = {
+  name: 'obj2',
+  func: obj1.func
+}
+
+
+var callback2 = obj2.func()
+setTimeout(callback2, 1500) // obj2
+
+var obj3 = {name: 'obj3'}
+var callback3 = obj1.func.call(obj3)
+setTimeout(callback3, 2000) // obj3
