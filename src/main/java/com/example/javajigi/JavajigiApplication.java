b8796cdbb3306951cbb4ad75e11ab19676fa8acb
soonho <dbstnsgh2@naver.com>
2022-06-05 19:04:57 +0900

    Initial Commit

---
+++ src/main/java/com/example/javajigi/JavajigiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javajigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajigiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavajigiApplication.class, args);
+    }
+
+}
