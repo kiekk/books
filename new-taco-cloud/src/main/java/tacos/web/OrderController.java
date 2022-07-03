ba5414fed39a7b55ffba51baca7964b808f942c0
soonho <dbstnsgh2@naver.com>
2022-07-03 20:37:46 +0900

    taco-cloud: 폼 제출 처리

--- new-taco-cloud/src/main/java/tacos/web/OrderController.java
+++ new-taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -3,7 +3,9 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.domain.Order;
 
@@ -16,6 +18,16 @@ public class OrderController {
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 
 }
