f23b2db103962d7674884582af852cc7ddc993aa
soonho <dbstnsgh2@naver.com>
2022-07-03 19:07:19 +0900

    taco-cloud: private -> public

--- new-taco-cloud/src/main/java/tacos/domain/Ingredient.java
+++ new-taco-cloud/src/main/java/tacos/domain/Ingredient.java
@@ -11,7 +11,7 @@ public class Ingredient {
     private final String name;
     private final Type type;
 
-    private static enum Type {
+    public static enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 }
