18929bb10351a06f4e26f4523d867b26cd7b007e
soonho <dbstnsgh2@naver.com>
2022-07-09 22:13:08 +0900

    taco-cloud: add JdbcOrderRepository to OrderController

--- new-taco-cloud/src/main/java/tacos/web/OrderController.java
+++ new-taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,5 +1,6 @@
 package tacos.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,28 +8,37 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import tacos.domain.Order;
+import tacos.repository.OrderRepository;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderRepository orderRepository;
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order,
+                               Errors errors,
+                               SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: " + order);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
         return "redirect:/";
     }
 
