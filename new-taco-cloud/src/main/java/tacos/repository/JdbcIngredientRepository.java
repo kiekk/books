b37b9232f3e7e9a1f28976f722409a5424cb99ec
soonho <dbstnsgh2@naver.com>
2022-07-09 14:50:37 +0900

    taco-cloud: add repository IngredientRepository

---
+++ new-taco-cloud/src/main/java/tacos/repository/JdbcIngredientRepository.java
@@ -0,0 +1,17 @@
+package tacos.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcIngredientRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+}
