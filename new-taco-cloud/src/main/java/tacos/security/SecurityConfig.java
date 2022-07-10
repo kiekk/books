73a9fd2367dfe66b86f96a4729a65b62ef311bea
soonho <dbstnsgh2@naver.com>
2022-07-10 16:11:54 +0900

    taco-cloud: inmemory에서 jdbcauthentication으로 변경

--- new-taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ new-taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,11 +1,18 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,13 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("{noop}1234")
-                .roles("USER")
-                .and()
-                .withUser("user2")
-                .password("{noop}1234")
-                .roles("USER");
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource);
     }
 }
