fad02c17dff36997632c0d1481cc18c499b2c536
soonho <dbstnsgh2@naver.com>
2022-07-10 17:43:21 +0900

    taco-cloud: user entity @Table annotation 추가 - user가 예약어라 table 생성시 오류 발생

--- new-taco-cloud/src/main/java/tacos/domain/User.java
+++ new-taco-cloud/src/main/java/tacos/domain/User.java
@@ -8,10 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -19,6 +16,7 @@ import java.util.Collection;
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @RequiredArgsConstructor
+@Table(name = "USERS")
 public class User implements UserDetails {
     private static final long serialVersionUID = 1L;
 
