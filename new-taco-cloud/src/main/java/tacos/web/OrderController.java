d78c3da69739b371e857cf7d31af434e9ca9e05e
soonho <dbstnsgh2@naver.com>
2022-07-10 17:46:38 +0900

    taco-cloud: 주문시 회원 정보 저장

--- new-taco-cloud/src/main/java/tacos/web/OrderController.java
+++ new-taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.domain.Order;
+import tacos.domain.User;
 import tacos.repository.OrderRepository;
 
 import javax.validation.Valid;
@@ -32,10 +34,13 @@ public class OrderController {
     @PostMapping
     public String processOrder(@Valid Order order,
                                Errors errors,
-                               SessionStatus sessionStatus) {
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+
+        order.setUser(user);
 
         orderRepository.save(order);
         sessionStatus.setComplete();
