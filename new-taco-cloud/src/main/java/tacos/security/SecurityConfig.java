28f702db785691b637c35ea6ecffe2daf98e16c8
soonho <dbstnsgh2@naver.com>
2022-07-10 15:57:33 +0900

    taco-cloud: add config security

--- new-taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ new-taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,9 +1,31 @@
 package tacos.security;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/order").hasRole("USER")
+                .antMatchers("/**").permitAll()
+                .and()
+                .httpBasic();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("{noop}1234")
+                .roles("USER")
+                .and()
+                .withUser("user2")
+                .password("{noop}1234")
+                .roles("USER");
+    }
 }
