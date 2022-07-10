a3615de93d4dbfbcf83019fa8b65bd5681afd794
soonho <dbstnsgh2@naver.com>
2022-07-10 15:55:11 +0900

    taco-cloud: security 버전 호환성은 여기까지/ 앞으로의 예제 진행을 위해 버전 다운

--- new-taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ new-taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,58 +1,9 @@
 package tacos.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-import javax.sql.DataSource;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz.antMatchers("/design", "/orders").hasRole("USER")
-                        .antMatchers("/**").permitAll()).httpBasic(withDefaults());
-        return http.build();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-                .build();
-    }
-
-    @Bean
-    public UserDetailsManager users(DataSource dataSource) {
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("user1")
-                .password("1234")
-                .roles("USER")
-                .build();
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-                .username("user2")
-                .password("1234")
-                .roles("USER")
-                .build();
-        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-//        users.createUser(user1);
-//        users.createUser(user2);
-        return users;
-    }
 
 }
