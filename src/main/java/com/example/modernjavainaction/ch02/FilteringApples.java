2c2674204b228e18751b8a91511d218b1e0afcff
soonho <dbstnsgh2@naver.com>
2022-07-27 01:33:00 +0900

    ch02 - 목록 필터링 - 1 (필터 색상 parameter로 받기)

---
+++ src/main/java/com/example/modernjavainaction/ch02/FilteringApples.java
@@ -0,0 +1,58 @@
+package com.example.modernjavainaction.ch02;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FilteringApples {
+
+    public static void main(String... args) {
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, Color.GREEN),
+                new Apple(155, Color.GREEN),
+                new Apple(120, Color.RED)
+        );
+
+        // [FilteringApples.Apple(weight=80, color=GREEN), FilteringApples.Apple(weight=155, color=GREEN)]
+        List<Apple> greenApples = filterApplesByColor(inventory, Color.GREEN);
+        System.out.println(greenApples);
+
+        // [Apple{color=RED, weight=120}]
+        List<Apple> redApples = filterApplesByColor(inventory, Color.RED);
+        System.out.println(redApples);
+    }
+
+
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor() == color) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    enum Color {
+        RED,
+        GREEN
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    @AllArgsConstructor
+    public static class Apple {
+
+        private int weight;
+        private Color color;
+
+    }
+
+}
