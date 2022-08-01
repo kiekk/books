20c932bd15703d18af5cb38d12c76c524ef0cc7e
soonho <dbstnsgh2@naver.com>
2022-08-02 06:38:21 +0900

    ch03 - sorting 예제 - 1 (comparator 직접 구현)

---
+++ src/main/java/com/example/modernjavainaction/ch03/Sorting.java
@@ -0,0 +1,29 @@
+package com.example.modernjavainaction.ch03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Sorting {
+    public static void main(String[] args) {
+        List<Apple> inventory = new ArrayList<>(Arrays.asList(
+                new Apple(80, Color.GREEN),
+                new Apple(155, Color.GREEN),
+                new Apple(120, Color.RED)
+        ));
+
+        // [Apple{color=GREEN, weight=80}, Apple{color=RED, weight=120}, Apple{color=GREEN, weight=155}]
+        inventory.sort(new AppleComparator());
+        System.out.println(inventory);
+    }
+
+    static class AppleComparator implements Comparator<Apple> {
+
+        @Override
+        public int compare(Apple a1, Apple a2) {
+            return a1.getWeight() - a2.getWeight();
+        }
+
+    }
+}
