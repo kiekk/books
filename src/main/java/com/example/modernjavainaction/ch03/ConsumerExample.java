08fb8f6db3d4d7f544c091025aa7087b7a484282
soonho <dbstnsgh2@naver.com>
2022-08-02 06:56:00 +0900

    ch03 - Consumer 예제 - 1

---
+++ src/main/java/com/example/modernjavainaction/ch03/ConsumerExample.java
@@ -0,0 +1,21 @@
+package com.example.modernjavainaction.ch03;
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        forEach(Arrays.asList(1, 2, 3, 4, 5), i -> System.out.println(i));
+
+    }
+
+    public static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T t : list) {
+            c.accept(t);
+        }
+    }
+}
