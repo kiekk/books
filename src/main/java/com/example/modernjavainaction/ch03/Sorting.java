6efcb31439dd5b58c05075736a93221827248c46
soonho <dbstnsgh2@naver.com>
2022-08-02 06:39:15 +0900

    ch03 - sorting 예제 - 2 (comparator 익명 클래스로 구현)

--- src/main/java/com/example/modernjavainaction/ch03/Sorting.java
+++ src/main/java/com/example/modernjavainaction/ch03/Sorting.java
@@ -16,6 +16,16 @@ public class Sorting {
         // [Apple{color=GREEN, weight=80}, Apple{color=RED, weight=120}, Apple{color=GREEN, weight=155}]
         inventory.sort(new AppleComparator());
         System.out.println(inventory);
+
+        // [Apple{color=GREEN, weight=80}, Apple{color=RED, weight=120}, Apple{color=GREEN, weight=155}]
+        inventory.sort(new Comparator<Apple>() {
+
+            @Override
+            public int compare(Apple a1, Apple a2) {
+                return a1.getWeight() - a2.getWeight();
+            }
+        });
+        System.out.println(inventory);
     }
 
     static class AppleComparator implements Comparator<Apple> {
