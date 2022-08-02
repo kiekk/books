ed8afef2159ff846e2873b88f3fcc7838bb63479
soonho <dbstnsgh2@naver.com>
2022-08-03 06:55:55 +0900

    ch03 - Comparator 예제 - 2 (Comparator 연결)

--- src/main/java/com/example/modernjavainaction/ch03/ComparatorExample.java
+++ src/main/java/com/example/modernjavainaction/ch03/ComparatorExample.java
@@ -10,18 +10,32 @@ public class ComparatorExample {
     public static void main(String[] args) {
         List<Apple> inventory = new ArrayList<>(Arrays.asList(
                 new Apple(80, Color.GREEN),
-                new Apple(155, Color.GREEN),
+                new Apple(120, Color.GREEN),
                 new Apple(120, Color.RED)
         ));
 
-        System.out.println("before");
-        System.out.println(inventory);
+//        System.out.println("before - 1");
+//        System.out.println(inventory);
         // [Apple{color=GREEN, weight=80}, Apple{color=GREEN, weight=155}, Apple{color=RED, weight=120}]
 
-        inventory.sort(Comparator.comparing(Apple::getWeight).reversed());
+//        inventory.sort(Comparator.comparing(Apple::getWeight).reversed());
 
-        System.out.println("after");
-        System.out.println(inventory);
+//        System.out.println("after - 1");
+//        System.out.println(inventory);
         // [Apple{color=GREEN, weight=155}, Apple{color=RED, weight=120}, Apple{color=GREEN, weight=80}]
+
+
+        System.out.println("before - 2");
+        System.out.println(inventory);
+        // [Apple{color=GREEN, weight=80}, Apple{color=GREEN, weight=120}, Apple{color=RED, weight=120}]
+
+        inventory.sort(
+                Comparator.comparing(Apple::getWeight)
+                        .reversed()
+                        .thenComparing(Apple::getColor));
+
+        System.out.println("after - 2");
+        System.out.println(inventory);
+        // [Apple{color=RED, weight=120}, Apple{color=GREEN, weight=120}, Apple{color=GREEN, weight=80}]
     }
 }
