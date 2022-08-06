a8e53700699c1a4b489d44e85241e16fe8bb30d3
soonho <dbstnsgh2@naver.com>
2022-08-06 10:07:45 +0900

    ch05 - map 예제 - 3 (문자열 컬렉션에서의 map 사용 에러 flatMap 으로 해결)

--- src/main/java/com/example/modernjavainaction/ch05/MappingExample.java
+++ src/main/java/com/example/modernjavainaction/ch05/MappingExample.java
@@ -32,5 +32,25 @@ public class MappingExample {
 
         // distinct 를 사용한 의도는 중복되는 알파벳이 제거되어 출력되는 것인데, map 을 사용하면 의도한 결과가 출력되지 않음
         // 이는 map 에서 split 을 하게 될 경우 반환 타입이 String<String[]> 이기 때문이다.
+
+        // flatMap
+        /*
+            H
+            e
+            l
+            o
+            W
+            r
+            d
+         */
+        words.stream()
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .forEach(System.out::println);
+
+        // map 에서 split 을 처리한 뒤 다시 flatMap 을 사용하여 다시 stream 을 만들면 반환 타입이 Stream<String> 이 된다.
+        // 따라서 이 후 distinct 에서 의도한대로 중복 알파벳이 제거된다.
+        // flatMap 대신 map(Arrays::stream) 을 사용하게 되면 반환 타입이 Stream<Stream<String>> 이 되기 때문에 의도한대로 결과가 출력되지 않는다.
     }
 }
