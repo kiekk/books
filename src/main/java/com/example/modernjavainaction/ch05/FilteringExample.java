4a2a93a733150312016204f4e3213eb4b0061614
soonho <dbstnsgh2@naver.com>
2022-08-06 09:21:54 +0900

    ch05 - filtering 예제 - 2 (distinct 로 중복 요소 제거)

--- src/main/java/com/example/modernjavainaction/ch05/FilteringExample.java
+++ src/main/java/com/example/modernjavainaction/ch05/FilteringExample.java
@@ -2,6 +2,7 @@ package com.example.modernjavainaction.ch05;
 
 import com.example.modernjavainaction.ch04.Dish;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -21,6 +22,14 @@ public class FilteringExample {
                 .filter(Dish::isVegetarian)
                 .collect(toList());
         vegetarianMenu.forEach(System.out::println);
+
+        // distinct 로 중복 요소 제거
+        System.out.println("Filtering unique elements:");
+        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+        numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
 
     }
 }
