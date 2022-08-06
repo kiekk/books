21bf99aa3015dd90d8a1c21ffd869a6fa4f84486
soonho <dbstnsgh2@naver.com>
2022-08-06 10:03:51 +0900

    ch05 - map 예제 - 2 (문자열 컬렉션에서의 map 사용 에러)

--- src/main/java/com/example/modernjavainaction/ch05/MappingExample.java
+++ src/main/java/com/example/modernjavainaction/ch05/MappingExample.java
@@ -20,6 +20,17 @@ public class MappingExample {
                 .collect(toList());
         System.out.println(wordLengths);
 
+        // map
+        /*
+            [H, e, l, l, o]
+            [W, o, r, l, d]
+         */
+        words.stream()
+                .map(word -> word.split(""))
+                .distinct()
+                .forEach(x -> System.out.println(Arrays.toString(x)));
 
+        // distinct 를 사용한 의도는 중복되는 알파벳이 제거되어 출력되는 것인데, map 을 사용하면 의도한 결과가 출력되지 않음
+        // 이는 map 에서 split 을 하게 될 경우 반환 타입이 String<String[]> 이기 때문이다.
     }
 }
