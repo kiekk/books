817ac8e66942b8df18780cff65dd4c1fede00059
soonho <dbstnsgh2@naver.com>
2022-08-08 09:49:55 +0900

    ch05 - 기본형 특화 스트림 - boxed (특화 스트림을 일반 스트림으로 변환)

--- src/main/java/com/example/modernjavainaction/ch05/NumericStreamExample.java
+++ src/main/java/com/example/modernjavainaction/ch05/NumericStreamExample.java
@@ -1,5 +1,8 @@
 package com.example.modernjavainaction.ch05;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class NumericStreamExample {
 
     public static void main(String[] args) {
@@ -11,5 +14,9 @@ public class NumericStreamExample {
         int result2 = Dish.menu.stream().mapToInt(Dish::getCalories).sum();
         // 4300
         System.out.println(result2);
+
+        // boxed: 특화 스트림을 일반 스트림으로 변환
+        IntStream intStream = Dish.menu.stream().mapToInt(Dish::getCalories);
+        Stream<Integer> boxed = intStream.boxed();
     }
 }
