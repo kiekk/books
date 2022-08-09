6cf39147c2b4fd33c411d51f544ab6e926323d64
soonho <dbstnsgh2@naver.com>
2022-08-10 06:08:26 +0900

    ch05 - 스트림 예제 - Stream.of

---
+++ src/main/java/com/example/modernjavainaction/ch05/BuildingStreamExample.java
@@ -0,0 +1,11 @@
+package com.example.modernjavainaction.ch05;
+
+import java.util.stream.Stream;
+
+public class BuildingStreamExample {
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
+        stream.map(String::toUpperCase).forEach(System.out::println);
+    }
+}
