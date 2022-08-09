12694930b9f00cb04747eb42208df2bee78a710a
soonho <dbstnsgh2@naver.com>
2022-08-10 06:20:14 +0900

    ch05 - 스트림 예제 - Arrays.stream (파일로 스트림 만들기)

--- src/main/java/com/example/modernjavainaction/ch05/BuildingStreamExample.java
+++ src/main/java/com/example/modernjavainaction/ch05/BuildingStreamExample.java
@@ -1,5 +1,8 @@
 package com.example.modernjavainaction.ch05;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
@@ -17,5 +20,17 @@ public class BuildingStreamExample {
 
         int[] numbers = { 2, 3, 5, 7, 11, 13 };
         System.out.println(Arrays.stream(numbers).sum());
+
+        try {
+            long uniqueWords = Files.lines(Paths.get("C:\\study\\modern-java-in-action\\src\\main\\resources\\modernjavainaction\\chap05\\data.txt"), Charset.defaultCharset())
+                    .flatMap(line -> Arrays.stream(line.split(" ")))
+                    .distinct()
+                    .count();
+
+            System.out.println("There are " + uniqueWords + " unique words in data.txt");
+        } catch (Exception e) {
+            System.out.println("File Exception");
+        }
+        // Stream 은 AutoCloseable 인터페이스르 구현했기 때문에 자원을 개발자가 직접 닫아줄 필요가 없다.
     }
 }
