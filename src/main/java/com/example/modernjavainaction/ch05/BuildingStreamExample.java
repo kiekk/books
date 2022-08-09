225686193525e564dd9273cde7941d6516ab8bd8
soonho <dbstnsgh2@naver.com>
2022-08-10 06:11:26 +0900

    ch05 - 스트림 예제 - Stream.empty

--- src/main/java/com/example/modernjavainaction/ch05/BuildingStreamExample.java
+++ src/main/java/com/example/modernjavainaction/ch05/BuildingStreamExample.java
@@ -7,5 +7,8 @@ public class BuildingStreamExample {
     public static void main(String[] args) {
         Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
         stream.map(String::toUpperCase).forEach(System.out::println);
+
+        Stream<String> emptyStream = Stream.empty();
+        emptyStream.forEach(System.out::println);
     }
 }
