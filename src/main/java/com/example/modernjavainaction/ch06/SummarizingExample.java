c2294aa48569145e6e9b24bf33d07306935c0252
soonho <dbstnsgh2@naver.com>
2022-08-12 06:18:02 +0900

    ch06 - collect 에서의 요약 연산 - averagingInt / averagingLong / averagingDouble

--- src/main/java/com/example/modernjavainaction/ch06/SummarizingExample.java
+++ src/main/java/com/example/modernjavainaction/ch06/SummarizingExample.java
@@ -1,5 +1,6 @@
 package com.example.modernjavainaction.ch06;
 
+import static java.util.stream.Collectors.averagingInt;
 import static java.util.stream.Collectors.summingInt;
 
 public class SummarizingExample {
@@ -7,6 +8,8 @@ public class SummarizingExample {
     public static void main(String[] args) {
         // Total calories in menu: 4300
         System.out.println("Total calories in menu: " + calculateTotalCalories());
+        // Average calories in menu: 477.77777777777777
+        System.out.println("Average calories in menu: " + calculateAverageCalories());
     }
 
     private static int calculateTotalCalories() {
@@ -15,4 +18,8 @@ public class SummarizingExample {
 
     // summingInt 는 mapToInt 와 sum 으로 대체 가능
 //    private static int calculateTotalCalories() { return Dish.menu.stream().mapToInt(Dish::getCalories).sum(); }
+
+    private static Double calculateAverageCalories() {
+        return Dish.menu.stream().collect(averagingInt(Dish::getCalories));
+    }
 }
