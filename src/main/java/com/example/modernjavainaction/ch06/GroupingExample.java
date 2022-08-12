a61412c94a44f398c2b0d597b959c31bda1b13a2
soonho <dbstnsgh2@naver.com>
2022-08-13 01:33:00 +0900

    ch06 - Grouping 예제 - 4 (filter, filtering 차이)

--- src/main/java/com/example/modernjavainaction/ch06/GroupingExample.java
+++ src/main/java/com/example/modernjavainaction/ch06/GroupingExample.java
@@ -16,6 +16,11 @@ public class GroupingExample {
         System.out.println("Dish names grouped by type: " + groupDishNamesByType());
         // Dish tags grouped by type: {OTHER=[salty, greasy, natural, light, tasty, fresh, fried], FISH=[roasted, tasty, fresh, delicious], MEAT=[salty, greasy, roasted, fried, crisp]}
         System.out.println("Dish tags grouped by type: " + groupDishTagsByType());
+        // Caloric dishes grouped by type 1: {OTHER=[Dish(name=french fries), Dish(name=pizza)], MEAT=[Dish(name=pork), Dish(name=beef)]}
+        System.out.println("Caloric dishes grouped by type 1: " + groupCaloricDishesByType1());
+        // Caloric dishes grouped by type 2: {OTHER=[Dish(name=french fries), Dish(name=pizza)], FISH=[], MEAT=[Dish(name=pork), Dish(name=beef)]}
+        System.out.println("Caloric dishes grouped by type 2: " + groupCaloricDishesByType2());
+        // groupCaloricDishesByType1 과 같이 filter 를 먼저 하게 되면 FISH 타입 자체가 조회되지 않아 Grouping 대상이 되지 않는다.
     }
 
     private static Map<Dish.Type, List<Dish>> groupDishesByType() {
@@ -33,4 +38,15 @@ public class GroupingExample {
                 groupingBy(Dish::getType,
                         flatMapping(dish -> dishTags.get(dish.getName()).stream(), toSet())));
     }
+
+    private static Map<Dish.Type, List<Dish>> groupCaloricDishesByType1() {
+        return Dish.menu.stream().filter(dish -> dish.getCalories() > 500).collect(groupingBy(Dish::getType));
+    }
+
+    private static Map<Dish.Type, List<Dish>> groupCaloricDishesByType2() {
+        return Dish.menu.stream().collect(
+                groupingBy(Dish::getType,
+                        filtering(dish -> dish.getCalories() > 500, toList())));
+    }
+
 }
