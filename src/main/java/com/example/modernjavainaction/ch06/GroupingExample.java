fb46ee6b6e00948e34c44b607c67641f470bb951
soonho <dbstnsgh2@naver.com>
2022-08-14 07:22:55 +0900

    ch06 - Grouping 예제 - 2 (메뉴 이름만 그룹핑)

--- src/main/java/com/example/modernjavainaction/ch06/GroupingExample.java
+++ src/main/java/com/example/modernjavainaction/ch06/GroupingExample.java
@@ -3,16 +3,25 @@ package com.example.modernjavainaction.ch06;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.*;
 
 public class GroupingExample {
 
     public static void main(String[] args) {
         // Dishes grouped by type: {OTHER=[Dish(name=french fries), Dish(name=rice), Dish(name=season fruit), Dish(name=pizza)], MEAT=[Dish(name=pork), Dish(name=beef), Dish(name=chicken)], FISH=[Dish(name=prawns), Dish(name=salmon)]}
         System.out.println("Dishes grouped by type: " + groupDishesByType());
+        // Dish names grouped by type: {OTHER=[french fries, rice, season fruit, pizza], FISH=[prawns, salmon], MEAT=[pork, beef, chicken]}
+        System.out.println("Dish names grouped by type: " + groupDishNamesByType());
     }
 
     private static Map<Dish.Type, List<Dish>> groupDishesByType() {
         return Dish.menu.stream().collect(groupingBy(Dish::getType));
     }
+
+    private static Map<Dish.Type, List<String>> groupDishNamesByType() {
+        return Dish.menu.stream().collect(
+                groupingBy(Dish::getType,
+                        mapping(Dish::getName, toList())));
+    }
+
 }
