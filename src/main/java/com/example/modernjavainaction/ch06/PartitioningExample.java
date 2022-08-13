bab7d2eb3e474ce51a4ef148084f9faa2047cdb5
soonho <dbstnsgh2@naver.com>
2022-08-14 08:45:15 +0900

    ch06 - Partitioning 예제 - 2 (partitioningBy, groupingBy 사용)

--- src/main/java/com/example/modernjavainaction/ch06/PartitioningExample.java
+++ src/main/java/com/example/modernjavainaction/ch06/PartitioningExample.java
@@ -3,6 +3,7 @@ package com.example.modernjavainaction.ch06;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.partitioningBy;
 
 public class PartitioningExample {
@@ -10,9 +11,15 @@ public class PartitioningExample {
     public static void main(String[] args) {
         // Dishes partitioned by vegetarian: {false=[Dish(name=pork), Dish(name=beef), Dish(name=chicken), Dish(name=prawns), Dish(name=salmon)], true=[Dish(name=french fries), Dish(name=rice), Dish(name=season fruit), Dish(name=pizza)]}
         System.out.println("Dishes partitioned by vegetarian: " + partitionByVegetarian());
+        // Vegetarian Dishes by type: {false={MEAT=[Dish(name=pork), Dish(name=beef), Dish(name=chicken)], FISH=[Dish(name=prawns), Dish(name=salmon)]}, true={OTHER=[Dish(name=french fries), Dish(name=rice), Dish(name=season fruit), Dish(name=pizza)]}}
+        System.out.println("Vegetarian Dishes by type: " + vegetarianDishesByType());
     }
 
     private static Map<Boolean, List<Dish>> partitionByVegetarian() {
         return Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian));
+    }
+
+    private static Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType() {
+        return Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian, groupingBy(Dish::getType)));
     }
 }
