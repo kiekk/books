3fa79c0c741c6a4c49571c2f91169076d27da6b8
soonho <dbstnsgh2@naver.com>
2022-08-14 09:04:53 +0900

    ch06 - Partitioning 예제 - 3 (collectingAndThen 사용)

--- src/main/java/com/example/modernjavainaction/ch06/PartitioningExample.java
+++ src/main/java/com/example/modernjavainaction/ch06/PartitioningExample.java
@@ -2,9 +2,10 @@ package com.example.modernjavainaction.ch06;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.partitioningBy;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.*;
 
 public class PartitioningExample {
 
@@ -13,6 +14,8 @@ public class PartitioningExample {
         System.out.println("Dishes partitioned by vegetarian: " + partitionByVegetarian());
         // Vegetarian Dishes by type: {false={MEAT=[Dish(name=pork), Dish(name=beef), Dish(name=chicken)], FISH=[Dish(name=prawns), Dish(name=salmon)]}, true={OTHER=[Dish(name=french fries), Dish(name=rice), Dish(name=season fruit), Dish(name=pizza)]}}
         System.out.println("Vegetarian Dishes by type: " + vegetarianDishesByType());
+        // Most caloric dishes by vegetarian: {false=Dish(name=pork), true=Dish(name=pizza)}
+        System.out.println("Most caloric dishes by vegetarian: " + mostCaloricPartitionedByVegetarian());
     }
 
     private static Map<Boolean, List<Dish>> partitionByVegetarian() {
@@ -21,5 +24,13 @@ public class PartitioningExample {
 
     private static Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType() {
         return Dish.menu.stream().collect(partitioningBy(Dish::isVegetarian, groupingBy(Dish::getType)));
+    }
+
+    private static Object mostCaloricPartitionedByVegetarian() {
+        return Dish.menu.stream().collect(
+                partitioningBy(Dish::isVegetarian,
+                        collectingAndThen(
+                                maxBy(comparingInt(Dish::getCalories)),
+                                Optional::get)));
     }
 }
