377cac6d52ebe3f3d16491aed53e6bfcfcb55136
soonho <dbstnsgh2@naver.com>
2022-08-15 09:26:16 +0900

    ch07 - 병렬 스트림 예제 - 7 (태스크 개수에 따라 순차/병렬 스트림 처리)

--- src/main/java/com/example/modernjavainaction/ch07/ParallelStreamsHarness.java
+++ src/main/java/com/example/modernjavainaction/ch07/ParallelStreamsHarness.java
@@ -1,9 +1,12 @@
 package com.example.modernjavainaction.ch07;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class ParallelStreamsHarness {
+
+    public static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
 
     public static void main(String[] args) {
         System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreamsExample::iterativeSum, 10_000_000L) + " msecs");
@@ -14,6 +17,8 @@ public class ParallelStreamsHarness {
         System.out.println("SideEffect sum done in: " + measurePerf(ParallelStreamsExample::sideEffectSum, 10_000_000L) + " msecs");
         System.out.println("SideEffect parallel sum done in: " + measurePerf(ParallelStreamsExample::sideEffectParallelSum, 10_000_000L) + " msecs");
         // 병렬 스트림의 단점 : 다수의 스레드에서 동시에 데이터에 접근하는 경우 데이터 레이스 문제가 발생합니다.
+
+        System.out.println("ForkJoin sum done in: " + measurePerf(ForkJoinSumCalculator::forkJoinSum, 10_000_000L) + " msecs");
     }
 
     public static <T, R> long measurePerf(Function<T, R> f, T input) {
