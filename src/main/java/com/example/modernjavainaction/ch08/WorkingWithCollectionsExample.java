715ea2806bd6be1fbc2650fe1f1b01c29888b408
soonho <dbstnsgh2@naver.com>
2022-08-15 10:22:32 +0900

    ch08 - replaceAll 예제 - 1 (map 사용 - 새로운 컬렉션을 만듬)

---
+++ src/main/java/com/example/modernjavainaction/ch08/WorkingWithCollectionsExample.java
@@ -0,0 +1,34 @@
+package com.example.modernjavainaction.ch08;
+
+import static java.util.Map.entry;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class WorkingWithCollectionsExample {
+
+    public static void main(String[] args) {
+        workingWithLists();
+    }
+
+    private static void workingWithLists() {
+        System.out.println("------ Working with Lists ------");
+
+        System.out.println("--> Transforming list items with a Stream");
+        List<String> referenceCodes = Arrays.asList("a12", "C14", "b13");
+        referenceCodes.stream()
+                .map(code -> Character.toUpperCase(code.charAt(0)) + code.substring(1))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+        System.out.println("... but the original List remains unchanged: " + referenceCodes);
+
+    }
+
+}
