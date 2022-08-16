0d7b101e072da927fe309d27f46206221e161f7a
soonho <dbstnsgh2@naver.com>
2022-08-17 07:08:08 +0900

    ch11 - 두 Optional 합치기 (ifPresent(), Optional.of() 사용)

--- src/main/java/com/example/modernjavainaction/ch11/OptionalExample.java
+++ src/main/java/com/example/modernjavainaction/ch11/OptionalExample.java
@@ -75,4 +75,19 @@ public class OptionalExample {
                 .collect(toSet());
         return collect;
     }
+
+    public static Optional<Insurance> nullSafeFindCheapestInsurance(Optional<Person> person, Optional<Car> car) {
+        if (person.isPresent() && car.isPresent()) {
+            return Optional.of(findCheapestInsurance(person.get(), car.get()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Insurance findCheapestInsurance(Person person, Car car) {
+        // 다른 보험사에서 제공한 질의 서비스
+        // 모든 데이터 비교
+        Insurance cheapestCompany = new Insurance();
+        return cheapestCompany;
+    }
 }
