ff5eb01c9d54543cac9e387f1bc624e82e328d87
soonho <dbstnsgh2@naver.com>
2022-08-18 06:43:30 +0900

    ch13 - 인터페이스 다중 상속 - 공통 기능 또는 특정 로직으로 제공하고 싶은 경우는 default 메소드로 제공

---
+++ src/main/java/com/example/modernjavainaction/ch13/Moveable.java
@@ -0,0 +1,21 @@
+package com.example.modernjavainaction.ch13;
+
+public interface Moveable {
+
+    int getX();
+
+    int getY();
+
+    void setX(int x);
+
+    void setY(int y);
+
+    default void moveHorizontally(int distance) {
+        setX(getX() + distance);
+    }
+
+    default void moveVertically(int distance) {
+        setY(getY() + distance);
+    }
+
+}
---
+++ src/main/java/com/example/modernjavainaction/ch13/Sun.java
@@ -0,0 +1,33 @@
+package com.example.modernjavainaction.ch13;
+
+public class Sun implements Moveable, Rotatable {
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void setX(int x) {
+
+    }
+
+    @Override
+    public void setY(int y) {
+
+    }
+
+    @Override
+    public void setRotatableAngle(int angleInDegrees) {
+
+    }
+
+    @Override
+    public int getRotatableAngle() {
+        return 0;
+    }
+}
