eae52112aed518d6101b2e3cacb0c4564558b62d
soonho <dbstnsgh2@naver.com>
2022-08-17 20:25:20 +0900

    ch11 - Optional 예외 처리 - 1 (Optional 사용 X)

--- src/main/java/com/example/modernjavainaction/ch11/ReadPositiveIntParamExample.java
+++ src/main/java/com/example/modernjavainaction/ch11/ReadPositiveIntParamExample.java
@@ -2,6 +2,7 @@ package com.example.modernjavainaction.ch11;
 
 
 import java.util.Optional;
+import java.util.Properties;
 
 
 public class ReadPositiveIntParamExample {
@@ -11,6 +12,20 @@ public class ReadPositiveIntParamExample {
         System.out.println(stringToInt(null));
         // Optional[1]
         System.out.println(stringToInt("1"));
+
+      Properties props = new Properties();
+      props.setProperty("a", "5");
+      props.setProperty("b", "true");
+      props.setProperty("c", "-3");
+
+      // 5
+      System.out.println(readDurationImperative(props, "a"));
+      // 0
+      System.out.println(readDurationImperative(props, "b"));
+      // 0
+      System.out.println(readDurationImperative(props, "c"));
+      // 0
+      System.out.println(readDurationImperative(props, "d"));
     }
 
     public static Optional<Integer> stringToInt(String s) {
@@ -19,6 +34,20 @@ public class ReadPositiveIntParamExample {
         } catch (NumberFormatException e) {
             return Optional.empty();
         }
+    }
+
+    public static int readDurationImperative(Properties props, String name) {
+        String value = props.getProperty(name);
+        if (value != null) {
+            try {
+                int i = Integer.parseInt(value);
+                if (i > 0) {
+                    return i;
+                }
+            } catch (NumberFormatException nfe) {
+            }
+        }
+        return 0;
     }
 
 }
