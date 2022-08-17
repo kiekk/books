66c61e787f4c61f674bcd771dcca28c470fd83c5
soonho <dbstnsgh2@naver.com>
2022-08-18 06:37:34 +0900

    ch13 - 디폴트 메소드 예제 - 2 (Resizable V2)

--- src/main/java/com/example/modernjavainaction/ch13/Resizable.java
+++ src/main/java/com/example/modernjavainaction/ch13/Resizable.java
@@ -12,4 +12,13 @@ public interface Resizable extends Drawable {
 
     void setAbsoluteSize(int width, int height);
 
+    // V2 메소드 추가
+    // 이렇게 하면 Ellipse is not abstract and does not override abstract method setRelativeSize(int,int) in Resizable
+    // 예외 발생
+//    void setRelativeSize(int widthFactor, int heightFactor);
+
+    // 이 경우 디폴트 메소드를 사용하여 해결
+    default void setRelativeSize(int widthFactor, int heightFactor) {
+        setAbsoluteSize(widthFactor, heightFactor);
+    }
 }
