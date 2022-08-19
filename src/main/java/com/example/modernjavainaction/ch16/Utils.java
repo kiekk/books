0f7a130f492754645c8460fc527a8bd0d6520b7c
soonho <dbstnsgh2@naver.com>
2022-08-20 08:24:29 +0900

    ch16 - 비동기 API 흉내 (Thread.sleep)

---
+++ src/main/java/com/example/modernjavainaction/ch16/Utils.java
@@ -0,0 +1,13 @@
+package com.example.modernjavainaction.ch16;
+
+public class Utils {
+
+    public static void delay() {
+        int delay = 1000;
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
