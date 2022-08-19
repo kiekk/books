375115c6ffbab86be5cda036cf324b6b745db6cf
soonho <dbstnsgh2@naver.com>
2022-08-20 08:39:58 +0900

    ch16 - 비동기 메서드 (Future) 예외 처리

--- src/main/java/com/example/modernjavainaction/ch16/Shop.java
+++ src/main/java/com/example/modernjavainaction/ch16/Shop.java
@@ -8,34 +8,38 @@ import static com.example.modernjavainaction.ch16.Utils.delay;
 
 public class Shop {
 
-  private final String name;
-  private final Random random;
+    private final String name;
+    private final Random random;
 
-  public Shop(String name) {
-    this.name = name;
-    random = new Random(name.charAt(0) * name.charAt(1) * name.charAt(2));
-  }
+    public Shop(String name) {
+        this.name = name;
+        random = new Random(name.charAt(0) * name.charAt(1) * name.charAt(2));
+    }
 
-  public double getPrice(String product) {
-    return calculatePrice(product);
-  }
+    public double getPrice(String product) {
+        return calculatePrice(product);
+    }
 
-  private double calculatePrice(String product) {
-    delay();
-    return random.nextDouble() * product.charAt(0) + product.charAt(1);
-  }
+    private double calculatePrice(String product) {
+        delay();
+        return random.nextDouble() * product.charAt(0) + product.charAt(1);
+    }
 
-  public Future<Double> getPriceAsync(String product) {
-    CompletableFuture<Double> futurePrice = new CompletableFuture<>();
-    new Thread(() -> {
-      double price = calculatePrice(product);
-      futurePrice.complete(price);
-    }).start();
-    return futurePrice;
-  }
+    public Future<Double> getPriceAsync(String product) {
+        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+        new Thread(() -> {
+            try {
+                double price = calculatePrice(product);
+                futurePrice.complete(price);
+            } catch (Exception ex) {
+                futurePrice.completeExceptionally(ex);
+            }
+        }).start();
+        return futurePrice;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
 }
