f833adabcb16ad8a9832e96e587c872956ca83c1
soonho <dbstnsgh2@naver.com>
2022-09-07 23:08:42 +0900

    ch04: add custom JobParametersIncrementer

--- src/main/java/io/spring/batch/helloworld/JobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/JobConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,7 +28,7 @@ public class JobConfiguration {
         return jobBuilderFactory.get("basicJob")
                 .start(step1())
                 .validator(validator())
-                .incrementer(new RunIdIncrementer())
+                .incrementer(new DailyJobTimeStamper())
                 .build();
     }
 
@@ -57,7 +56,7 @@ public class JobConfiguration {
         CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
         DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator();
         defaultJobParametersValidator.setRequiredKeys(new String[]{"fileName"});
-        defaultJobParametersValidator.setOptionalKeys(new String[]{"name"});
+        defaultJobParametersValidator.setOptionalKeys(new String[]{"name", "currentDate"});
 
         defaultJobParametersValidator.afterPropertiesSet();
 
