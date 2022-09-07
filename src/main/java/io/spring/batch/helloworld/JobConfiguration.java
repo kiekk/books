d84fa8ad3a9a9072f9896774a438bf7ab069407f
soonho <dbstnsgh2@naver.com>
2022-09-07 22:57:53 +0900

    ch04: use @StepScope & jobParameters

--- src/main/java/io/spring/batch/helloworld/JobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/JobConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,17 +29,14 @@ public class JobConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(helloWorldTasklet())
+                .tasklet(helloWorldTasklet(null))
                 .build();
     }
 
     @Bean
-    public Tasklet helloWorldTasklet() {
+    @StepScope
+    public Tasklet helloWorldTasklet(@Value("#{jobParameters['name']}") String name) {
         return (contribution, chunkContext) -> {
-            String name = (String) chunkContext.getStepContext()
-                    .getJobParameters()
-                    .get("name");
-
             System.out.printf("Hello, %s\n", name);
             return RepeatStatus.FINISHED;
         };
