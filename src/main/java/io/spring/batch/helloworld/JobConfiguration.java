fe97505c1132b0e926ebae4e8b3f24baddcb2d9b
soonho <dbstnsgh2@naver.com>
2022-09-07 23:00:50 +0900

    ch04: add parameter validator

--- src/main/java/io/spring/batch/helloworld/JobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/JobConfiguration.java
@@ -2,10 +2,12 @@ package io.spring.batch.helloworld;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
@@ -40,6 +42,14 @@ public class JobConfiguration {
             System.out.printf("Hello, %s\n", name);
             return RepeatStatus.FINISHED;
         };
+    }
+
+    @Bean
+    public JobParametersValidator validator() {
+        DefaultJobParametersValidator validator = new DefaultJobParametersValidator();
+        validator.setRequiredKeys(new String[]{"fileName"});
+        validator.setOptionalKeys(new String[]{"name"});
+        return validator;
     }
 
 }
