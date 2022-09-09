d35c3dd69e0cf5eb9f32e59afb91f5aac0a82583
soonho <dbstnsgh2@naver.com>
2022-09-10 05:48:05 +0900

    ch06: add TransactionReader

---
+++ src/main/java/io/spring/batch/helloworld/TransactionReader.java
@@ -0,0 +1,69 @@
+package io.spring.batch.helloworld;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.file.transform.FieldSet;
+
+public class TransactionReader implements ItemStreamReader<Transaction> {
+
+    private final ItemStreamReader<FieldSet> fieldSetReader;
+    private int recordCount = 0;
+    private int expectedRecordCount = 0;
+
+    public TransactionReader(ItemStreamReader<FieldSet> fieldSetReader) {
+        this.fieldSetReader = fieldSetReader;
+    }
+
+    @Override
+    public Transaction read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        return process(fieldSetReader.read());
+    }
+
+    private Transaction process(FieldSet fieldSet) {
+        Transaction result = null;
+
+        if (fieldSet == null) {
+            return result;
+        }
+
+        if (fieldSet.getFieldCount() > 1) {
+            result = new Transaction();
+            result.setAccountNumber(fieldSet.readString(0));
+            result.setTimeStamp(fieldSet.readDate(1, "yyyy-MM-DD HH:mm:ss"));
+            result.setAmount(fieldSet.readDouble(2));
+
+            recordCount++;
+        } else {
+            expectedRecordCount = fieldSet.readInt(0);
+        }
+
+        return result;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        fieldSetReader.open(executionContext);
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        fieldSetReader.update(executionContext);
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+        fieldSetReader.close();
+    }
+
+    @AfterStep
+    public ExitStatus afterStep(StepExecution execution) {
+        if (recordCount == expectedRecordCount) {
+            return execution.getExitStatus();
+        } else {
+            return ExitStatus.STOPPED;
+        }
+    }
+
+}
