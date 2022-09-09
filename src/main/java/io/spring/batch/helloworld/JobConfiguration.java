ec5e51baf755d01ff0e633147caccc152b51720e
soonho <dbstnsgh2@naver.com>
2022-09-10 05:19:46 +0900

    ch06: add QuartzJob

--- src/main/java/io/spring/batch/helloworld/JobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/JobConfiguration.java
@@ -1,24 +1,14 @@
 package io.spring.batch.helloworld;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Properties;
 
 @Configuration
 @RequiredArgsConstructor
@@ -39,56 +29,10 @@ public class JobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step1 run");
+                    System.out.println("step1 run!");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
-    }
-
-    @RestController
-    @RequiredArgsConstructor
-    public static class JobLaunchingController {
-
-        private final JobLauncher jobLauncher;
-        private final ApplicationContext context;
-        private final JobExplorer jobExplorer;
-
-        @PostMapping("/run")
-        public ExitStatus runJob(@RequestBody JobLaunchRequest request) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-            Job job = context.getBean(request.getName(), Job.class);
-            JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(), jobExplorer)
-                    .getNextJobParameters(job)
-                    .toJobParameters();
-            return jobLauncher.run(job, jobParameters).getExitStatus();
-        }
-
-    }
-
-    public static class JobLaunchRequest {
-        private String name;
-        private Properties jobParameters;
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public Properties getJobParamsProperties() {
-            return jobParameters;
-        }
-
-        public void setJobParamsProperties(Properties jobParameters) {
-            this.jobParameters = jobParameters;
-        }
-
-        public JobParameters getJobParameters() {
-            Properties properties = new Properties();
-            properties.putAll(jobParameters);
-            return new JobParametersBuilder(properties).toJobParameters();
-        }
     }
 
 }
