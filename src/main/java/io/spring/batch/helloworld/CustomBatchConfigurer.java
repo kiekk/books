be5baaa51400aca394b4b4f42c61b4e77f437126
soonho <dbstnsgh2@naver.com>
2022-09-10 03:56:26 +0900

    ch05: Custom JobExplorer

--- src/main/java/io/spring/batch/helloworld/CustomBatchConfigurer.java
+++ src/main/java/io/spring/batch/helloworld/CustomBatchConfigurer.java
@@ -1,18 +1,25 @@
 package io.spring.batch.helloworld;
 
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 public class CustomBatchConfigurer extends DefaultBatchConfigurer {
 
     @Autowired
-    @Qualifier("batchTransactionManager")
-    private PlatformTransactionManager transactionManager;
+    @Qualifier("repositoryDataSource")
+    private DataSource dataSource;
 
     @Override
-    public PlatformTransactionManager getTransactionManager() {
-        return transactionManager;
+    protected JobExplorer createJobExplorer() throws Exception {
+        JobExplorerFactoryBean factoryBean = new JobExplorerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTablePrefix("FOO_");
+        factoryBean.afterPropertiesSet();
+        return factoryBean.getObject();
     }
 }
