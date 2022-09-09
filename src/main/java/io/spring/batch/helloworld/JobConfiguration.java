b7d0dad8c58a898e093d824900afac7038489e8f
soonho <dbstnsgh2@naver.com>
2022-09-10 04:30:08 +0900

    ch05: spring.batch.job.enabled - false

--- src/main/java/io/spring/batch/helloworld/JobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/JobConfiguration.java
@@ -5,8 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,24 +15,21 @@ public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final JobExplorer jobExplorer;
 
     @Bean
-    public Tasklet explorerTasklet() {
-        return new ExploringTasklet(jobExplorer);
-    }
-
-    @Bean
-    public Step explorerStep() {
-        return stepBuilderFactory.get("explorerStep")
-                .tasklet(explorerTasklet())
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
                 .build();
     }
 
     @Bean
-    public Job explorerJob() {
-        return jobBuilderFactory.get("explorerJob")
-                .start(explorerStep())
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 run");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
--- src/main/java/io/spring/batch/helloworld/SpringBatchBookApplication.java
+++ src/main/java/io/spring/batch/helloworld/SpringBatchBookApplication.java
@@ -4,12 +4,20 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchBookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchBookApplication.class, args);
+        // java 코드로 property 설정
+//        SpringApplication springApplication = new SpringApplication(SpringBatchBookApplication.class);
+//        Properties properties = new Properties();
+//        properties.put("spring.batch.job.enabled", false);
+//        springApplication.setDefaultProperties(properties);
+//        springApplication.run(args);
     }
 
 }
