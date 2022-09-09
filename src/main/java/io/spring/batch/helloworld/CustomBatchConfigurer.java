9f0eec1b1087bb3da422ae29e6a0b6320b7a7f52
soonho <dbstnsgh2@naver.com>
2022-09-10 03:53:13 +0900

    ch05: Custom TransactionManager

--- src/main/java/io/spring/batch/helloworld/CustomBatchConfigurer.java
+++ src/main/java/io/spring/batch/helloworld/CustomBatchConfigurer.java
@@ -1,28 +1,18 @@
 package io.spring.batch.helloworld;
 
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
-import org.springframework.batch.support.DatabaseType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
-import javax.sql.DataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 public class CustomBatchConfigurer extends DefaultBatchConfigurer {
 
     @Autowired
-    @Qualifier("repositoryDataSource")
-    private DataSource dataSource;
+    @Qualifier("batchTransactionManager")
+    private PlatformTransactionManager transactionManager;
 
     @Override
-    protected JobRepository createJobRepository() throws Exception {
-        JobRepositoryFactoryBean factoryBean = new JobRepositoryFactoryBean();
-        factoryBean.setDatabaseType(DatabaseType.MYSQL.getProductName());
-        factoryBean.setTablePrefix("FOO_");
-        factoryBean.setIsolationLevelForCreate("ISOLATION_REPEATABLE_READ");
-        factoryBean.setDataSource(dataSource);
-        factoryBean.afterPropertiesSet();
-        return factoryBean.getObject();
+    public PlatformTransactionManager getTransactionManager() {
+        return transactionManager;
     }
 }
