4cfebb9de12f88eef004e2f10dcd1adbbddd5d71
soonho <dbstnsgh2@naver.com>
2022-09-11 20:58:05 +0900

    ch07: edit ToString to Customer

--- src/main/java/io/spring/batch/helloworld/Customer.java
+++ src/main/java/io/spring/batch/helloworld/Customer.java
@@ -1,13 +1,15 @@
 package io.spring.batch.helloworld;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Customer {
@@ -22,5 +24,26 @@ public class Customer {
     private String zipCode;
 
     private List<Transaction> transactions = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+
+        output.append(firstName);
+        output.append(" ");
+        output.append(middleInitial);
+        output.append(". ");
+        output.append(lastName);
+
+        if (transactions != null && transactions.size() > 0) {
+            output.append(" has ");
+            output.append(transactions.size());
+            output.append(" transactions.");
+        } else {
+            output.append(" has no transactions.");
+        }
+
+        return output.toString();
+    }
 
 }
