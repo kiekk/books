5e6c8b38fb5a473358dfe5fb6efc020d2b1fd6d9
soonho <dbstnsgh2@naver.com>
2022-09-11 20:33:37 +0900

    ch07: use FieldSetMapper

--- src/main/java/io/spring/batch/helloworld/BatchJobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/BatchJobConfiguration.java
@@ -48,15 +48,11 @@ public class BatchJobConfiguration {
         return new FlatFileItemReaderBuilder<Customer>()
                 .name("customerItemReader")
                 .resource(new ClassPathResource(inputFile))
-                .fixedLength()
-                .columns(new Range[]{
-                        new Range(1, 11), new Range(12, 12), new Range(13, 22),
-                        new Range(23, 26), new Range(27, 46), new Range(47, 62),
-                        new Range(63, 64), new Range(65, 69)
-                })
+                .delimited()
                 .names("firstName", "middleInitial", "lastName",
                         "addressNumber", "street", "city", "state", "zipCode")
                 .targetType(Customer.class)
+                .fieldSetMapper(new CustomerFieldSetMapper())
                 .build();
     }
 
