b287d511b1bd58e47c5ed094dca694c2ec8ca552
soonho <dbstnsgh2@naver.com>
2022-09-11 19:53:32 +0900

    ch07: 이전 예제 코드 제거

--- src/main/java/io/spring/batch/helloworld/TransactionDao.java
+++
@@ -1,9 +0,0 @@
-package io.spring.batch.helloworld;
-
-import java.util.List;
-
-public interface TransactionDao {
-
-    List<Transaction> getTransactionsByAccountNumber(String accountNumber);
-
-}
