b3b8b556b6f071802fc9243332914372e5de46c3
soonho <dbstnsgh2@naver.com>
2022-09-14 01:33:00 +0900

    ch07: use JdbcCursorItemReader

--- src/main/java/io/spring/batch/helloworld/BatchJobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/BatchJobConfiguration.java
@@ -1,6 +1,5 @@
 package io.spring.batch.helloworld;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -8,27 +7,14 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.MultiResourceItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.builder.MultiResourceItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.FieldSetMapper;
-import org.springframework.batch.item.file.mapping.PatternMatchingCompositeLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.LineTokenizer;
-import org.springframework.batch.item.json.JacksonJsonObjectReader;
-import org.springframework.batch.item.json.JsonItemReader;
-import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
-import java.util.Map;
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -48,97 +34,24 @@ public class BatchJobConfiguration {
     public Step copyFileStep() {
         return stepBuilderFactory.get("copyFileStep")
                 .<Customer, Customer>chunk(10)
-                .reader(customerFileReader(null))
+                .reader(customerItemReader(null))
                 .writer(itemWriter())
                 .build();
     }
 
     @Bean
-    @StepScope
-    public MultiResourceItemReader multiResourceItemReader(
-            @Value("#{jobParameters['customerFile']}") Resource[] inputFiles) {
-        ;
-        return new MultiResourceItemReaderBuilder<>()
-                .name("multiResourceReader")
-                .resources(inputFiles)
-                .delegate(customerFileReader(null))
-                .build();
-    }
-
-    @Bean
-    @StepScope
-    public JsonItemReader<Customer> customerFileReader(
-            @Value("#{jobParameters['customerFile']}") Resource inputFile
-    ) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd hhLmm:ss"));
-
-        JacksonJsonObjectReader<Customer> jsonObjectReader = new JacksonJsonObjectReader<>(Customer.class);
-        jsonObjectReader.setMapper(objectMapper);
-
-        return new JsonItemReaderBuilder<Customer>()
-                .name("customerFileReader")
-                .jsonObjectReader(jsonObjectReader)
-                .resource(inputFile)
-                .build();
-    }
-
-    @Bean
-    public Jaxb2Marshaller customerMarshaller() {
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setClassesToBeBound(Customer.class, Transaction.class);
-
-        return marshaller;
-    }
-
-    @Bean
-    @StepScope
-    public FlatFileItemReader customerItemReader() {
-        return new FlatFileItemReaderBuilder<Customer>()
-                .name("customerItemReader")
-                .lineMapper(lineTokenizer())
-                .build();
-    }
-
-    @Bean
-    public ItemWriter itemWriter() {
+    public ItemWriter<Customer> itemWriter() {
         return items -> items.forEach(System.out::println);
     }
 
     @Bean
-    public PatternMatchingCompositeLineMapper lineTokenizer() {
-        Map<String, LineTokenizer> lineTokenizerMap = new HashMap<>();
-        lineTokenizerMap.put("CUST*", customerLineTokenizer());
-        lineTokenizerMap.put("TRANS*", transactionLineTokenizer());
-
-        Map<String, FieldSetMapper> fieldSetMapperMap = new HashMap<>();
-
-        BeanWrapperFieldSetMapper<Customer> customerFieldSetMapper = new BeanWrapperFieldSetMapper<>();
-        customerFieldSetMapper.setTargetType(Customer.class);
-
-        fieldSetMapperMap.put("CUST*", customerFieldSetMapper);
-        fieldSetMapperMap.put("TRANS*", new TransactionFieldSetMapper());
-
-        PatternMatchingCompositeLineMapper lineMappers = new PatternMatchingCompositeLineMapper();
-        lineMappers.setTokenizers(lineTokenizerMap);
-        lineMappers.setFieldSetMappers(fieldSetMapperMap);
-
-        return lineMappers;
+    public JdbcCursorItemReader<Customer> customerItemReader(DataSource dataSource) {
+        return new JdbcCursorItemReaderBuilder<Customer>()
+                .name("customerItemReader")
+                .dataSource(dataSource)
+                .sql("select * from customer where city = ?")
+                .rowMapper(new CustomRowMapper())
+                .build();
     }
-
-    @Bean
-    public DelimitedLineTokenizer transactionLineTokenizer() {
-        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
-        lineTokenizer.setNames("prefix", "accountNumber", "transactionDate", "amount");
-        return lineTokenizer;
-    }
-
-    @Bean
-    public DelimitedLineTokenizer customerLineTokenizer() {
-        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
-        lineTokenizer.setNames("firstNames", "middleInitial", "lastName", "address", "city", "state", "zipCode");
-        lineTokenizer.setIncludedFields(1, 2, 3, 4, 5, 6, 7);
-        return lineTokenizer;
-    }
-
+    
 }
