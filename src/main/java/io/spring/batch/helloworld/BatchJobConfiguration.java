d8abe20ad7b006db0a3153bf339d9fcb243f9a69
soonho <dbstnsgh2@naver.com>
2022-09-18 22:48:03 +0900

    ch07: use ItemReaderAdapter

--- src/main/java/io/spring/batch/helloworld/BatchJobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/BatchJobConfiguration.java
@@ -5,16 +5,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.item.adapter.ItemReaderAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
-
-import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
@@ -34,7 +28,7 @@ public class BatchJobConfiguration {
     public Step copyFileStep() {
         return stepBuilderFactory.get("copyFileStep")
                 .<Customer, Customer>chunk(10)
-                .reader(customerItemReader(null, null))
+                .reader(customerItemReader(null))
                 .writer(itemWriter())
                 .build();
     }
@@ -45,16 +39,13 @@ public class BatchJobConfiguration {
     }
 
     @Bean
-    @StepScope
-    public RepositoryItemReader<Customer> customerItemReader(CustomerRepository customerRepository,
-                                                             @Value("#{jobParameters['city']}") String city) {
-        return new RepositoryItemReaderBuilder<Customer>()
-                .name("customerItemReader")
-                .arguments(Collections.singletonList(city))
-                .methodName("findByCity")
-                .repository(customerRepository)
-                .sorts(Collections.singletonMap("lastName", Sort.Direction.ASC))
-                .build();
+    public ItemReaderAdapter<Customer> customerItemReader(CustomerService customerService) {
+        ItemReaderAdapter<Customer> adapter = new ItemReaderAdapter<>();
+
+        adapter.setTargetObject(customerService);
+        adapter.setTargetMethod("getCustomer");
+
+        return adapter;
     }
 
 }
