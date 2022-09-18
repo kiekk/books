e12cccfd8c9c956e5923a7ad2f4ed8c14df1c6e3
soonho <dbstnsgh2@naver.com>
2022-09-18 17:54:40 +0900

    ch07: edit entity Customer / add file customer data, schema sql

--- src/main/java/io/spring/batch/helloworld/Customer.java
+++ src/main/java/io/spring/batch/helloworld/Customer.java
@@ -1,52 +1,21 @@
 package io.spring.batch.helloworld;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
-import java.util.ArrayList;
-import java.util.List;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Customer {
 
+    private Long id;
     private String firstName;
     private String middleInitial;
     private String lastName;
     private String address;
-    private String street;
     private String city;
     private String state;
     private String zipCode;
-
-    private List<Transaction> transactions = new ArrayList<>();
-
-    @Override
-    public String toString() {
-        StringBuilder output = new StringBuilder();
-
-        output.append(firstName);
-        output.append(" ");
-        output.append(middleInitial);
-        output.append(". ");
-        output.append(lastName);
-
-        if (transactions != null && transactions.size() > 0) {
-            output.append(" has ");
-            output.append(transactions.size());
-            output.append(" transactions.");
-        } else {
-            output.append(" has no transactions.");
-        }
-
-        return output.toString();
-    }
 
 }
