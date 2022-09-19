4c4d28ff5f86fa7d286c61a37709c2f15beaf408
soonho <dbstnsgh2@naver.com>
2022-09-20 00:00:37 +0900

    ch08: use CompositeItemProcessor

--- src/main/java/io/spring/batch/helloworld/batch/ValidationJob.java
+++ src/main/java/io/spring/batch/helloworld/batch/ValidationJob.java
@@ -1,6 +1,8 @@
 package io.spring.batch.helloworld.batch;
 
 import io.spring.batch.helloworld.domain.Customer;
+import io.spring.batch.helloworld.domain.UniqueLastNameValidator;
+import io.spring.batch.helloworld.service.UpperCaseNameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -8,13 +10,18 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.adapter.ItemProcessorAdapter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.batch.item.support.ScriptItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -52,17 +59,50 @@ public class ValidationJob {
         return stepBuilderFactory.get("copyFileStep")
                 .<Customer, Customer>chunk(5)
                 .reader(customerItemReader(null))
-                .processor(itemProcessor(null))
+                .processor(itemProcessor())
                 .writer(itemWriter())
                 .build();
     }
 
     @Bean
+    public UniqueLastNameValidator validator() {
+        UniqueLastNameValidator uniqueLastNameValidator = new UniqueLastNameValidator();
+        uniqueLastNameValidator.setName("validator");
+        return uniqueLastNameValidator;
+    }
+
+    @Bean
+    public ValidatingItemProcessor<Customer> customerValidatingItemProcessor() {
+        ValidatingItemProcessor<Customer> itemProcessor = new ValidatingItemProcessor<>(validator());
+        itemProcessor.setFilter(true);
+        return itemProcessor;
+    }
+
+    @Bean
+    public ItemProcessorAdapter<Customer, Customer> upperCaseItemProcessor(UpperCaseNameService service) {
+        ItemProcessorAdapter<Customer, Customer> adapter = new ItemProcessorAdapter<>();
+        adapter.setTargetObject(service);
+        adapter.setTargetMethod("upperCase");
+        return adapter;
+    }
+
+    @Bean
     @StepScope
-    public ScriptItemProcessor<Customer, Customer> itemProcessor(
+    public ScriptItemProcessor<Customer, Customer> lowerCaseItemProcessor(
             @Value("#{jobParameters['script']}") Resource script) {
         ScriptItemProcessor<Customer, Customer> itemProcessor = new ScriptItemProcessor<>();
         itemProcessor.setScript(script);
+        return itemProcessor;
+    }
+
+    @Bean
+    public CompositeItemProcessor<Customer, Customer> itemProcessor() {
+        CompositeItemProcessor<Customer, Customer> itemProcessor = new CompositeItemProcessor<>();
+        itemProcessor.setDelegates(Arrays.asList(
+                customerValidatingItemProcessor(),
+                upperCaseItemProcessor(null),
+                lowerCaseItemProcessor(null)
+        ));
         return itemProcessor;
     }
 
