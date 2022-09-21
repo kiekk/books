c7063821f43ca1ed9af7cffb6fd8b6a9bf74e62e
soonho <dbstnsgh2@naver.com>
2022-09-22 06:22:07 +0900

    ch09: change Customer to Entity

--- src/main/java/io/spring/batch/helloworld/domain/Customer.java
+++ src/main/java/io/spring/batch/helloworld/domain/Customer.java
@@ -4,11 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Getter
 @Setter
 @ToString
-public class Customer {
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String middleInitial;
