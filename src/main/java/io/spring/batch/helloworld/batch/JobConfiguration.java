e92a1c2e15dbc2ffdba046f9f296f767526e36c0
soonho <dbstnsgh2@naver.com>
2022-09-21 22:01:21 +0900

    ch09: user FlatFileItemReader / FlatFileItemWriter

---
+++ src/main/java/io/spring/batch/helloworld/batch/JobConfiguration.java
@@ -0,0 +1,69 @@
+package io.spring.batch.helloworld.batch;
+
+import io.spring.batch.helloworld.domain.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<Customer> customerFileReader(
+            @Value("#{jobParameters['customerFile']}") Resource inputFile) {
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("customerFileReader")
+                .resource(inputFile)
+                .delimited()
+                .names("firstName", "middleInitial", "lastName", "address", "city", "state", "zip")
+                .targetType(Customer.class)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<Customer> customerItemWriter(
+            @Value("#{jobParameters['outputFile']}") Resource outputFile) {
+        return new FlatFileItemWriterBuilder<Customer>()
+                .name("customerItemWriter")
+                .resource(outputFile)
+                .formatted()
+                .format("%s %s lives at %s %s in %s, %s.")
+                .names("firstName", "middleInitial", "lastName", "address", "city", "state", "zip")
+                .build();
+    }
+
+    @Bean
+    public Step formatStep() {
+        return stepBuilderFactory.get("formatStep")
+                .<Customer, Customer>chunk(10)
+                .reader(customerFileReader(null))
+                .writer(customerItemWriter(null))
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("formatJob")
+                .start(formatStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+}
