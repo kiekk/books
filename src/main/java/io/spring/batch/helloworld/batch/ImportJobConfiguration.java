05977d35ad0d413ea7984a04fdc644065363a1ea
soonho <dbstnsgh2@naver.com>
2022-10-01 12:02:25 +0900

    ch10: add itemwriter - customerNameUpdateItemWriter / customerAddressUpdateItemWriter / customerContactUpdateItemWriter

--- src/main/java/io/spring/batch/helloworld/batch/ImportJobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/batch/ImportJobConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
@@ -24,6 +26,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
 
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,6 +41,46 @@ public class ImportJobConfiguration {
     public Job job() throws Exception {
         return jobBuilderFactory.get("importJob")
                 .start(importCustomerUpdates())
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<CustomerUpdate> customerNameUpdateItemWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<CustomerUpdate>()
+                .beanMapped()
+                .sql("UPDATE CUSTOMER SET FIRST_NAME = COALESCE(:firstName, FIRST_NAME), " +
+                        "MIDDLE_NAME = COALESCE(:middleName, MIDDLE_NAME), " +
+                        "LAST_NAME = COALESCE(:lastName, LAST_NAME) " +
+                        "WHERE CUSTOMER_ID = :customerId")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<CustomerUpdate> customerAddressUpdateItemWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<CustomerUpdate>()
+                .beanMapped()
+                .sql("UPDATE CUSTOMER SET ADDRESS1 = COALESCE(:address1, ADDRESS1), " +
+                        "ADDRESS2 = COALESCE(:address2, ADDRESS2), " +
+                        "CITY = COALESCE(:city, CITY) " +
+                        "STATE = COALESCE(:state, STATE) " +
+                        "POSTAL_CODE = COALESCE(:postalCode, POSTAL_CODE) " +
+                        "WHERE CUSTOMER_ID = :customerId")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<CustomerUpdate> customerContactUpdateItemWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<CustomerUpdate>()
+                .beanMapped()
+                .sql("UPDATE CUSTOMER SET EMAIL_ADDRESS = COALESCE(:emailAddress, EMAIL_ADDRESS), " +
+                        "HOME_PHONE = COALESCE(:homePhone, HOME_PHONE), " +
+                        "CELL_PHONE = COALESCE(:cellPhone, CELL_PHONE) " +
+                        "WORK_PHONE = COALESCE(:workPhone, WORK_PHONE) " +
+                        "NOTIFICATION_PREF = COALESCE(:notificationPreferences, NOTIFICATION_PREF) " +
+                        "WHERE CUSTOMER_ID = :customerId")
+                .dataSource(dataSource)
                 .build();
     }
 
