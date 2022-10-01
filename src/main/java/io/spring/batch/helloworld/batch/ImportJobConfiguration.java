b061df1eb56bd8acf7af8a398b9ca02e53857900
soonho <dbstnsgh2@naver.com>
2022-10-01 11:23:49 +0900

    ch10: add custom itemreader - customerUpdateItemReader

--- src/main/java/io/spring/batch/helloworld/batch/ImportJobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/batch/ImportJobConfiguration.java
@@ -6,8 +6,13 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,6 +32,17 @@ public class ImportJobConfiguration {
     public Step importCustomerUpdates() {
         return stepBuilderFactory.get("importCustomerUpdates")
                 .<CustomerUpdate, CustomerUpdate>chunk(100)
+                .reader(customerUpdateItemReader(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<CustomerUpdate> customerUpdateItemReader(
+            @Value("#{jobParameters['customerUpdateFile']}") Resource inputFile) {
+        return new FlatFileItemReaderBuilder<CustomerUpdate>()
+                .name("customerUpdateItemReader")
+                .resource(inputFile)
                 .build();
     }
 
