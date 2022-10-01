59f6b2cc5f65f93b6826ed8fc8d813d985a7f427
soonho <dbstnsgh2@naver.com>
2022-10-01 22:22:42 +0900

    ch11: add filter InitialAuthenticationFilter

---
+++ ch11/application-server/src/main/java/com/example/ssia/security/filter/InitialAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.example.ssia.security.filter;
+
+import com.example.ssia.security.token.OtpAuthentication;
+import com.example.ssia.security.token.UsernamePasswordAuthentication;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class InitialAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationManager manager;
+
+    @Value("${jwt.signing.key}")
+    private String signingKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String username = request.getHeader("username");
+        String password = request.getHeader("password");
+        String code = request.getHeader("code");
+
+        if (code == null) {
+            Authentication authentication = new UsernamePasswordAuthentication(username, password);
+            manager.authenticate(authentication);
+        } else {
+            Authentication authentication = new OtpAuthentication(username, code);
+            Authentication authenticate = manager.authenticate(authentication);
+
+            SecretKey key = Keys.hmacShaKeyFor(signingKey.getBytes(StandardCharsets.UTF_8));
+
+            String jwt = Jwts.builder()
+                    .setClaims(Map.of("username", username))
+                    .signWith(key)
+                    .compact();
+
+            response.setHeader("Authorization", jwt);
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !request.getServletPath().equals("/login");
+    }
+}
