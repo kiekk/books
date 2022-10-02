806d6cee2eb8d72107b1d92cbaf1575e0fb1940e
soonho <dbstnsgh2@naver.com>
2022-10-03 05:46:08 +0900

    ch11: parallel step

--- src/main/java/io/spring/batch/helloworld/batch/JobConfiguration.java
+++ src/main/java/io/spring/batch/helloworld/batch/JobConfiguration.java
@@ -7,16 +7,20 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import javax.sql.DataSource;
 
@@ -53,6 +57,21 @@ public class JobConfiguration {
 
     @Bean
     @StepScope
+    public StaxEventItemReader<Transaction> transactionItemReader(
+            @Value("#{jobParameters['transactionFile']}") Resource transactionFile) {
+        Jaxb2Marshaller unmarshaller = new Jaxb2Marshaller();
+        unmarshaller.setClassesToBeBound(Transaction.class);
+
+        return new StaxEventItemReaderBuilder<Transaction>()
+                .name("fooReader")
+                .resource(transactionFile)
+                .addFragmentRootElements("transaction")
+                .unmarshaller(unmarshaller)
+                .build();
+    }
+
+    @Bean
+    @StepScope
     public JdbcBatchItemWriter<Transaction> writer(DataSource dataSource) {
         return new JdbcBatchItemWriterBuilder<Transaction>()
                 .dataSource(dataSource)
@@ -61,11 +80,22 @@ public class JobConfiguration {
                 .build();
     }
 
+
     @Bean
-    public Job multithreadedJob() {
-        return jobBuilderFactory.get("multithreadedJob")
+    public Job parallelStepsJob() {
+        Flow secondFlow = new FlowBuilder<Flow>("secondFlow")
+                .start(step2())
+                .build();
+
+        Flow parallelFlow = new FlowBuilder<Flow>("parallelFlow")
                 .start(step1())
-                .incrementer(new RunIdIncrementer())
+                .split(new SimpleAsyncTaskExecutor())
+                .add(secondFlow)
+                .build();
+
+        return jobBuilderFactory.get("parallelStepsJob")
+                .start(parallelFlow)
+                .end()
                 .build();
     }
 
@@ -73,9 +103,17 @@ public class JobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .<Transaction, Transaction>chunk(100)
-                .reader(fileTransactionReader(null))
+                .reader(transactionItemReader(null))
                 .writer(writer(null))
-                .taskExecutor(new SimpleAsyncTaskExecutor())
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .<Transaction, Transaction>chunk(100)
+                .reader(transactionItemReader(null))
+                .writer(writer(null))
                 .build();
     }
 
