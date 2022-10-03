a04ed94f437e432b58391d165e455eb226a506a7
soonho <dbstnsgh2@naver.com>
2022-10-03 09:08:39 +0900

    ch12: use application.yml

--- ch12/oauth2/src/main/java/com/example/ssia/security/SecurityConfig.java
+++ ch12/oauth2/src/main/java/com/example/ssia/security/SecurityConfig.java
@@ -3,10 +3,6 @@ package com.example.ssia.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,24 +10,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .oauth2Login(httpSecurityOAuth2LoginConfigurer -> {
-                    httpSecurityOAuth2LoginConfigurer.clientRegistrationRepository(clientRegistrationRepository());
-                });
+                .oauth2Login();
 
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
     }
 
-    private ClientRegistration clientRegistration() {
-        return CommonOAuth2Provider.GITHUB
-                .getBuilder("github")
-                .clientId("clientId")
-                .clientSecret("clientSecret")
-                .build();
-    }
-
-    private ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(clientRegistration());
-    }
 }
