e43d4b45c081e81a7b60ce4f7c4b500e39109256
soonho <dbstnsgh2@naver.com>
2022-10-05 05:39:46 +0900

    ch15: 리소스 서버 설정 변경 (공개키는 권한 서버에서 가져옴)

--- ch15/jwt-resource-server/src/main/java/com/example/ssia/config/ResourceServerConfig.java
+++ ch15/jwt-resource-server/src/main/java/com/example/ssia/config/ResourceServerConfig.java
@@ -1,40 +1,11 @@
 package com.example.ssia.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
-    @Value("${jwt.key}")
-    private String jwtKey;
-    @Value("${publicKey}")
-    private String publicKey;
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.tokenStore(tokenStore());
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(jwtAccessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setVerifierKey(publicKey);
-        jwtAccessTokenConverter.setSigningKey(jwtKey);
-        return jwtAccessTokenConverter;
-    }
-
 }
+
