704c82d2d8cfc8305b7e23e4cbc7bf9fa61b53e7
soonho <dbstnsgh2@naver.com>
2022-10-05 06:07:47 +0900

    ch15: 리소스 서버에서 토큰 정보 파싱

--- ch15/jwt-resource-server/src/main/java/com/example/ssia/controller/HelloController.java
+++ ch15/jwt-resource-server/src/main/java/com/example/ssia/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.ssia.controller;
 
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public String hello(OAuth2Authentication authentication) {
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+
+        return "Hello!" + details.getDecodedDetails();
     }
 
 }
