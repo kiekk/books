3c4eb19b684c2386a25e92b24502cfe5e318915d
soonho <dbstnsgh2@naver.com>
2022-10-05 05:36:08 +0900

    ch15: 리소스 서버에서 공개 키 사용

--- ch15/jwt-resource-server/src/main/java/com/example/ssia/config/ResourceServerConfig.java
+++ ch15/jwt-resource-server/src/main/java/com/example/ssia/config/ResourceServerConfig.java
@@ -1,15 +1,10 @@
 package com.example.ssia.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -21,9 +16,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Value("${jwt.key}")
     private String jwtKey;
+    @Value("${publicKey}")
+    private String publicKey;
 
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    public void configure(ResourceServerSecurityConfigurer resources) {
         resources.tokenStore(tokenStore());
     }
 
@@ -35,6 +32,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setVerifierKey(publicKey);
         jwtAccessTokenConverter.setSigningKey(jwtKey);
         return jwtAccessTokenConverter;
     }
