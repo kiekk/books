2f0437c07e3b8e11126658ed18fe60ac7be29924
soonho <dbstnsgh2@naver.com>
2022-10-10 08:52:22 +0900

    ch16: 권한 체크 - 권한 부여 식 사용

--- ch16/global-method-authorization/src/main/java/com/example/globalmethodauthorization/controller/HelloController.java
+++ ch16/global-method-authorization/src/main/java/com/example/globalmethodauthorization/controller/HelloController.java
@@ -3,7 +3,10 @@ package com.example.globalmethodauthorization.controller;
 import com.example.globalmethodauthorization.service.NameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +17,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, " + nameService.getName();
+    }
+
+    @GetMapping("/secret/names/{name}")
+    public List<String> names(@PathVariable String name) {
+        return nameService.getSecretNames(name);
     }
     
 }
--- ch16/global-method-authorization/src/main/java/com/example/globalmethodauthorization/service/NameService.java
+++ ch16/global-method-authorization/src/main/java/com/example/globalmethodauthorization/service/NameService.java
@@ -3,12 +3,21 @@ package com.example.globalmethodauthorization.service;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class NameService {
+
+    private final Map<String, List<String>> secretNames = Map.of("natalie", List.of("Energico", "Perfecto"), "emma", List.of("Fantastico"));
 
     @PreAuthorize("hasAnyAuthority('write')")
     public String getName() {
         return "Fantastic";
     }
 
+    @PreAuthorize("#name == authentication.principal.username")
+    public List<String> getSecretNames(String name) {
+        return secretNames.get(name);
+    }
 }
