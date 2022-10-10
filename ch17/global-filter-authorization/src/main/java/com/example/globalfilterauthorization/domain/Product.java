ac08346b9df97dce7fc661aa1b74c9294c0a2d8b
soonho <dbstnsgh2@naver.com>
2022-10-10 10:01:48 +0900

    ch17: add domain Product

---
+++ ch17/global-filter-authorization/src/main/java/com/example/globalfilterauthorization/domain/Product.java
@@ -0,0 +1,17 @@
+package com.example.globalfilterauthorization.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private String name;
+    private String owner;
+
+}
