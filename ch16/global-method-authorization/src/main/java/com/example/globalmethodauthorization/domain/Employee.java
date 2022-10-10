b28217ef7eae4706c04e320b13b3682340c55bb0
soonho <dbstnsgh2@naver.com>
2022-10-10 09:01:50 +0900

    ch16: add service BookService - set @PostAuthorize

--- ch16/global-method-authorization/src/main/java/com/example/globalmethodauthorization/domain/Employee.java
+++ ch16/global-method-authorization/src/main/java/com/example/globalmethodauthorization/domain/Employee.java
@@ -1,5 +1,6 @@
 package com.example.globalmethodauthorization.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Employee {
 
     private String name;
