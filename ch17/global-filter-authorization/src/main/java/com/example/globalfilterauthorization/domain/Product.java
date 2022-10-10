0e642aaf30ed488e4b1e1b9758550efc7380ebbd
soonho <dbstnsgh2@naver.com>
2022-10-10 10:35:53 +0900

    ch17: add entity Product

--- ch17/global-filter-authorization/src/main/java/com/example/globalfilterauthorization/domain/Product.java
+++ ch17/global-filter-authorization/src/main/java/com/example/globalfilterauthorization/domain/Product.java
@@ -5,12 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private String owner;
 
