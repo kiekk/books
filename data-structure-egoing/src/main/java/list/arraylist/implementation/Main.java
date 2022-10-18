b9f226f0817ebbb775329f4f827b4376234b320e
shyoon <dbstnsgh2@naver.com>
2022-10-18 16:27:59 +0900

    egoing-arraylist: ArrayList - Insert 구현

--- data-structure-egoing/src/main/java/list/arraylist/implementation/Main.java
+++ data-structure-egoing/src/main/java/list/arraylist/implementation/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         ArrayList numbers = new ArrayList();
 
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+        numbers.addLast(40);
     }
 }
