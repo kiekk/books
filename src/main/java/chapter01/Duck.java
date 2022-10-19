7d9536846fa7e36335be7c194a2ee046ba407d1a
shyoon <dbstnsgh2@naver.com>
2022-10-20 08:39:22 +0900

    ch01: 고객 요구 사항에 따른 확장 / 상속을 사용할 때의 단점

--- src/main/java/chapter01/Duck.java
+++ src/main/java/chapter01/Duck.java
@@ -5,7 +5,14 @@ package chapter01;
  quack, swim 은 공통 메소드로 추출, 그리고 각 오리를 표현할 수 있도록 display 를 추상 메소드로 구현하여
  실 구현 객체에서 표현 방법을 지정
  */
+/*
+요구 사항 추가로 인해 오리가 날 수 있도록 fly 메소드를 공통 메소드로 추가
+ */
 public abstract class Duck {
+
+    public void fly() {
+        System.out.println("fly!!!");
+    }
 
     public void quack() {
         System.out.println("Quack!!");
--- src/main/java/chapter01/Main.java
+++ src/main/java/chapter01/Main.java
@@ -5,13 +5,29 @@ public class Main {
         Duck mallardDuck = new MallardDuck();
         Duck readHeadDuck = new RedheadDuck();
 
+        // 새로운 오리 추가 (고무 오리) rubber duck;
+        Duck rubberDuck = new RubberDuck();
 
         mallardDuck.quack();
         mallardDuck.swim();
         mallardDuck.display();
+        mallardDuck.fly();
 
         readHeadDuck.quack();
         readHeadDuck.swim();
         readHeadDuck.display();
+        readHeadDuck.fly();
+
+        rubberDuck.quack();
+        rubberDuck.swim();
+        readHeadDuck.display();
+        rubberDuck.fly(); // 의도하지 않은 결과, 고무 오리는 날 수 없다.
+
+        /*
+        각 오리 객체마다의 행동들을 제어하기 위해 Duck 메소드를 매번 오버라이딩 하기 때문에
+        정확하게 오리들이 어떤 행동들을 하는지 파악하기 어렵습니다.
+
+        또한 실 구현 객체인 서브 클래스에서 매번 오버라이딩하기 때문에 코드가 중복됩니다.
+         */
     }
 }
