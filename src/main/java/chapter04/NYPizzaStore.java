83111abde567dc1621c8e2ac0ac0f8cc211cb80f
shyoon <dbstnsgh2@naver.com>
2022-10-20 17:27:28 +0900

    ch04-factory-pattern: 팩토리 패턴 학습 예제 - 2 (피자 스토어 객체 생성)

---
+++ src/main/java/chapter04/NYPizzaStore.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+public class NYPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        switch (type) {
+            case "cheese":
+                return new NYStyleCheesePizza();
+            case "veggie":
+                return new NYStyleVeggiePizza();
+            case "clam":
+                return new NYStyleClamPizza();
+            case "pepperoni":
+                return new NYStylePepperoniPizza();
+            default:
+                return null;
+        }
+    }
+}
