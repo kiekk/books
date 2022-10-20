c7d7fdfe6641f51312c8086be8e49f91dc5cf2fb
shyoon <dbstnsgh2@naver.com>
2022-10-20 09:09:53 +0900

    ch01: 캡슐화 미적용 시 문제

--- src/main/java/chapter01/Main.java
+++ src/main/java/chapter01/Main.java
@@ -2,9 +2,6 @@ package chapter01;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        각 Duck 구현 객체들을 Duck 타입으로 묶을 수 있습니다.
-         */
         Duck mallardDuck = new MallardDuck();
         Duck redHeadDuck = new RedheadDuck();
         Duck rubberDuck = new RubberDuck();
@@ -24,9 +21,15 @@ public class Main {
         rubberDuck.performQuack();
         rubberDuck.performFly();
 
-        /*
-        이전에 살펴봤던 상속, 인터페이스의 단점은 해결 되었지만
-        각 구현 객체들의 생성자를 보면 QuackBehavior, FlyBehavior 의 실제 구현체들을 지정해주고 있는 것은 단점입니다.
-         */
+        // 노출할 필요가 없는 필드들에 직접 접근할 수 있는 것은 좋지 않습니다. (캡슐화 적용 X)
+//        mallardDuck.flyBehavior;
+//        mallardDuck.quackBehavior;
+
+//        redHeadDuck.flyBehavior;
+//        redHeadDuck.quackBehavior;
+        
+//        rubberDuck.flyBehavior;
+//        rubberDuck.quackBehavior;
+
     }
 }
