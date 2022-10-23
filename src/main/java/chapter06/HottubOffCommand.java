a3954ef3ef81b0b89fbd182d71ab22b54639aaa5
shyoon <dbstnsgh2@naver.com>
2022-10-24 08:42:40 +0900

    ch06-command-pattern: 욕조 클래스 / 커맨드 클래스 추가

---
+++ src/main/java/chapter06/HottubOffCommand.java
@@ -0,0 +1,15 @@
+package chapter06;
+
+public class HottubOffCommand implements Command {
+    Hottub hottub;
+
+    public HottubOffCommand(Hottub hottub) {
+        this.hottub = hottub;
+    }
+
+    @Override
+    public void execute() {
+        hottub.cool();
+        hottub.off();
+    }
+}
---
+++ src/main/java/chapter06/HottubOnCommand.java
@@ -0,0 +1,16 @@
+package chapter06;
+
+public class HottubOnCommand implements Command {
+    Hottub hottub;
+
+    public HottubOnCommand(Hottub hottub) {
+        this.hottub = hottub;
+    }
+
+    @Override
+    public void execute() {
+        hottub.on();
+        hottub.heat();
+        hottub.bubblesOn();
+    }
+}
