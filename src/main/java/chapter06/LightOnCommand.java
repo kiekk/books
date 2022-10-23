1598bae644dc359e3ea4230e17dbda195d1cd358
shyoon <dbstnsgh2@naver.com>
2022-10-23 20:19:19 +0900

    ch06-command-pattern: 조명 클래스 / 커맨드 클래스 추가

---
+++ src/main/java/chapter06/LightOnCommand.java
@@ -0,0 +1,14 @@
+package chapter06;
+
+public class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.on();
+    }
+}
