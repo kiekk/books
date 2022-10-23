5ded33dc1b6ea43e307fa90f13a576d6c57a3f64
shyoon <dbstnsgh2@naver.com>
2022-10-24 08:00:06 +0900

    ch05-singleton-pattern: 싱글톤 멀티 스레딩 문제 해결 - 2 (처음부터 인스턴스 생성)

--- src/main/java/chapter05/Singleton.java
+++ src/main/java/chapter05/Singleton.java
@@ -1,15 +1,12 @@
 package chapter05;
 
 public class Singleton {
-    private static Singleton uniqueInstance;
+    private static final Singleton uniqueInstance = new Singleton();
 
     private Singleton() {
     }
 
-    public static synchronized Singleton getInstance() {
-        if (uniqueInstance == null) {
-            uniqueInstance = new Singleton();
-        }
+    public static Singleton getInstance() {
         return uniqueInstance;
     }
 
