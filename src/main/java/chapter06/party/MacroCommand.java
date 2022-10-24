9538edf9e7efbe7cd3afb32a949cdc53087c6451
shyoon <dbstnsgh2@naver.com>
2022-10-24 09:16:36 +0900

    ch06-command-pattern: 매크로 커맨드 기능 추가

---
+++ src/main/java/chapter06/party/MacroCommand.java
@@ -0,0 +1,27 @@
+package chapter06.party;
+
+import chapter06.Command;
+
+public class MacroCommand implements Command {
+    Command[] commands;
+
+    public MacroCommand(Command[] commands) {
+        this.commands = commands;
+    }
+
+    public void execute() {
+        for (Command command : commands) {
+            command.execute();
+        }
+    }
+
+    /**
+     * NOTE:  these commands have to be done backwards to ensure
+     * proper undo functionality
+     */
+    public void undo() {
+        for (int i = commands.length - 1; i >= 0; i--) {
+            commands[i].undo();
+        }
+    }
+}
