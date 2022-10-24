91a938bc10f3bf249bdfce0f1ec419419e91ef93
shyoon <dbstnsgh2@naver.com>
2022-10-24 13:40:07 +0900

    ch09-iterator-pattern: List, Array 각각 순회

---
+++ src/main/java/chapter09/MenuTestDrive.java
@@ -0,0 +1,54 @@
+package chapter09;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MenuTestDrive {
+    public static void main(String[] args) {
+        printMenus();
+    }
+
+    public static void printMenus() {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
+
+        // 메소드명은 동일, return type 만 다르다.
+        List<MenuItem> breakfastItems = pancakeHouseMenu.getMenuItems();
+        MenuItem[] lunchItems = dinerMenu.getMenuItems();
+
+        // print with forEach
+        System.out.println("=== forEach ===");
+        breakfastItems.forEach(System.out::println);
+        Arrays.asList(lunchItems).forEach(System.out::println);
+        System.out.println("=== forEach ===");
+
+        // Using enhanced for loop
+        System.out.println("USING ENHANCED FOR");
+        for (MenuItem menuItem : breakfastItems) {
+            System.out.print(menuItem.getName());
+            System.out.println("\t\t" + menuItem.getPrice());
+            System.out.println("\t" + menuItem.getDescription());
+        }
+        for (MenuItem menuItem : lunchItems) {
+            System.out.print(menuItem.getName());
+            System.out.println("\t\t" + menuItem.getPrice());
+            System.out.println("\t" + menuItem.getDescription());
+        }
+
+        // Exposing implementation
+        System.out.println("USING FOR LOOPS");
+        for (int i = 0; i < breakfastItems.size(); i++) {
+            MenuItem menuItem = (MenuItem) breakfastItems.get(i);
+            System.out.print(menuItem.getName());
+            System.out.println("\t\t" + menuItem.getPrice());
+            System.out.println("\t" + menuItem.getDescription());
+        }
+
+        for (int i = 0; i < lunchItems.length; i++) {
+            MenuItem menuItem = lunchItems[i];
+            System.out.print(menuItem.getName());
+            System.out.println("\t\t" + menuItem.getPrice());
+            System.out.println("\t" + menuItem.getDescription());
+        }
+    }
+}
