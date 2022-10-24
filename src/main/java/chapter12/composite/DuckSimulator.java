660bfc10ce0ff9b2576f554e93c8f5697c661df0
shyoon <dbstnsgh2@naver.com>
2022-10-24 17:20:51 +0900

    ch12-combining-pattern: 데코레이터 패턴 - 오리가 소리 낸 횟수 기능 추가

--- src/main/java/chapter12/composite/DuckSimulator.java
+++ src/main/java/chapter12/composite/DuckSimulator.java
@@ -8,10 +8,10 @@ public class DuckSimulator {
     }
 
     void simulate() {
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable rubberDuck = new RubberDuck();
-        Quackable decoyDuck = new DecoyDuck();
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+        Quackable decoyDuck = new QuackCounter(new DecoyDuck());
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
         System.out.println("오리 시뮬레이션 게임");
@@ -21,6 +21,9 @@ public class DuckSimulator {
         simulate(rubberDuck);
         simulate(decoyDuck);
         simulate(gooseDuck);
+
+        // 거위가 소리 낸 횟수는 포함 X
+        System.out.println("오리가 소리 낸 횟수 : " + QuackCounter.getQuacks() + " 번");
     }
 
     void simulate(Quackable duck) {
