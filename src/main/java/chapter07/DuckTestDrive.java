83185c96d65d2ae18b90c8d6c9581956334f9cdc
shyoon <dbstnsgh2@naver.com>
2022-10-24 09:29:51 +0900

    ch07-adapter-pattern: 어댑터 패턴 적용 전

---
+++ src/main/java/chapter07/DuckTestDrive.java
@@ -0,0 +1,23 @@
+package chapter07;
+
+public class DuckTestDrive {
+    public static void main(String[] args) {
+        Duck duck = new MallardDuck();
+        Turkey turkey = new WildTurkey();
+
+        System.out.println("칠면조가 말하길");
+        turkey.gobble();
+        turkey.fly();
+
+        System.out.println("오리가 말하길");
+        testDuck(duck);
+
+        // 만약 칠면조 객체를 오리 객체 대신 사용해야 한다면?
+        // Duck duck = new WildTurkey(); // Error!
+    }
+
+    static void testDuck(Duck duck) {
+        duck.quack();
+        duck.fly();
+    }
+}
