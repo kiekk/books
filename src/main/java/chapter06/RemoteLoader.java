cce43cbb8f1b7f589a2c5e270999fced2db550a8
shyoon <dbstnsgh2@naver.com>
2022-10-24 09:06:52 +0900

    ch06-command-pattern: 선풍기 코드 테스트

--- src/main/java/chapter06/RemoteLoader.java
+++ src/main/java/chapter06/RemoteLoader.java
@@ -7,31 +7,34 @@ public class RemoteLoader {
         RemoteControl remoteControl = new RemoteControl();
 
         Light livingRoomLight = new Light("Living Room");
-        Light kitchenLight = new Light("Kitchen");
-        CeilingFan ceilingFan = new CeilingFan("Living Room");
-        GarageDoor garageDoor = new GarageDoor("Garage");
-        Stereo stereo = new Stereo("Living Room");
 
-        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
-        LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
-        LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
-
-        CeilingFanHighCommand ceilingFanOn = new CeilingFanHighCommand(ceilingFan);
-        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
-
-        GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
-        GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
-
-        StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
-        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+        LightOnCommand livingRoomLightOn =
+                new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff =
+                new LightOffCommand(livingRoomLight);
 
         remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-        remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
-        remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
-        remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
-        remoteControl.setCommand(4, garageDoorUp, garageDoorDown);
 
+        remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(0);
+        System.out.println(remoteControl);
+        remoteControl.undoButtonWasPushed();
+        remoteControl.offButtonWasPushed(0);
+        remoteControl.onButtonWasPushed(0);
+        System.out.println(remoteControl);
+        remoteControl.undoButtonWasPushed();
+
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
+
+        CeilingFanMediumCommand ceilingFanMedium =
+                new CeilingFanMediumCommand(ceilingFan);
+        CeilingFanHighCommand ceilingFanHigh =
+                new CeilingFanHighCommand(ceilingFan);
+        CeilingFanOffCommand ceilingFanOff =
+                new CeilingFanOffCommand(ceilingFan);
+
+        remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff);
+        remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff);
 
         remoteControl.onButtonWasPushed(0);
         remoteControl.offButtonWasPushed(0);
@@ -41,12 +44,5 @@ public class RemoteLoader {
         remoteControl.onButtonWasPushed(1);
         System.out.println(remoteControl);
         remoteControl.undoButtonWasPushed();
-
-        remoteControl.onButtonWasPushed(2);
-        remoteControl.offButtonWasPushed(2);
-        remoteControl.onButtonWasPushed(3);
-        remoteControl.offButtonWasPushed(3);
-        remoteControl.onButtonWasPushed(4);
-        remoteControl.offButtonWasPushed(4);
     }
 }
