aa94862994dd72d06f3b98eeddd6e05409313c26
shyoon <dbstnsgh2@naver.com>
2022-10-24 15:09:06 +0900

    ch09-composite-pattern: composite 패턴으로 iterator 패턴 개선

---
+++ src/main/java/chapter09/composite/Menu.java
@@ -0,0 +1,45 @@
+package chapter09.composite;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Menu extends MenuComponent {
+    ArrayList<MenuComponent> menuComponents = new ArrayList<>();
+    String name;
+    String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i) {
+        return menuComponents.get(i);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void print() {
+        System.out.print("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("---------------------");
+
+        for (MenuComponent menuComponent : menuComponents) {
+            menuComponent.print();
+        }
+    }
+}
