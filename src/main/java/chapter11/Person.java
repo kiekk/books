fb83f683cd7e324e593b24ecba4bcf92979d412a
shyoon <dbstnsgh2@naver.com>
2022-10-24 17:02:00 +0900

    ch11-proxy-pattern: 프록시 패턴 학습 - Dynamic Proxy

---
+++ src/main/java/chapter11/Person.java
@@ -0,0 +1,21 @@
+package chapter11;
+
+public interface Person {
+
+    String getName();
+
+    String getGender();
+
+    String getInterests();
+
+    int getGeekRating();
+
+    void setName(String name);
+
+    void setGender(String gender);
+
+    void setInterests(String interests);
+
+    void setGeekRating(int rating);
+
+}
