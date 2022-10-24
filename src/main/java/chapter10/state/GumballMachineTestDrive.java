eeb3e73402c52b0f3eeb88334f0056f241a33095
shyoon <dbstnsgh2@naver.com>
2022-10-24 15:47:03 +0900

    ch10-state-pattern: 보너스 상태 추가

--- src/main/java/chapter10/state/GumballMachineTestDrive.java
+++ src/main/java/chapter10/state/GumballMachineTestDrive.java
@@ -3,12 +3,15 @@ package chapter10.state;
 public class GumballMachineTestDrive {
 
     public static void main(String[] args) {
-        GumballMachine gumballMachine = new GumballMachine(2);
+        GumballMachine gumballMachine =
+                new GumballMachine(10);
 
         System.out.println(gumballMachine);
 
         gumballMachine.insertQuarter();
         gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
 
         System.out.println(gumballMachine);
 
@@ -17,7 +20,24 @@ public class GumballMachineTestDrive {
         gumballMachine.insertQuarter();
         gumballMachine.turnCrank();
 
-        gumballMachine.refill(5);
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
         gumballMachine.insertQuarter();
         gumballMachine.turnCrank();
 
