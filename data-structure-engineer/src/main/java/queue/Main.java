4753108d6b014a93eace620cc344b3b34830fb0d
shyoon <dbstnsgh2@naver.com>
2022-10-25 17:47:57 +0900

    engineer-queue: Queue 구현하기

---
+++ data-structure-engineer/src/main/java/queue/Main.java
@@ -0,0 +1,21 @@
+package queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> q = new Queue<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.isEmpty());
+        System.out.println(q.remove()); // Error!
+
+    }
+}
