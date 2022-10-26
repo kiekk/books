d55af2217727a500177b45fe460d73dfd67f6a5f
shyoon <dbstnsgh2@naver.com>
2022-10-26 11:53:37 +0900

    engineer-tree: Tree 구현 및 순회

---
+++ data-structure-engineer/src/main/java/tree/Main.java
@@ -0,0 +1,23 @@
+package tree;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree t = new Tree();
+        Node n4 = t.makeNode(null, 4, null);
+        Node n5 = t.makeNode(null, 5, null);
+        Node n2 = t.makeNode(n4, 2, n5);
+        Node n3 = t.makeNode(null, 3, null);
+        Node n1 = t.makeNode(n2, 1, n3);
+
+        t.setRoot(n1);
+
+        System.out.println("---in order---");
+        t.inorder(t.getRoot());
+
+        System.out.println("---pre order---");
+        t.preorder(t.getRoot());
+
+        System.out.println("---post order---");
+        t.postorder(t.getRoot());
+    }
+}
---
+++ data-structure-engineer/src/main/java/tree/Tree.java
@@ -0,0 +1,45 @@
+package tree;
+
+public class Tree {
+    public Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public Node makeNode(Node left, int data, Node right) {
+        Node node = new Node();
+        node.data = data;
+        node.left = left;
+        node.right = right;
+        return node;
+    }
+
+    public void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.println(node.data);
+            inorder(node.right);
+        }
+    }
+
+    public void preorder(Node node) {
+        if (node != null) {
+            System.out.println(node.data);
+            preorder(node.left);
+            preorder(node.right);
+        }
+    }
+
+    public void postorder(Node node) {
+        if (node != null) {
+            postorder(node.left);
+            postorder(node.right);
+            System.out.println(node.data);
+        }
+    }
+}
