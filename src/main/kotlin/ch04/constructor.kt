fe78c40537c31ff862938ec117b8e26276751d49
shyoon <dbstnsgh2@naver.com>
2022-11-01 08:26:13 +0900

    ch04: init - 2

--- src/main/kotlin/ch04/constructor.kt
+++ src/main/kotlin/ch04/constructor.kt
@@ -2,6 +2,8 @@ class Person2(firstName: String, familyName: String) {
     var fullName = "$firstName $familyName"
 
     init {
+        // init 블록 안에서는 return 키워드 사용 불가능
+//        if(firstName.isEmpty()&& familyName.isEmpty()) return
         println("Created new Person instance: $fullName")
     }
 }
