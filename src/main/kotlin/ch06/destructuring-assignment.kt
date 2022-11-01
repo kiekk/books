06e220ec8930e2787670675cec57fbfc43d6b0cf
shyoon <dbstnsgh2@naver.com>
2022-11-01 17:51:23 +0900

    ch06: destructuring assignment - 3

--- src/main/kotlin/ch06/destructuring-assignment.kt
+++ src/main/kotlin/ch06/destructuring-assignment.kt
@@ -1,14 +1,14 @@
 fun main() {
     val person = Person4("John", "Doe", 25)
 
-    val (familyName, age) = person
-    // 파라미터 생략시 순서대로 매핑되기 때문에 의도한 결과대로 출력되지 않는다.
+    val (_, familyName, age) = person
+    // 사용하지 않는 파라미터의 위치에 _ 를 작성
 
     println("person.firstName: ${person.firstName}")
     println("person.familyName: ${person.familyName}")
     println("person.age: ${person.age}")
 
 //    println("firstName: ${firstName}")
-    println("familyName: ${familyName}") // Doe 가 출력되어야 하지만 John 이 출력
-    println("age: ${age}") // 25 가 출력되어야 하지만 Doe 가 출력
+    println("familyName: ${familyName}") // Doe
+    println("age: ${age}") // 25
 }
