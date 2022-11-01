121686cef4924c0898d95336fed678e00afe8d5b
shyoon <dbstnsgh2@naver.com>
2022-11-01 17:12:48 +0900

    ch05: enum class & when

---
+++ src/main/kotlin/ch06/enum-when.kt
@@ -0,0 +1,10 @@
+enum class Direction {
+    NORTH, SOUTH, WEST, EAST
+}
+
+fun rotateClockWise(direction: Direction) = when (direction) {
+    Direction.NORTH -> Direction.EAST
+    Direction.EAST -> Direction.SOUTH
+    Direction.SOUTH -> Direction.WEST
+    Direction.WEST -> Direction.NORTH
+}
