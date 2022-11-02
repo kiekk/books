b0cb534d05f9a5737a4f3c858150570ebf84ece9
shyoon <dbstnsgh2@naver.com>
2022-11-03 08:51:49 +0900

    ch09: 선언 지점 변성 - 2

--- src/main/kotlin/ch09/declaration-site-variance.kt
+++ src/main/kotlin/ch09/declaration-site-variance.kt
@@ -1,14 +1,15 @@
-interface List<T> {
+// 무공변 상태를 공변 상태로 만들기 위해서는 out 키워드를 사용
+interface List2<out T> {
     val size: Int
     fun get(index: Int): T
 }
 
-class ListByArray<T>(private vararg val items: T) : List<T> {
+class ListByArray<T>(private vararg val items: T) : List2<T> {
     override val size: Int get() = items.size
     override fun get(index: Int) = items[index]
 }
 
-fun <T> concat(list1: List<T>, list2: List<T>) = object : List<T> {
+fun <T> concat(list1: List2<T>, list2: List2<T>) = object : List2<T> {
     override val size: Int
         get() = list1.size + list2.size
 
@@ -24,5 +25,5 @@ fun <T> concat(list1: List<T>, list2: List<T>) = object : List<T> {
 fun main() {
     val numbers = ListByArray<Number>(1, 2.5, 3f)
     val integers = ListByArray(10, 30, 30)
-    val result = concat(numbers, integers) // error: type mismatch
+    val result = concat(numbers, integers) // Not Error
 }
