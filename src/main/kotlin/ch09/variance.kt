49875a044dfec444fbef36ca7faa25992c4a0a34
shyoon <dbstnsgh2@naver.com>
2022-11-03 08:45:41 +0900

    ch09: variance - 3. 반공변 (contravariant)

--- src/main/kotlin/ch09/variance.kt
+++ src/main/kotlin/ch09/variance.kt
@@ -7,4 +7,16 @@ fun main() {
     val stringProducer: () -> String = { "hello" }
     val anyProducer: () -> Any = stringProducer
     println(anyProducer())
+
+    // 반공변 상태 (contravariant)
+    val anyConsumer: (Any) -> Unit = { println(it) }
+    val stringConsumer: (String) -> Unit = anyConsumer
+    stringConsumer("hello")
+
+    // X<T>
+    // X 가 생산자 역할을 할 경우: T 를 공변적으로 선언할 수 있고,
+    // A 가 B 의 하위 타입이면 X<A> 도 X<B> 의 하위 타입이 된다.
+    // X 가 소비자 역할을 할 경우: T 를 반공변적으로 선언할 수 있고,
+    // B 가 A 의 하위 타입이면 X<A> 가 X<B> 의 하위 타입이 된다.
+    // 나머지는 무공변 상태.
 }
