b9c322f963dd88f21a562f2da4e5534ff942d41f
shyoon <dbstnsgh2@naver.com>
2022-11-03 09:11:24 +0900

    ch09: type alias - 2

--- src/main/kotlin/ch09/type-alias.kt
+++ src/main/kotlin/ch09/type-alias.kt
@@ -3,6 +3,15 @@ typealias IntMap = HashMap<Int, Int>
 
 fun readFirst(filter: IntPredicate) = generateSequence { readLine()?.toIntOrNull() }.firstOrNull(filter)
 
+// type alias 를 사용하여 내포된 클래스를 더 축약하
+sealed class Status {
+    object Success : Status()
+    class Error(val message: String) : Status()
+}
+
+typealias StSuccess = Status.Success
+typealias StError = Status.Error
+
 fun main() {
     val map = IntMap().also {
         it[1] = 2
