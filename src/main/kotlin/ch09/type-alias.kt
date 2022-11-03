2c3784c023dde909e1f68e7a9823d819660196bc
shyoon <dbstnsgh2@naver.com>
2022-11-03 09:12:23 +0900

    ch09: type alias - 3

--- src/main/kotlin/ch09/type-alias.kt
+++ src/main/kotlin/ch09/type-alias.kt
@@ -12,6 +12,10 @@ sealed class Status {
 typealias StSuccess = Status.Success
 typealias StError = Status.Error
 
+// type alias 에 제네릭 사용
+typealias ThisPredicate<T> = T.() -> Boolean
+typealias MultiMap<K, V> = Map<K, Collection<V>>
+
 fun main() {
     val map = IntMap().also {
         it[1] = 2
