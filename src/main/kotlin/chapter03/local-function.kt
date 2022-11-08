2e25df300e6290444260471efb256837903d21ff
shyoon <dbstnsgh2@naver.com>
2022-11-08 18:15:44 +0900

    ch03: function - 10.2 (로컬 함수로 코드 중복 해결)

--- src/main/kotlin/chapter03/local-function.kt
+++ src/main/kotlin/chapter03/local-function.kt
@@ -1,18 +1,17 @@
 class User(val id: Int, val name: String, val address: String)
 
-// validate 코드가 중복
 fun saveUser(user: User) {
-    if (user.name.isEmpty()) {
-        throw IllegalArgumentException(
-            "Can't save user ${user.id}: empty Name"
-        )
+
+    fun validate(user: User, value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException(
+                "Can't save user ${user.id}: empty $fieldName"
+            )
+        }
     }
 
-    if (user.address.isEmpty()) {
-        throw IllegalArgumentException(
-            "Can't save user ${user.id}: empty Address"
-        )
-    }
+    validate(user, user.name, "Name")
+    validate(user, user.address, "Address")
 }
 
 fun main() {
