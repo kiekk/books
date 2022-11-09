cdfcca721328ca3edc02055c5a51b38856f763f9
shyoon <dbstnsgh2@naver.com>
2022-11-09 16:44:59 +0900

    ch05: buildString API를 사용하여 코드 리팩토링

--- src/main/kotlin/chapter05/apply.kt
+++ src/main/kotlin/chapter05/apply.kt
@@ -1,11 +1,12 @@
 package chapter05
 
-fun alphabet2() = StringBuilder().apply {
+// StringBuilder 를 사용해 String 을 생성하는 기능을 지원하는 buildString API 를 사용하여 리팩토링
+fun alphabet2() = buildString {
     for (letter in 'A'..'Z') {
         append(letter)
     }
     append("\nNow I know the alphabet!")
-}.toString()
+}
 
 fun main() {
     println(alphabet2())
