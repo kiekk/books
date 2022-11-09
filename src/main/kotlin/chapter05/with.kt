f4f5d05ed08b1d4f87c3942ea9121ed5d9d86105
shyoon <dbstnsgh2@naver.com>
2022-11-09 16:41:47 +0900

    ch05: with - 2 (알파벳 생성 코드 리팩토링)

--- src/main/kotlin/chapter05/with.kt
+++ src/main/kotlin/chapter05/with.kt
@@ -1,12 +1,18 @@
 package chapter05
 
+// with 함수를 사용하여 코드 리팩토링
 fun alphabet(): String {
-    val result = StringBuilder()
-    for (letter in 'A'..'Z') {
-        result.append(letter)
+    val stringBuilder = StringBuilder()
+    // with 함수 내부에서 사용하려는 수신 객체를 지정한다.
+    return with(stringBuilder) {
+        for (letter in 'A'..'Z') {
+            // 이 때의 this 는  stringBuilder 를 가리킨다.
+            this.append(letter)
+        }
+        // this 는 생략이 가능하다.
+        append("\nNow I know the alphabet!")
+        this.toString()
     }
-    result.append("\nNow I know the alphabet!")
-    return result.toString()
 }
 
 fun main() {
