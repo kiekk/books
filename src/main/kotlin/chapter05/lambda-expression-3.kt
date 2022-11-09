d44ee64159c36218822acae628640e0d0d4db01d
shyoon <dbstnsgh2@naver.com>
2022-11-09 14:22:37 +0900

    ch05: Lambda Expression - 4

--- src/main/kotlin/chapter05/lambda-expression-3.kt
+++ src/main/kotlin/chapter05/lambda-expression-3.kt
@@ -6,4 +6,16 @@ fun main() {
         // 인자로 람다식 전달
         transform = { p: Person -> p.name })
     println(names)
+
+    // 람다식 괄호 밖으로 빼기
+    val names2 = people.joinToString(" ") { p: Person -> p.name }
+    println(names2)
+
+    // 타입 생략 가능
+    val names3 = people.joinToString(" ") { p -> p.name }
+    println(names3)
+
+    // 디폴트 파라미터 이름 it 사용
+    val names4 = people.joinToString(" ") { it.name }
+    println(names4)
 }
