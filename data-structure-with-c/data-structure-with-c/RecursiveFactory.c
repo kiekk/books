1d0f63d317e5a16667cc1d500b962f4db427e65b
soonho <dbstnsgh2@naver.com>
2022-11-10 20:27:51 +0900

    data-structure-with-c: 피보나치 수열 - 1

--- data-structure-with-c/data-structure-with-c/RecursiveFactory.c
+++ data-structure-with-c/data-structure-with-c/RecursiveFactory.c
@@ -9,10 +9,10 @@ int Factorial(int n) {
 	}
 }
 
-int main(void) {
+/*int main(void) {
 	printf("1! = %d \n", Factorial(1));
 	printf("2! = %d \n", Factorial(2));
 	printf("3! = %d \n", Factorial(3));
 	printf("4! = %d \n", Factorial(4));
 	printf("5! = %d \n", Factorial(5));
-}
+}*/
