2bbe58bcd34eae74f47d80e294b33484f9a7feed
shyoon <dbstnsgh2@naver.com>
2022-11-11 11:15:54 +0900

    ch07: 산술 연산자 오버로딩 - 3 (두 피연산자의 타입이 다른 연산자 정의)

---
+++ src/main/kotlin/chapter07/operation-overloading-3.kt
@@ -0,0 +1,11 @@
+package chapter07
+
+operator fun Point.times(scale: Double): Point {
+    return Point((x * scale).toInt(), (y * scale).toInt())
+}
+
+fun main() {
+    val p = Point(10, 20)
+    println(p * 1.5)
+}
+
