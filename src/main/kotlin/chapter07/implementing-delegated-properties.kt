642cb9b0d2d7b7cd4882b628a5ab3362c2462ece
shyoon <dbstnsgh2@naver.com>
2022-11-11 14:05:04 +0900

    ch07: Implemeting Properties - 4 (표준 라이브러리 Delegated.observable 사용하여 프로퍼티 변경 통지)

--- src/main/kotlin/chapter07/implementing-delegated-properties.kt
+++ src/main/kotlin/chapter07/implementing-delegated-properties.kt
@@ -2,6 +2,7 @@ package chapter07
 
 import java.beans.PropertyChangeListener
 import java.beans.PropertyChangeSupport
+import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
 open class PropertyChangeAware {
@@ -16,24 +17,16 @@ open class PropertyChangeAware {
     }
 }
 
-class ObservableProperty(
-    var propValue: Int, val changeSupport: PropertyChangeSupport
-) {
-    operator fun getValue(p: Person5, prop: KProperty<*>): Int = propValue
-
-    operator fun setValue(p: Person5, prop: KProperty<*>, newValue: Int) {
-        val oldValue = propValue
-        propValue = newValue
-        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
-    }
-}
-
 class Person5(
     val name: String, age: Int, salary: Int
 ) : PropertyChangeAware() {
 
-    var age: Int by ObservableProperty(age, changeSupport)
-    var salary: Int by ObservableProperty(salary, changeSupport)
+    private val observer = { prop: KProperty<*>, oldValue: Int, newValue: Int ->
+        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
+    }
+
+    var age: Int by Delegates.observable(age, observer)
+    var salary: Int by Delegates.observable(salary, observer)
 }
 
 fun main() {
