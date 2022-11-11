9f5c0dd37514eb0bd80a4870181fa44b2483ab8e
shyoon <dbstnsgh2@naver.com>
2022-11-11 14:08:02 +0900

    ch07: 프로퍼티 값을 맵에 저장 - 1

---
+++ src/main/kotlin/chapter07/expando-object.kt
@@ -0,0 +1,20 @@
+package chapter07
+
+class Person6 {
+    private val _attributes = hashMapOf<String, String>()
+
+    fun setAttribute(attrName: String, value: String) {
+        _attributes[attrName] = value
+    }
+
+    val name: String
+        get() = _attributes["name"]!!
+}
+
+fun main() {
+    val p = Person6()
+    val data = mapOf("name" to "Dmitry", "company" to "JetBrains")
+    for ((attrName, value) in data)
+        p.setAttribute(attrName, value)
+    println(p.name)
+}
