c9c1a4f264f5b58681b9610b967775f4b4e75548
soonho <dbstnsgh2@naver.com>
2022-11-13 11:13:00 +0900

    data-structure-with-c: Merge Sort

--- data-structure-with-c/chapter10/chapter10/HeapSort.c
+++ data-structure-with-c/chapter10/chapter10/HeapSort.c
@@ -23,16 +23,16 @@ void HeapSort(int arr[], int n, PriorityComp pc)
 		arr[i] = HDelete(&heap);
 }
 
-int main(void)
-{
-	int arr[4] = { 3, 4, 2, 1 };
-	int i;
-
-	HeapSort(arr, sizeof(arr) / sizeof(int), PriComp);
-
-	for (i = 0; i < 4; i++)
-		printf("%d ", arr[i]);
-
-	printf("\n");
-	return 0;
-}
+//int main(void)
+//{
+//	int arr[4] = { 3, 4, 2, 1 };
+//	int i;
+//
+//	HeapSort(arr, sizeof(arr) / sizeof(int), PriComp);
+//
+//	for (i = 0; i < 4; i++)
+//		printf("%d ", arr[i]);
+//
+//	printf("\n");
+//	return 0;
+//}
