c715f9555300e5ac633960b59721acaa45e31808
soonho <dbstnsgh2@naver.com>
2022-11-13 11:57:49 +0900

    data-structure-with-c: Quick Sort

--- data-structure-with-c/chapter10/chapter10/MergeSort.c
+++ data-structure-with-c/chapter10/chapter10/MergeSort.c
@@ -55,17 +55,17 @@ void MergeSort(int arr[], int left, int right)
 	}
 }
 
-int main(void)
-{
-	int arr[7] = { 3, 2, 4, 1, 7, 6, 5 };
-	int i;
-
-	// 배열 arr의 전체 영역 정렬 
-	MergeSort(arr, 0, sizeof(arr) / sizeof(int) - 1);
-
-	for (i = 0; i < 7; i++)
-		printf("%d ", arr[i]);
-
-	printf("\n");
-	return 0;
-}
+//int main(void)
+//{
+//	int arr[7] = { 3, 2, 4, 1, 7, 6, 5 };
+//	int i;
+//
+//	// 배열 arr의 전체 영역 정렬 
+//	MergeSort(arr, 0, sizeof(arr) / sizeof(int) - 1);
+//
+//	for (i = 0; i < 7; i++)
+//		printf("%d ", arr[i]);
+//
+//	printf("\n");
+//	return 0;
+//}
