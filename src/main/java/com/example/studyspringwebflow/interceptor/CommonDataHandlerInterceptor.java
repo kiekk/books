f5cf2017ce09710761ab5907add2ea86ca07d5b6
soonho <dbstnsgh2@naver.com>
2022-11-13 18:52:01 +0900

    add formatter, converter, interceptor

--- src/main/java/com/example/studyspringwebflow/interceptor/CommonDataHandlerInterceptor.java
+++ src/main/java/com/example/studyspringwebflow/interceptor/CommonDataHandlerInterceptor.java
@@ -1,6 +1,20 @@
 package com.example.studyspringwebflow.interceptor;
 
+import com.example.studyspringwebflow.service.BookstoreService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
 public class CommonDataHandlerInterceptor implements HandlerInterceptor {
+
+    private final BookstoreService bookstoreService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.setAttribute("randomBooks", this.bookstoreService.findRandomBooks());
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
 }
