23e0a11a932d352b8e10eac2b8b64ca78e40c600
soonho <dbstnsgh2@naver.com>
2022-12-05 21:17:33 +0900

    Calculator Test 개선 - 2

--- src/main/java/com/example/javajigi/CalculatorTest.java
+++ src/main/java/com/example/javajigi/CalculatorTest.java
@@ -1,12 +1,29 @@
 package com.example.javajigi;
 
 public class CalculatorTest {
-    // 테스트용 클래스를 별도로 작성하여 프로덕션 코드와 테스트 코드를 분리
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add(3, 4));
-        System.out.println(calculator.subtract(5, 4));
-        System.out.println(calculator.multiply(2, 6));
-        System.out.println(calculator.divide(8, 4));
+
+        add(calculator);
+        subtract(calculator);
+        multiply(calculator);
+        divide(calculator);
+    }
+
+    // 프로덕션 코드별 별도의 테스트 메소드를 작성하여 테스트 코드를 기능별로 분리
+    public static void add(Calculator calculator) {
+        System.out.println(calculator.add(9, 3));
+    }
+
+    public static void subtract(Calculator calculator) {
+        System.out.println(calculator.subtract(9, 3));
+    }
+
+    public static void multiply(Calculator calculator) {
+        System.out.println(calculator.multiply(9, 3));
+    }
+
+    public static void divide(Calculator calculator) {
+        System.out.println(calculator.divide(9, 3));
     }
 }
