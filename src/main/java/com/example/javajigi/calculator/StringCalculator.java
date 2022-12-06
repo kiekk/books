515962c832fe29106362b302d13b894eac7e1ba5
soonho <dbstnsgh2@naver.com>
2022-12-06 21:49:56 +0900

    문자열 계산기 리팩토링 - 5

--- src/main/java/com/example/javajigi/calculator/StringCalculator.java
+++ src/main/java/com/example/javajigi/calculator/StringCalculator.java
@@ -1,5 +1,6 @@
 package com.example.javajigi.calculator;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,12 +11,13 @@ public class StringCalculator {
     // 2. 텍스트 -> 숫자 변환 코드 분리
     // 3. null, empty 체크 로직 분리
     // 4. split 코드 분리
+    // 덧셈 메소드 안에 parseInt 로직 분리
     int add(String text) {
         if (nullOrEmpty(text)) {
             return 0;
         }
 
-        return sum(split(text));
+        return sum(toInts(split(text)));
     }
 
     private String[] split(String text) {
@@ -31,10 +33,10 @@ public class StringCalculator {
         return text == null || text.isEmpty();
     }
 
-    private int sum(String[] tokens) {
+    private int sum(int[] numbers) {
         int sum = 0;
-        for (String token : tokens) {
-            sum += parseInt(token);
+        for (int number : numbers) {
+            sum += number;
         }
         return sum;
     }
@@ -45,5 +47,14 @@ public class StringCalculator {
             throw new RuntimeException("양수만 입력 가능합니다.");
         }
         return intValue;
+    }
+
+    private int[] toInts(String[] tokens) {
+        int tokenLength = tokens.length;
+        int[] numbers = new int[tokenLength];
+        for (int i = 0; i < tokenLength; i++) {
+            numbers[i] = parseInt(tokens[i]);
+        }
+        return numbers;
     }
 }
