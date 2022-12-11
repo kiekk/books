c65d18602b972d2614652e1d75713b574944cd48
soonho <dbstnsgh2@naver.com>
2022-12-11 10:05:30 +0900

    refactoring: add controller - CreateUserController / ListUserController / LoginUserController

---
+++ src/main/java/com/example/javajigi/controller/LoginUserController.java
@@ -0,0 +1,20 @@
+package com.example.javajigi.controller;
+
+import com.example.javajigi.db.DataBase;
+import com.example.javajigi.http.HttpRequest;
+import com.example.javajigi.http.HttpResponse;
+import com.example.javajigi.model.User;
+
+public class LoginUserController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+        User user = DataBase.findUserById(request.getParameter("userId"));
+        if (user != null && user.login(request.getParameter("password"))) {
+            response.addHeader("Set-Cookie", "logined=true");
+        } else {
+            response.sendRedirect("/user/login_failed.html");
+        }
+    }
+
+}
