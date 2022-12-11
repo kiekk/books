29a02e3492645c344a1987961ff9edf0a738808b
soonho <dbstnsgh2@naver.com>
2022-12-11 10:50:03 +0900

    servlet-jsp: HttpServlet 기반인 Controller 로 변경

---
+++ src/main/java/com/example/javajigi/controller/ListUserController.java
@@ -0,0 +1,29 @@
+package com.example.javajigi.controller;
+
+import com.example.javajigi.db.DataBase;
+import com.example.javajigi.util.UserSessionUtils;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users")
+public class ListUserController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!UserSessionUtils.isLogined(req.getSession())) {
+            resp.sendRedirect("/users/loginForm");
+            return;
+        }
+
+        req.setAttribute("users", DataBase.findAll());
+
+        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+        rd.forward(req, resp);
+    }
+}
