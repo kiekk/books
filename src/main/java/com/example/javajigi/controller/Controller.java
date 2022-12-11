04b3278faefea2a0458ac7ccb905a72e658377ad
soonho <dbstnsgh2@naver.com>
2022-12-11 10:43:39 +0900

    servlet-jsp: 이전 예제 코드 제거

--- src/main/java/com/example/javajigi/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.example.javajigi.controller;
-
-import com.example.javajigi.http.HttpRequest;
-import com.example.javajigi.http.HttpResponse;
-
-public interface Controller {
-    void service(HttpRequest request, HttpResponse response);
-}
