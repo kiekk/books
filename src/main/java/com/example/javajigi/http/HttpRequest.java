a7efee4bcf8426761cc39be287e787c6217a1340
soonho <dbstnsgh2@naver.com>
2022-12-11 09:05:20 +0900

    refactoring: add HttpRequest

---
+++ src/main/java/com/example/javajigi/http/HttpRequest.java
@@ -0,0 +1,70 @@
+package com.example.javajigi.http;
+
+import com.example.javajigi.util.HttpRequestUtils;
+import com.example.javajigi.util.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+
+    private final Map<String, String> headers = new HashMap<>();
+    private Map<String, String> params = new HashMap<>();
+    private RequestLine requestLine;
+
+    public HttpRequest(InputStream in) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            log.debug("request line : {}", line);
+            if (line == null) {
+                return;
+            }
+
+            requestLine = new RequestLine(line);
+
+            line = br.readLine();
+
+            while (!line.equals("")) {
+                log.debug("header : {}", line);
+                String[] tokens = line.split(":");
+                headers.put(tokens[0].trim(), tokens[1].trim());
+                line = br.readLine();
+            }
+
+            if (requestLine.isPost()) {
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            } else {
+                params = requestLine.getParams();
+            }
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public HttpMethod getMethod() {
+        return requestLine.getMethod();
+    }
+
+    public String getPath() {
+        return requestLine.getPath();
+    }
+
+    public String getHeader(String name) {
+        return headers.get(name);
+    }
+
+    public String getParameter(String name) {
+        return params.get(name);
+    }
+
+}
