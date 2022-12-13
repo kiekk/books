48724f6fe633b61e7621bd272f993f6da703304e
soonho <dbstnsgh2@naver.com>
2022-12-13 20:32:38 +0900

    ajax: 질문 목록 데이터 조회

--- src/main/java/com/example/javajigi/controller/user/HomeController.java
+++ src/main/java/com/example/javajigi/controller/user/HomeController.java
@@ -1,7 +1,7 @@
 package com.example.javajigi.controller.user;
 
 import com.example.javajigi.controller.Controller;
-import com.example.javajigi.db.DataBase;
+import com.example.javajigi.dao.QuestionDao;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +10,8 @@ public class HomeController implements Controller {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        request.setAttribute("users", DataBase.findAll());
+        QuestionDao questionDao = new QuestionDao();
+        request.setAttribute("questions", questionDao.findAll());
         return "/home.jsp";
     }
 
