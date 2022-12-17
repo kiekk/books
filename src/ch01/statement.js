0e276b6edf47d7f0cdde7933779f359524f09423
soonho <dbstnsgh2@naver.com>
2022-12-17 19:49:34 +0900

    statement 기능 추가 - 9. htmlStatement 추가

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -1,7 +1,12 @@
 export {statement};
 import createStatementData from "./createStatementData";
 
-// HTML 버전 기능 추가를 위한 함수 분리
+function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency', currency: 'USD', minimumFractionDigits: 2
+    }).format(aNumber / 100);
+}
+
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
@@ -17,10 +22,22 @@ function renderPlainText(data) {
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency', currency: 'USD', minimumFractionDigits: 2
-        }).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
     }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
 }
