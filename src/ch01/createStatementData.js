ff1e7374ae433164f90d9052bcc75729b4003126
soonho <dbstnsgh2@naver.com>
2022-12-17 19:58:26 +0900

    createStatementData 리팩토링 - 1. 공연료 계산기 class 추가

--- src/ch01/createStatementData.js
+++ src/ch01/createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+}
+
 export default function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -7,6 +14,7 @@ export default function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
