8d9f23b42d267382bd95ccefe8486484c52b7ccc
soonho <dbstnsgh2@naver.com>
2022-12-17 19:27:38 +0900

    statement 기능 추가 - 1. 파라미터 변경

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -2,13 +2,16 @@ export {statement};
 
 // HTML 버전 기능 추가를 위한 함수 분리
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         // 청구 내역 출력
         result += ` ${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience} 석)\n`;
     }
@@ -19,7 +22,7 @@ function renderPlainText(invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
@@ -27,7 +30,7 @@ function renderPlainText(invoice, plays) {
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             volumeCredits += volumeCreditsFor(perf);
         }
         return volumeCredits;
