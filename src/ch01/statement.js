6c2bab34ed41debdb56916f870153f9f88b2f249
soonho <dbstnsgh2@naver.com>
2022-12-17 17:35:10 +0900

    statement 리팩토링 - 2. 중첨 함수로 변경

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -29,29 +29,29 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
-}
 
-// 함수 추출하기
-function amountFor(aPerformance, play) {
-    let result = 0;
+    // 중첩 함수로 선언
+    function amountFor(aPerformance, play) {
+        let result = 0;
 
-    switch (play.type) {
-        case 'tragedy': // 비극
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 10000 * (aPerformance.audience - 30);
-            }
-            break;
-        case 'comedy': // 희극
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+        switch (play.type) {
+            case 'tragedy': // 비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 10000 * (aPerformance.audience - 30);
+                }
+                break;
+            case 'comedy': // 희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }
+
+        return result;
     }
-
-    return result;
 }
