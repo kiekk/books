64257620ddac3bb4098d12bfe34aab0e6bad94a8
soonho <dbstnsgh2@naver.com>
2022-12-17 17:57:42 +0900

    statement 리팩토링 - 11. 함수 추출 및 변수 인라인

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -2,20 +2,25 @@ export {statement};
 
 // 공연료 청구 프로그램
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
         // 청구 내역 출력
         result += ` ${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience} 석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 
-    // 함수 추출 -> 변수 인라인
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
+
     function totalVolumeCredits() {
         let volumeCredits = 0;
         for (let perf of invoice.performances) {
