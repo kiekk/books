3b11988b9dbc9cc6a13e45f28da2383a3e4163a3
soonho <dbstnsgh2@naver.com>
2022-12-17 17:24:44 +0900

    statement 리팩토링 - 1. 함수 추출하기

--- src/ch01/statement.js
+++ src/ch01/statement.js
@@ -12,25 +12,7 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID]
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case 'tragedy': // 비극
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 10000 * (perf.audience - 30);
-                }
-                break;
-            case 'comedy': // 희극
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -46,5 +28,30 @@ function statement(invoice, plays) {
 
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
+    return result;
+}
+
+// 함수 추출하기
+function amountFor(aPerformance, play) {
+    let result = 0;
+
+    switch (play.type) {
+        case 'tragedy': // 비극
+            result = 40000;
+            if (aPerformance.audience > 30) {
+                result += 10000 * (aPerformance.audience - 30);
+            }
+            break;
+        case 'comedy': // 희극
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+            result += 300 * aPerformance.audience;
+            break;
+        default:
+            throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
     return result;
 }
