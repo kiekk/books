6644c0c648a7d03f17ea03812220faecd655d865
soonho <dbstnsgh2@naver.com>
2022-12-18 12:14:36 +0900

    ch06: SplitPhase 리팩토링 - 3. 중간 데이터 계산 로직 함수 추출

--- src/ch06/SplitPhase.js
+++ src/ch06/SplitPhase.js
@@ -1,12 +1,16 @@
 export function priceOrder(product, quantity, shippingMethod) {
-    const basePrice = product.basePrice * quantity;
+    const priceData = calculatePriceData(product, quantity);
 
+    return applyingShippingCost(priceData, shippingMethod);
+}
+
+// 중간 데이터 계산 로직 함수 추출
+function calculatePriceData(product, quantity) {
+    const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
 
-    const priceData = {basePrice, quantity, discount};
-
-    return applyingShippingCost(priceData, shippingMethod);
+    return {basePrice, quantity, discount};
 }
 
 // 배송비 계산 부분 함수 추출
