b8e7483c007e8bf5fe4d9c8a60147420e9be1918
soonho <dbstnsgh2@naver.com>
2022-12-18 10:27:48 +0900

    ch06: InlineVariable 리팩토링 - 1. 변수 인라인하기

--- src/ch06/InlineVariable.js
+++ src/ch06/InlineVariable.js
@@ -1,4 +1,5 @@
+// 변수 추출의 반대 <-> 인라인 변수
+// 불필요한 변수가 있다면 인라인하여 변수를 제거한다.
 export function isDeliveryFree(anOrder) {
-    let basePrice = (anOrder.basePrice > 1000);
-    return basePrice;
+    return (anOrder.basePrice > 1000);
 }
