16e57c35e51117fd2418d034425a0408b7b63441
soonho <dbstnsgh2@naver.com>
2022-12-18 10:15:00 +0900

    ch06: ExtractVariable 리팩토링 - 1. 변수 추출하기

--- src/ch06/ExtractVariable.js
+++ src/ch06/ExtractVariable.js
@@ -1,5 +1,9 @@
+// 변수 인라인의 반대 <-> 변수 추출
+// 가독성 / 디버깅 향상
 export function price(order) {
-    return order.quantity * order.itemPrice
-        - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05
-        + Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    // 각 단계별로 변수로 추출
+    let basePrice = order.quantity * order.itemPrice;
+    let quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    let shippingCost = Math.min(basePrice * 0.1, 100);
+    return basePrice - quantityDiscount + shippingCost;
 }
