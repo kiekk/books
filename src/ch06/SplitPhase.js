c77d3b21bdb5045c6440d1260fbfcec8197491a6
soonho <dbstnsgh2@naver.com>
2022-12-18 12:11:37 +0900

    ch06: SplitPhase 리팩토링 - 1. 배송비 계산 로직 함수 추출

--- src/ch06/SplitPhase.js
+++ src/ch06/SplitPhase.js
@@ -4,10 +4,13 @@ export function priceOrder(product, quantity, shippingMethod) {
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
 
+    return applyingShippingCost(basePrice, shippingMethod, quantity, discount);
+}
+
+// 배송비 계산 부분 함수 추출
+function applyingShippingCost(basePrice, shippingMethod, quantity, discount) {
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-
     const shippingCost = quantity * shippingPerCase;
-
     return basePrice - discount + shippingCost;
 }
