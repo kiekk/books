92161bdececa9c94d13a009d88e10d7e5c80e582
soonho <dbstnsgh2@naver.com>
2022-12-18 10:17:24 +0900

    ch06: ExtractVariable 리팩토링 - 2. 추출한 변수들을 클래스로 묶어 관리

---
+++ src/ch06/ExtractClass.js
@@ -0,0 +1,32 @@
+// 추출한 변수들을 하나의 클래스로 관리
+export class Order {
+    constructor(aRecord) {
+        this._data = aRecord;
+    }
+
+    get quantity() {
+        return this._data.quantity;
+    }
+
+    get itemPrice() {
+        return this._data.itemPrice;
+    }
+
+    get price() {
+        return this.basePrice -
+            this.quantityDiscount +
+            this.shipping;
+    }
+
+    get shipping() {
+        return Math.min(this.basePrice * 0.1, 100);
+    }
+
+    get quantityDiscount() {
+        return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+    }
+
+    get basePrice() {
+        return this.quantity * this.itemPrice;
+    }
+}
