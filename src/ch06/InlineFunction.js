959a209b2252a246f98bcfdbf835d1c99b3efc9c
soonho <dbstnsgh2@naver.com>
2022-12-18 10:09:59 +0900

    ch06: InlineFunction 리팩토링 - 1. 함수 인라인하기

--- src/ch06/InlineFunction.js
+++ src/ch06/InlineFunction.js
@@ -1,7 +1,5 @@
+// 함수 추출의 반대 <-> 함수 인라인
+// 불필요하게 함수가 분리되어 있다면 함수 인라인으로 정리
 export function rating(aDriver) {
-    return getRating(aDriver) ? 2 : 1;
-}
-
-function getRating(aDriver) {
-    return aDriver.numberOfLateDeliveries > 5;
+    return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
