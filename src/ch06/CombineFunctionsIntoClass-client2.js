d443dffc94569ea008cf28559c25b92ee7897a58
soonho <dbstnsgh2@naver.com>
2022-12-18 11:01:53 +0900

    ch06: Combine Functions Into Class 리팩토링 - 4. 추출한 taxableCharge 함수 클래스로 옮기기

--- src/ch06/CombineFunctionsIntoClass-client2.js
+++ src/ch06/CombineFunctionsIntoClass-client2.js
@@ -1,10 +1,6 @@
-import {acquireReading, Reading, taxThreshold} from "./CombineFunctionsIntoClass";
+import {acquireReading, Reading} from "./CombineFunctionsIntoClass";
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-export const taxableCharge = taxableChargeFn(aReading);
 
-// 세금 부과 코드를 함수로 추출
-function taxableChargeFn(aReading) {
-    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
-}
+export const taxableCharge = aReading.taxableCharge;
