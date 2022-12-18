ffd5d7cd6bf917e875be7f83f9fad9184fca8c8e
soonho <dbstnsgh2@naver.com>
2022-12-18 11:00:37 +0900

    ch06: Combine Functions Into Class 리팩토링 - 3. 세금 부과 코드 함수로 추출

--- src/ch06/CombineFunctionsIntoClass-client2.js
+++ src/ch06/CombineFunctionsIntoClass-client2.js
@@ -2,4 +2,9 @@ import {acquireReading, Reading, taxThreshold} from "./CombineFunctionsIntoClass
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-export const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+export const taxableCharge = taxableChargeFn(aReading);
+
+// 세금 부과 코드를 함수로 추출
+function taxableChargeFn(aReading) {
+    return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+}
