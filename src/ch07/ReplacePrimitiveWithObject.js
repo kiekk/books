12cd9ae6f0c169a07550853e63af37135a903ab2
soonho <dbstnsgh2@naver.com>
2022-12-18 15:52:52 +0900

    ch07: Replace Primitive With Object 작성

---
+++ src/ch07/ReplacePrimitiveWithObject.js
@@ -0,0 +1,26 @@
+class Priority {
+    constructor(value) {
+        this._value = value;
+    }
+
+    toString() {
+        return this._value;
+    }
+}
+
+export class Order {
+    constructor(data) {
+        this._priority = new Priority(data.priority);
+    }
+
+    get priorityString() {
+        return this._priority.toString();
+    }
+
+    set priority(value) {
+        this._priority = new Priority(value);
+    }
+}
+
+const orders = [new Order({priority: "normal"}), new Order({priority: "high"}), new Order({priority: "rush"})];
+export const highPriorityCount = orders.filter(o => "high" === o.priorityString || "rush" === o.priorityString).length;
