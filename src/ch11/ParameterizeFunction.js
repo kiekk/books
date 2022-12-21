e1646c168daead1000f7538f93f06636e8741afb
soonho <shyoon991@gmail.com>
2022-12-21 21:48:15 +0900

    ch11: Parameterize Function 리팩토링 - bottomBand 함수 변경

--- src/ch11/ParameterizeFunction.js
+++ src/ch11/ParameterizeFunction.js
@@ -1,15 +1,11 @@
 export function baseCharge(usage) {
     if (usage < 0) return usd(0);
-    const amount = bottomBand(usage) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
+    const amount = withinBand(usage, 0, 100) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
     return usd(amount);
 }
 
 function withinBand(usage, bottom, top) {
-    return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function bottomBand(usage) {
-    return Math.min(usage, 100);
+    return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function topBand(usage) {
