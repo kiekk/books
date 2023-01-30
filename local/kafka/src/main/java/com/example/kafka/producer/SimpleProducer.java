d03c3cff3d319bc3ef983691730657ae7da2d781
shyoon <shyoon991@gmail.com>
2023-01-30 10:17:42 +0900

    add async Callback

--- local/kafka/src/main/java/com/example/kafka/producer/SimpleProducer.java
+++ local/kafka/src/main/java/com/example/kafka/producer/SimpleProducer.java
@@ -1,23 +1,22 @@
 package com.example.kafka.producer;
 
+import com.example.kafka.callback.ProducerCallback;
 import com.example.kafka.partitioner.CustomPartitioner;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 public class SimpleProducer {
     private static final Logger log = LoggerFactory.getLogger(SimpleProducer.class);
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "localhost:9092";
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         Properties configs = new Properties();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -28,10 +27,9 @@ public class SimpleProducer {
             String messageValue = "testMessage";
             ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "Pangyo");
 
-            // 브로커 정상 전송 여부 확인 - 동기 (sync)
-            RecordMetadata metadata = producer.send(record).get();
+            // 브로커 정상 전송 여부 확인 - 비동기 (async)
+            producer.send(record, new ProducerCallback());
             log.info("---------------------------------{}", record);
-            log.info("---------------------------------{}", metadata.toString());
             producer.flush();
         }
     }
