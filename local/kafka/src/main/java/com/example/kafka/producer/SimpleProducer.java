aa683cbad0fbbf89eb9e4326119f549fd4e331fc
shyoon <shyoon991@gmail.com>
2023-01-30 10:12:10 +0900

    add Custom Partitioner

--- local/kafka/src/main/java/com/example/kafka/producer/SimpleProducer.java
+++ local/kafka/src/main/java/com/example/kafka/producer/SimpleProducer.java
@@ -1,32 +1,37 @@
 package com.example.kafka.producer;
 
+import com.example.kafka.partitioner.CustomPartitioner;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class SimpleProducer {
     private static final Logger log = LoggerFactory.getLogger(SimpleProducer.class);
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "localhost:9092";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties configs = new Properties();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
 
         try (KafkaProducer<String, String> producer = new KafkaProducer<>(configs)) {
             String messageValue = "testMessage";
-            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, messageValue);
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "Pangyo");
 
-            producer.send(record);
+            // 브로커 정상 전송 여부 확인 - 동기 (sync)
+            RecordMetadata metadata = producer.send(record).get();
             log.info("---------------------------------{}", record);
-
+            log.info("---------------------------------{}", metadata.toString());
             producer.flush();
         }
     }
