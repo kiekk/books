4745d124522644bc4b46f412e0018c72612ac544
shyoon <shyoon991@gmail.com>
2023-02-01 20:19:19 +0900

    use commitSync()

--- local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
+++ local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
@@ -24,7 +24,7 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
 
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
@@ -34,6 +34,8 @@ public class SimpleConsumer {
             for (ConsumerRecord<String, String> record : records) {
                 log.info("record:{}", record);
             }
+            // 오프셋 커밋을 명시적으로 수행
+            consumer.commitSync();
         }
     }
 }
