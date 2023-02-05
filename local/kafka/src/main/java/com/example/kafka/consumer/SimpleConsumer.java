c98f0052f2a0bed47c8f5ea2791c6ec16822dc49
shyoon <shyoon991@gmail.com>
2023-02-05 21:23:45 +0900

    use assign()

--- local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
+++ local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
@@ -4,12 +4,14 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class SimpleConsumer {
@@ -17,6 +19,7 @@ public class SimpleConsumer {
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "localhost:9092";
     private final static String GROUP_ID = "test-group";
+    private final static int PARTITION_NUMBER  = 0;
 
     public static void main(String[] args) {
         Properties configs = new Properties();
@@ -26,13 +29,12 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
-
-        consumer.subscribe(Arrays.asList(TOPIC_NAME));
-
+        // 토픽, 파티션 명시적으로 할당
+        consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, PARTITION_NUMBER)));
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
             for (ConsumerRecord<String, String> record : records) {
-                log.info("{}", record);
+                log.info("record:{}", record);
             }
         }
     }
