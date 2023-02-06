3a5f8781521d80a5719ebfdadfca20319549f395
shyoon <shyoon991@gmail.com>
2023-02-06 20:19:19 +0900

    use assignment()

--- local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
+++ local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
@@ -13,6 +13,7 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.Set;
 
 public class SimpleConsumer {
     private final static Logger log = LoggerFactory.getLogger(SimpleConsumer.class);
@@ -29,13 +30,8 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
-        // 토픽, 파티션 명시적으로 할당
-        consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, PARTITION_NUMBER)));
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
-            for (ConsumerRecord<String, String> record : records) {
-                log.info("record:{}", record);
-            }
-        }
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+        // 컨슈머에 할당된 토픽, 파티션 정보 확인
+        Set<TopicPartition> assignment = consumer.assignment();
     }
 }
