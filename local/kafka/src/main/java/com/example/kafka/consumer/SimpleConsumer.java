e2cbdc83221043451c2ec7158228427c62b04172
shyoon <shyoon991@gmail.com>
2023-02-07 21:13:18 +0900

    consumer 안전하게 종료

--- local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
+++ local/kafka/src/main/java/com/example/kafka/consumer/SimpleConsumer.java
@@ -4,34 +4,55 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Properties;
-import java.util.Set;
 
 public class SimpleConsumer {
     private final static Logger log = LoggerFactory.getLogger(SimpleConsumer.class);
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "localhost:9092";
     private final static String GROUP_ID = "test-group";
-    private final static int PARTITION_NUMBER  = 0;
+    private static KafkaConsumer<String, String> consumer;
 
     public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+
         Properties configs = new Properties();
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+
+        consumer = new KafkaConsumer<>(configs);
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
-        // 컨슈머에 할당된 토픽, 파티션 정보 확인
-        Set<TopicPartition> assignment = consumer.assignment();
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("{}", record);
+                }
+                consumer.commitSync();
+            }
+        } catch (WakeupException e) {
+            log.warn("Wakeup consumer");
+        } finally {
+            log.warn("Consumer close");
+            consumer.close();
+        }
+    }
+
+    static class ShutdownThread extends Thread {
+        public void run() {
+            log.info("Shutdown hook");
+            consumer.wakeup();
+        }
     }
 }
