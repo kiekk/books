3c7aff1df5a5094127ae2adc4c1cd44de513b78a
soonho <shyoon991@gmail.com>
2023-02-18 01:35:10 +0900

    use KafkaTemplate Configuration

--- local/kafka/src/main/java/com/example/kafka/KafkaApplication.java
+++ local/kafka/src/main/java/com/example/kafka/KafkaApplication.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
 public class KafkaApplication implements CommandLineRunner {
@@ -12,7 +16,7 @@ public class KafkaApplication implements CommandLineRunner {
     private static String TOPIC_NAME = "test";
 
     @Autowired
-    private KafkaTemplate<Integer, String> template;
+    private KafkaTemplate<String, String> customKafkaTemplate;
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(KafkaApplication.class);
@@ -21,9 +25,18 @@ public class KafkaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        for (int i = 0; i < 10; i++) {
-            template.send(TOPIC_NAME, "test" + i);
-        }
+        ListenableFuture<SendResult<String, String>> future = customKafkaTemplate.send(TOPIC_NAME, "test");
+        future.addCallback(new KafkaSendCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+
+            }
+
+            @Override
+            public void onFailure(KafkaProducerException ex) {
+
+            }
+        });
         System.exit(0);
     }
 }
