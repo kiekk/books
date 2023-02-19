dcaa4bcdc5e6d5bd94ab60f99762f9f52eb5d77f
soonho <shyoon991@gmail.com>
2023-02-19 19:30:22 +0900

    use @KafkaListener

--- local/kafka/src/main/java/com/example/kafka/KafkaApplication.java
+++ local/kafka/src/main/java/com/example/kafka/KafkaApplication.java
@@ -1,18 +1,24 @@
 package com.example.kafka;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaProducerException;
 import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
 public class KafkaApplication implements CommandLineRunner {
-
+    public static Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
     private static String TOPIC_NAME = "test";
 
     @Autowired
@@ -38,5 +44,17 @@ public class KafkaApplication implements CommandLineRunner {
             }
         });
         System.exit(0);
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-01")
+    public void commitListener(ConsumerRecords<String, String> records, Acknowledgment ack) {
+        records.forEach(record -> logger.info(record.toString()));
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = "test", groupId = "test-group-02")
+    public void consumerCommitListener(ConsumerRecords<String, String> records, Consumer<String, String> consumer) {
+        records.forEach(record -> logger.info(record.toString()));
+        consumer.commitSync();
     }
 }
