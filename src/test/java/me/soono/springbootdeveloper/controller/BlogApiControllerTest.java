a7c2c3fd29fcb1b009a5c5e9978138fd9dc47901
soonho <shyoon991@gmail.com>
2023-05-13 08:25:46 +0900

    ch06: add test method to BlogApiControllerTest - updateArticle()

--- src/test/java/me/soono/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/soono/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -3,6 +3,7 @@ package me.soono.springbootdeveloper.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.soono.springbootdeveloper.domain.Article;
 import me.soono.springbootdeveloper.dto.AddArticleRequest;
+import me.soono.springbootdeveloper.dto.UpdateArticleRequest;
 import me.soono.springbootdeveloper.repository.BlogRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -139,4 +140,38 @@ class BlogApiControllerTest {
 
         assertThat(articles).isEmpty();
     }
+
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    void updateArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        final String newTitle = "new Title";
+        final String newContent = "new Content";
+
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+        // when
+        ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+
+        assertThat(article.getTitle()).isEqualTo(newTitle);
+        assertThat(article.getContent()).isEqualTo(newContent);
+    }
+
 }
