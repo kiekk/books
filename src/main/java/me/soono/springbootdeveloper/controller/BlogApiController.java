b1bb1505e5dc97e7f1005356b30f75a2ed9df817
soonho <shyoon991@gmail.com>
2023-05-13 07:58:35 +0900

    ch06: add method to BlogApiController - findAllArticles()

--- src/main/java/me/soono/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/soono/springbootdeveloper/controller/BlogApiController.java
@@ -3,9 +3,12 @@ package me.soono.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.soono.springbootdeveloper.domain.Article;
 import me.soono.springbootdeveloper.dto.AddArticleRequest;
+import me.soono.springbootdeveloper.dto.ArticleResponse;
 import me.soono.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,6 +16,14 @@ import org.springframework.web.bind.annotation.*;
 public class BlogApiController {
 
     private final BlogService blogService;
+
+    @GetMapping("")
+    public List<ArticleResponse> findAllArticles() {
+        return blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+    }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/me/soono/springbootdeveloper/dto/ArticleResponse.java
+++ src/main/java/me/soono/springbootdeveloper/dto/ArticleResponse.java
@@ -1,4 +1,10 @@
 package me.soono.springbootdeveloper.dto;
 
+import me.soono.springbootdeveloper.domain.Article;
+
 public record ArticleResponse(String title, String content) {
+
+    public ArticleResponse(Article article) {
+        this(article.getTitle(), article.getContent());
+    }
 }
