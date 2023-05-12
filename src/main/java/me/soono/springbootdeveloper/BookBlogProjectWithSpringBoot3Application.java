cf94e9d99cba72784c18ac915e5ef8ca6504bdd2
soonho <shyoon991@gmail.com>
2023-05-13 06:56:49 +0900

    Initial Commit

---
+++ src/main/java/me/soono/springbootdeveloper/BookBlogProjectWithSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package me.soono.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBlogProjectWithSpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookBlogProjectWithSpringBoot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/soono/springbootdeveloper/BookBlogProjectWithSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.soono.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBlogProjectWithSpringBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
