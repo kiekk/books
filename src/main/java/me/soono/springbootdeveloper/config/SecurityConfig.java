759f4de60dbb7a79f2cab58189442f40a5d99611
soonho <shyoon991@gmail.com>
2023-05-13 10:49:19 +0900

    ch08: add config - SecurityConfig: add logout url

--- src/main/java/me/soono/springbootdeveloper/config/SecurityConfig.java
+++ src/main/java/me/soono/springbootdeveloper/config/SecurityConfig.java
@@ -5,12 +5,13 @@ import lombok.RequiredArgsConstructor;
 import me.soono.springbootdeveloper.service.user.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toStaticResources;
@@ -36,6 +37,7 @@ public class SecurityConfig {
                     formLogin.defaultSuccessUrl("/articles");
                 })
                 .logout(logout -> {
+                    logout.logoutRequestMatcher(AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/logout"));
                     logout.logoutSuccessUrl("/login");
                     logout.invalidateHttpSession(true);
                 })
