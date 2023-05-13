57b82944162b75b7fcd73843496abd47b4536506
soonho <shyoon991@gmail.com>
2023-05-13 10:28:50 +0900

    ch08: remove security warning

--- src/main/java/me/soono/springbootdeveloper/config/SecurityConfig.java
+++ src/main/java/me/soono/springbootdeveloper/config/SecurityConfig.java
@@ -8,28 +8,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toStaticResources;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
-    public WebSecurityCustomizer configure() {
-        return web -> web.ignoring()
-                .requestMatchers(toH2Console())
-                .requestMatchers("/static/**");
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authz -> {
+                    // security warning 제거
+                    // You are asking Spring Security to ignore Mvc [pattern='/static/**'].
+                    // This is not recommended -- please use permitAll via HttpSecurity#authorizeHttpRequests instead.
+                    authz.requestMatchers(toH2Console(), toStaticResources().atCommonLocations()).permitAll();
                     authz.requestMatchers("/login", "/signup", "/user").permitAll();
                     authz.anyRequest().authenticated();
                 })
