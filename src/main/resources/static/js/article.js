91f9504649b4fbfaf9e619b8d8f45108ed0b09a0
soonho <shyoon991@gmail.com>
2023-05-13 09:40:36 +0900

    ch07: add function update article

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -17,3 +17,32 @@ if (deleteButton) {
             });
     });
 }
+
+// 수정 기능
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        if (!confirm('수정 하시겠습니까?')) {
+            return;
+        }
+
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+            .then(() => {
+                alert('수정이 완료되었습니다.');
+                location.replace(`/articles/${id}`);
+            });
+    });
+}
