ad0020a851bf7d46c7f489bf24c8fcf8e0283809
soonho <shyoon991@gmail.com>
2023-05-13 17:05:27 +0900

    ch09: add controller - TokenApiController

---
+++ src/main/java/me/soono/springbootdeveloper/controller/token/TokenApiController.java
@@ -0,0 +1,22 @@
+package me.soono.springbootdeveloper.controller.token;
+
+import lombok.RequiredArgsConstructor;
+import me.soono.springbootdeveloper.dto.token.requeset.CreateAccessTokenRequest;
+import me.soono.springbootdeveloper.dto.token.response.CreateAccessTokenResponse;
+import me.soono.springbootdeveloper.service.token.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/tokens")
+@RequiredArgsConstructor
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateAccessTokenResponse createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        return new CreateAccessTokenResponse(tokenService.createNewAccessToken(request.getRefreshToken()));
+    }
+}
