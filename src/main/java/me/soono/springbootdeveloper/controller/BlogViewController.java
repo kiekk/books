9710240c3dc110b928b4bd4eba05e0c290d64e67
soonho <shyoon991@gmail.com>
2023-05-13 09:37:44 +0900

    ch07: add page - newArticle

--- src/main/java/me/soono/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/soono/springbootdeveloper/controller/BlogViewController.java
@@ -1,6 +1,7 @@
 package me.soono.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.soono.springbootdeveloper.domain.Article;
 import me.soono.springbootdeveloper.dto.ArticleListViewResponse;
 import me.soono.springbootdeveloper.dto.ArticleViewResponse;
 import me.soono.springbootdeveloper.service.BlogService;
@@ -9,17 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("articles")
 public class BlogViewController {
 
     private final BlogService blogService;
 
-    @GetMapping("")
+    @GetMapping("articles")
     public String getArticles(Model model) {
         List<ArticleListViewResponse> articles = blogService.findAll()
                 .stream()
@@ -30,10 +31,21 @@ public class BlogViewController {
         return "articleList";
     }
 
-    @GetMapping("{id}")
+    @GetMapping("articles/{id}")
     public String getArticle(@PathVariable Long id, Model model) {
         model.addAttribute("article", new ArticleViewResponse(blogService.findById(id)));
         return "article";
+    }
+
+    @GetMapping("new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 
 }
