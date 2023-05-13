1b62590a56204a5951fc93540cd2dce2615005de
soonho <shyoon991@gmail.com>
2023-05-13 09:28:32 +0900

    ch07: add page - article

--- src/main/java/me/soono/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/soono/springbootdeveloper/controller/BlogViewController.java
@@ -2,10 +2,12 @@ package me.soono.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.soono.springbootdeveloper.dto.ArticleListViewResponse;
+import me.soono.springbootdeveloper.dto.ArticleViewResponse;
 import me.soono.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,6 +28,12 @@ public class BlogViewController {
 
         model.addAttribute("articles", articles);
         return "articleList";
+    }
+
+    @GetMapping("{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        model.addAttribute("article", new ArticleViewResponse(blogService.findById(id)));
+        return "article";
     }
 
 }
