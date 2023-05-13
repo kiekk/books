7618cae4bf678721e1c73dac7daff05af6b26b1f
soonho <shyoon991@gmail.com>
2023-05-13 09:33:15 +0900

    ch07: add function delete article

---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,19 @@
+// 삭제 기능
+const deleteButton = document.getElementById('delete-btn');
+
+if (deleteButton) {
+    deleteButton.addEventListener('click', event => {
+        if (!confirm('정말 삭제하시겠습니까?')) {
+            return;
+        }
+
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/articles/${id}`, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                alert('삭제가 완료되었습니다.');
+                location.replace('/articles');
+            });
+    });
+}
