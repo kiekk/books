fd32f2a84a93bdd0deee08617da28bd6a327217f
soonho <shyoon991@gmail.com>
2023-07-22 20:01:44 +0900

    feat: add endpoint rsocket

--- aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/controller/PositionController.java
+++ aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/controller/PositionController.java
@@ -2,20 +2,28 @@ package com.thehecklers.aircraftpositions.controller;
 
 import com.thehecklers.aircraftpositions.domain.Aircraft;
 import com.thehecklers.aircraftpositions.repository.AircraftRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 @Controller
-@RequiredArgsConstructor
 public class PositionController {
 
     private final AircraftRepository aircraftRepository;
+    private final RSocketRequester requester;
     private final WebClient client =
             WebClient.create("http://localhost:7634/aircraft");
+
+    public PositionController(AircraftRepository aircraftRepository,
+                              RSocketRequester.Builder builder) {
+        this.aircraftRepository = aircraftRepository;
+        this.requester = builder.tcp("localhost", 7635);
+    }
 
 
     @GetMapping("aircraft")
@@ -29,6 +37,15 @@ public class PositionController {
 
         model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/acstream",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionsStream() {
+        return requester.route("acstream")
+                .data("Requesting aircraft positions")
+                .retrieveFlux(Aircraft.class);
     }
 
 }
