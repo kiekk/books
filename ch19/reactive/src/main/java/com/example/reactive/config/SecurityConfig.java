8d026526a1438295057637819a40a1da16697e36
soonho <shyoon991@gmail.com>
2023-07-28 12:30:59 +0900

    feat: anyExchange().access()

--- ch19/reactive/src/main/java/com/example/reactive/config/SecurityConfig.java
+++ ch19/reactive/src/main/java/com/example/reactive/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.reactive.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +14,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalTime;
+import java.util.function.Function;
 
 @Configuration
 public class SecurityConfig {
@@ -22,7 +29,7 @@ public class SecurityConfig {
                 .authorizeExchange(authz ->
                         authz
                                 .pathMatchers(HttpMethod.GET, "/hello").authenticated()
-                                .anyExchange().permitAll()
+                                .anyExchange().access(this::getAuthorizationDecisionMono)
                 )
                 .httpBasic(Customizer.withDefaults())
                 .build();
@@ -41,6 +48,34 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    private Mono<AuthorizationDecision> getAuthorizationDecisionMono(
+            Mono<Authentication> a,
+            AuthorizationContext c) {
+        String path = getRequestPath(c);
+
+        boolean restrictedTime = LocalTime.now().isAfter(LocalTime.NOON);
+
+        if (path.equals("/hello")) {
+            return a.map(isAdmin())
+                    .map(auth -> auth && !restrictedTime)
+                    .map(AuthorizationDecision::new);
+        }
+//        return Mono.just(new AuthorizationDecision(true)); // 요청 허용
+        return Mono.just(new AuthorizationDecision(false)); // 요청 거부
+    }
+
+    private String getRequestPath(AuthorizationContext c) {
+        return c.getExchange()
+                .getRequest()
+                .getPath()
+                .toString();
+    }
+
+    private Function<Authentication, Boolean> isAdmin() {
+        return p -> p.getAuthorities().stream()
+                .anyMatch(e -> e.getAuthority().equals("ROLE_ADMIN"));
     }
 
 }
