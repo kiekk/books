e99f50adcfc091d99df6ad5dff74ce517c151e28
soonho <shyoon991@gmail.com>
2023-07-29 07:19:15 +0900

    ch19: Init Project Resource Server

---
+++ ch19/reactive-resource-server/src/main/java/com/example/reactiveresourceserver/ReactiveResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactiveresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveResourceServerApplication.class, args);
+    }
+
+}
