aed976b4701fb5e3d180ecc9c377997c786d1249
soonho <shyoon991@gmail.com>
2023-07-28 12:09:12 +0900

    feat: ReactiveSecurityContextHolder에서 인증 객체 가져오기

--- ch19/reactive/src/main/java/com/example/reactive/HelloController.java
+++ ch19/reactive/src/main/java/com/example/reactive/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.reactive;
 
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,7 +10,9 @@ import reactor.core.publisher.Mono;
 public class HelloController {
 
     @RequestMapping("hello")
-    public Mono<String> hello(Mono<Authentication> auth) {
-        return auth.map(a -> "Hello : " + a.getName());
+    public Mono<String> hello() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(auth -> "Hello " + auth.getName());
     }
 }
