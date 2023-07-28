c2e0aa73081199b5b0756b91ea2dd17fee06a19e
soonho <shyoon991@gmail.com>
2023-07-29 08:12:52 +0900

    feat: security test - annotation 사용

---
+++ ch20/security-test/src/test/java/com/example/securitytest/MainTests.java
@@ -0,0 +1,47 @@
+package com.example.securitytest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc // 스프링 부트가 MockMvc를 자동 구성하게 합니다.
+public class MainTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("인증되지 않은 사용자는 '/hello' 를 요청할 수 없다.")
+    void hellUnauthenticated() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("인증된 사용자는 '/hello' 를 요청할 수 있다.")
+    void helloAuthenticated() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("Hello!"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "mary", password = "12345", authorities = "read")
+    @DisplayName("MockUser 세부 정보 설정")
+    void helloAuthenticated2() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("Hello!"))
+                .andExpect(status().isOk());
+    }
+
+}
