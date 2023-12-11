9f9bf1c560e25587c484c9bee9107b100553bfe3
soonho <shyoon991@gmail.com>
2023-12-11 20:15:27 +0900

    feat: 리액티브 컨트롤러 작성

---
+++ 1-reactive/src/main/java/org/example/hacking/ServerController.java
@@ -0,0 +1,21 @@
+package org.example.hacking;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ServerController {
+
+    private final KitchenService kitchen;
+
+    public ServerController(KitchenService kitchen) {
+        this.kitchen = kitchen;
+    }
+
+    @GetMapping(value = "/server", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Dish> serverDishes() {
+        return kitchen.getDishes();
+    }
+}
