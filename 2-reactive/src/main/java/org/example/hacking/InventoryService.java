7909fa01b3fab3213c7bf5e57462ef9d1edef83d
soonho <shyoon991@gmail.com>
2023-12-12 20:52:11 +0900

    feat: Example + fluent operation(평문형 연산)을 사용하여 검색 쿼리 작성

--- 2-reactive/src/main/java/org/example/hacking/InventoryService.java
+++ 2-reactive/src/main/java/org/example/hacking/InventoryService.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.ReactiveFluentMongoOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.data.mongodb.core.query.Criteria.byExample;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
@@ -38,6 +39,21 @@ public class InventoryService {
     Flux<Item> searchByFluentExample(String name, String description) {
         return fluentOperations.query(Item.class)
                 .matching(query(where("TV tray").is(name).and("Smurf").is(description)))
+                .all();
+    }
+
+    Flux<Item> searchByFluentExample(String name, String description, boolean useAnd) {
+        Item item = new Item(name, description, 0.0);
+
+        ExampleMatcher matcher = (useAnd
+                ? ExampleMatcher.matchingAll()
+                : ExampleMatcher.matchingAny())
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING)
+                .withIgnoreCase()
+                .withIgnorePaths("price");
+
+        return fluentOperations.query(Item.class)
+                .matching(query(byExample(Example.of(item, matcher))))
                 .all();
     }
 
