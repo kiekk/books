bd4c55cb7514619973c238f41986328d6103fb04
soonho <shyoon991@gmail.com>
2023-12-12 20:49:32 +0900

    feat: fluent operation(평문형 연산)을 사용하여 검색 쿼리 작성

--- 2-reactive/src/main/java/org/example/hacking/InventoryService.java
+++ 2-reactive/src/main/java/org/example/hacking/InventoryService.java
@@ -2,16 +2,22 @@ package org.example.hacking;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.mongodb.core.ReactiveFluentMongoOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Service
 public class InventoryService {
 
     private final ItemRepository repository;
+    private final ReactiveFluentMongoOperations fluentOperations;
 
-    public InventoryService(ItemRepository repository) {
+    public InventoryService(ItemRepository repository, ReactiveFluentMongoOperations fluentOperations) {
         this.repository = repository;
+        this.fluentOperations = fluentOperations;
     }
 
     Flux<Item> searchByExample(String name, String description, boolean useAnd) {
@@ -27,6 +33,12 @@ public class InventoryService {
         Example<Item> probe = Example.of(item, matcher);
 
         return repository.findAll(probe);
+    }
+
+    Flux<Item> searchByFluentExample(String name, String description) {
+        return fluentOperations.query(Item.class)
+                .matching(query(where("TV tray").is(name).and("Smurf").is(description)))
+                .all();
     }
 
 }
