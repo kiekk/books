a31f5ca8bf3fc8b9f294126ff60fae5187a00db0
soonho <shyoon991@gmail.com>
2023-12-12 20:09:26 +0900

    feat: 장바구니 추가 reactive로 구현

--- 2-reactive/src/main/java/org/example/hacking/HomeController.java
+++ 2-reactive/src/main/java/org/example/hacking/HomeController.java
@@ -2,6 +2,8 @@ package org.example.hacking;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
@@ -25,6 +27,28 @@ public class HomeController {
                         cartRepository.findById("My Cart")
                                 .defaultIfEmpty(new Cart("My Cart")))
                 .build());
+    }
+
+    @PostMapping("/add/{id}")
+    Mono<String> addToCart(@PathVariable String id) {
+        return cartRepository.findById("My Cart")
+                .defaultIfEmpty(new Cart("My Cart"))
+                .flatMap(cart -> cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getItem()
+                                .getId().equals(id))
+                        .findAny()
+                        .map(cartItem -> {
+                            cartItem.increment();
+                            return Mono.just(cart);
+                        })
+                        .orElseGet(() -> itemRepository.findById(id)
+                                .map(CartItem::new)
+                                .map(cartItem -> {
+                                    cart.getCartItems().add(cartItem);
+                                    return cart;
+                                })))
+                .flatMap(cartRepository::save)
+                .thenReturn("redirect:/");
     }
 
 }
