fe3368e0a05af1144229c6c5016e4331152e0130
shyoon <shyoon991@gmail.com>
2023-12-15 19:34:35 +0900

    feat: Initial Module - 6-reactive

---
+++ 6-reactive/src/main/java/com/example/hacking/ApiItemController.java
@@ -0,0 +1,48 @@
+package com.example.hacking;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@RestController
+public class ApiItemController {
+
+    private final ItemRepository itemRepository;
+
+    public ApiItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping("/api/items")
+    Flux<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    @GetMapping("/api/items/{id}")
+    Mono<Item> findOne(@PathVariable String id) {
+        return itemRepository.findById(id);
+    }
+
+    @PostMapping("/api/items")
+    Mono<ResponseEntity<?>> addNewItem(@RequestBody Mono<Item> item) {
+        return item.flatMap(itemRepository::save)
+                .map(savedItem ->
+                        ResponseEntity
+                                .created(URI.create("/api/items/" + savedItem.getId()))
+                                .body(savedItem));
+    }
+
+    @PutMapping("/api/items/{id}")
+    public Mono<ResponseEntity<?>> updateItem(
+            @RequestBody Mono<Item> item,
+            @PathVariable String id) {
+        return item
+                .map(content -> new Item(id, content.getName(), content.getDescription(),
+                        content.getPrice()))
+                .flatMap(itemRepository::save)
+                .map(ResponseEntity::ok);
+    }
+}
