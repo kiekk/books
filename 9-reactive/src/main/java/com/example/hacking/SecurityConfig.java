6e27c9e977c4dc5394ab3712967870ec0ef75b9a
shyoon <shyoon991@gmail.com>
2023-12-15 22:21:00 +0900

    feat: SecurityConfig 설정 추가 & endpoint 권한 적용

--- 9-reactive/src/main/java/com/example/hacking/SecurityConfig.java
+++ 9-reactive/src/main/java/com/example/hacking/SecurityConfig.java
@@ -2,11 +2,31 @@ package com.example.hacking;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    static final String USER = "USER";
+    static final String INVENTORY = "INVENTORY";
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.POST, "/").hasRole(INVENTORY)
+                        .pathMatchers(HttpMethod.DELETE, "/**").hasRole(INVENTORY)
+                        .anyExchange().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
 
     @Bean
     ReactiveUserDetailsService userDetailsService(UserRepository userRepository) {
--- 9-reactive/src/main/java/com/example/hacking/UserLoaderConfig.java
+++ 9-reactive/src/main/java/com/example/hacking/UserLoaderConfig.java
@@ -7,11 +7,25 @@ import org.springframework.data.mongodb.core.MongoOperations;
 
 import java.util.List;
 
+import static com.example.hacking.SecurityConfig.INVENTORY;
+import static com.example.hacking.SecurityConfig.USER;
+
 @Configuration
 public class UserLoaderConfig {
 
+    static String role(String auth) {
+        return "ROLE_" + auth;
+    }
+
     @Bean
     CommandLineRunner userLoader(MongoOperations mongoOperations) {
-        return args -> mongoOperations.save(new User("soono", "password", List.of("ROLE_USER")));
+        return args -> {
+            mongoOperations.save(
+                    new User("soono", "password", List.of(role(USER), role(INVENTORY)))
+            );
+            mongoOperations.save(
+                    new User("soono2", "password", List.of(role(INVENTORY)))
+            );
+        };
     }
 }
