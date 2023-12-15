759ecd9f3e47288708fab370e6b01755e2fd92a5
shyoon <shyoon991@gmail.com>
2023-12-16 08:54:22 +0900

    feat: security test 추가
    *@WithMockuser 애노테이션을 사용하여 테스트용 인증 객체 추가

--- 9-reactive/src/main/java/com/example/hacking/ItemRepository.java
+++ 9-reactive/src/main/java/com/example/hacking/ItemRepository.java
@@ -2,7 +2,10 @@ package com.example.hacking;
 
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
-public interface ItemRepository extends ReactiveCrudRepository<Item, String>,
-        ReactiveQueryByExampleExecutor<Item> {
+public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
+
+    Mono<Item> findByName(String name);
+
 }
