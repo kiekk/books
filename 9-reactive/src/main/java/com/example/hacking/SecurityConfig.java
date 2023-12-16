2b4a8014234944122881467aed50a3ba93d0b5b2
shyoon <shyoon991@gmail.com>
2023-12-16 09:10:11 +0900

    feat: 메서드 기반 권한 체크 추가

--- 9-reactive/src/main/java/com/example/hacking/SecurityConfig.java
+++ 9-reactive/src/main/java/com/example/hacking/SecurityConfig.java
@@ -2,14 +2,15 @@ package com.example.hacking;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
     static final String USER = "USER";
@@ -19,8 +20,6 @@ public class SecurityConfig {
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers(HttpMethod.POST, "/").hasRole(INVENTORY)
-                        .pathMatchers(HttpMethod.DELETE, "/**").hasRole(INVENTORY)
                         .anyExchange().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
