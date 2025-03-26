0741a27fbc43a4a5c0c4943881a1fe0214e57db8
shyoon <shyoon991@gmail.com>
2025-03-26 21:53:28 +0900

    chapter1-1: data.sql, schema.sql 추가
    * TODO: 책에서는 프로젝트 실행하여 테스트를 할 수 있도록 했지만 이 부분은 @Test로 확인할 수 있도록 변경하는 것이 좋겠다.

--- chapter1/OneToManyBidirectional/src/main/java/com/bookstroe/OneToManyBidirectionalApplication.java
+++ chapter1/OneToManyBidirectional/src/main/java/com/bookstroe/OneToManyBidirectionalApplication.java
@@ -1,13 +1,38 @@
 package com.bookstroe;
 
+import com.bookstroe.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OneToManyBidirectionalApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public OneToManyBidirectionalApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OneToManyBidirectionalApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            System.out.println("\nInsert author with books  ...");
+            bookstoreService.insertAuthorWithBooks();
+
+            System.out.println("\nDelete a book of an author...");
+            bookstoreService.deleteBookOfAuthor();
+
+            System.out.println("\nInsert other author with books  ...");
+            bookstoreService.insertOtherAuthorWithBooks();
+
+            System.out.println("\nDelete all book of an author...");
+            bookstoreService.deleteAllBooksOfAuthor();
+        };
+    }
 }
