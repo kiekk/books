1f635a2ab1679c8a3246d4897a87dfce371f6485
shyoon <kiekk@i-commtech.com>
2025-03-27 11:20:54 +0900

    chapter1-1: 주석 수정
    * JPA 메서드에 파라미터 바인딩 시 에러 발생에 대한 spring boot 버전 수정 및 spring 버전 추가
    * spring 6.1, spring boot 3.2 버전 부터 에러가 발생하며 관련해서 참고 링크 추가
    
    링크: https://www.korecmblog.com/blog/upgrade-tospring6.1-parameter-name-retention
    링크: https://velog.io/@ghwns9991/%EC%8A%A4%ED%94%84%EB%A7%81-%EB%B6%80%ED%8A%B8-3.2-%EB%A7%A4%EA%B0%9C%EB%B3%80%EC%88%98-%EC%9D%B4%EB%A6%84-%EC%9D%B8%EC%8B%9D-%EB%AC%B8%EC%A0%9C

--- chapter1/OneToManyBidirectional/src/main/java/com/bookstroe/repository/AuthorRepository.java
+++ chapter1/OneToManyBidirectional/src/main/java/com/bookstroe/repository/AuthorRepository.java
@@ -9,6 +9,6 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     // 책에서는 위치 기반 파라미터 설정이지만 명확하게 이름 기반 파라미터 설정을 사용한다.
     @Query("SELECT a FROM Author a JOIN FETCH a.books WHERE a.name = :name")
     Author fetchByName(@Param("name") String name);
-    // spring boot 3.0 부터 @Param을 설정하지 않으면 에러가 발생한다.
+    // spring 6.1, spring boot 3.2 부터 @Param을 설정하지 않으면 에러가 발생한다.
     // Caused by: java.lang.IllegalStateException: For queries with named parameters you need to provide names for method parameters; Use @Param for query method parameters, or when on Java 8+ use the javac flag -parameters
 }
