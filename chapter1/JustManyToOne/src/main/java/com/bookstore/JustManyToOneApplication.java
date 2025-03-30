10792462507350d48d6baa5a2667b387bac95ee1
shyoon <shyoon991@gmail.com>
2025-03-30 18:34:38 +0900

    chapter1-3: 단방향 @ManyToOne의 효율성 - Project Init

---
+++ chapter1/JustManyToOne/src/main/java/com/bookstore/JustManyToOneApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustManyToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustManyToOneApplication.class, args);
+	}
+
+}
