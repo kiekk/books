01cbebdc68ee5b517b2a4eb7ea00a56ecab0c4cc
shyoon <shyoon991@gmail.com>
2025-03-31 16:33:42 +0900

    chapter1-3: BookRepository - @Param 설정

--- chapter1/JustManyToOne/src/main/java/com/bookstore/repository/BookRepository.java
+++ chapter1/JustManyToOne/src/main/java/com/bookstore/repository/BookRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Query("SELECT b FROM Book b WHERE b.author.id = :id")
-    List<Book> fetchBooksOfAuthorById(Long id);
+    List<Book> fetchBooksOfAuthorById(@Param("id") Long id);
 
     @Query("SELECT b FROM Book b WHERE b.author.id = :id")
-    Page<Book> fetchPageBooksOfAuthorById(Long id, Pageable pageable);
+    Page<Book> fetchPageBooksOfAuthorById(@Param("id") Long id, Pageable pageable);
 }
