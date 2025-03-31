15f0ca5a8a89765e51e6068c48632e574c7e4a1a
shyoon <shyoon991@gmail.com>
2025-03-31 20:47:35 +0900

    chapter1-4: BookstoreService 추가
    * Author(N) <-> Book(N) @ManyToMany 양방향 연관관계 쿼리 확인

--- chapter1/ManyToManyBidirectional/src/main/java/com/bookstore/ManyToManyBidirectionalApplication.java
+++ chapter1/ManyToManyBidirectional/src/main/java/com/bookstore/ManyToManyBidirectionalApplication.java
@@ -1,13 +1,27 @@
 package com.bookstore;
 
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ManyToManyBidirectionalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManyToManyBidirectionalApplication.class, args);
-	}
+    private final BookstoreService bookstoreService;
+
+    public ManyToManyBidirectionalApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyToManyBidirectionalApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> bookstoreService.insertAuthorsWithBooks();
+    }
 
 }
