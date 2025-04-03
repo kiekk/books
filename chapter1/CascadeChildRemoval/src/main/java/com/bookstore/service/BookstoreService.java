a827994180cb2eac60490abcb80bb1fdfd106d1b
shyoon <shyoon991@gmail.com>
2025-04-03 20:45:59 +0900

    chapter1-6: BookstoreService 추가 & deleteViaCascadeRemove() 메서드 동작 확인
    * Author 삭제 시 books가 조회되지 않았기 때문에 books를 제거하기 위해 select 문을 호출해 books를 조회하여 영속성 컨텍스트에 저장한다.

---
+++ chapter1/CascadeChildRemoval/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,29 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Author;
+import com.bookstore.repository.AuthorRepository;
+import com.bookstore.repository.BookRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class BookstoreService {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BookstoreService(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional
+    public void deleteViaCascadeRemove() {
+        Author author = authorRepository.findByName("Joana Nimar");
+
+        authorRepository.delete(author);
+    }
+
+}
