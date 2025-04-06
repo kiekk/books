aa6d18261a1ec0b753e6152705c18b78765e8f2a
shyoon <shyoon991@gmail.com>
2025-04-06 21:51:26 +0900

    chapter1-9: @Basic(fetch = FetchType.LAZY) 동작 확인

--- chapter1/NamedEntityGraphBasicAttrs/src/main/java/com/bookstore/NamedEntityGraphBasicAttrsApplication.java
+++ chapter1/NamedEntityGraphBasicAttrs/src/main/java/com/bookstore/NamedEntityGraphBasicAttrsApplication.java
@@ -1,13 +1,33 @@
 package com.bookstore;
 
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NamedEntityGraphBasicAttrsApplication {
 
+    private final BookstoreService bookstoreService;
+
+    public NamedEntityGraphBasicAttrsApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(NamedEntityGraphBasicAttrsApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            System.out.println("\nCall AuthorRepository#findByAgeGreaterThanAndGenre(), fetch graph:");
+            bookstoreService.displayAuthorsAndBooksByAgeAndGenre(40, "Anthology");
+
+            System.out.println("\nCall AuthorRepository#findByGenreAndAgeGreaterThan(), load graph:");
+            bookstoreService.displayAuthorsAndBooksByGenreAndAge("Anthology", 40);
+        };
     }
 
 }
