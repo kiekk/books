02b559fe52188d0c4d0c84790f288dc958c87565
shyoon <shyoon991@gmail.com>
2025-04-07 21:28:40 +0900

    chapter1-10: @Where 어노테이션 동작 확인

--- chapter1/FilterAssociation/src/main/java/com/bookstore/FilterAssociationApplication.java
+++ chapter1/FilterAssociation/src/main/java/com/bookstore/FilterAssociationApplication.java
@@ -1,13 +1,35 @@
 package com.bookstore;
 
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FilterAssociationApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public FilterAssociationApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FilterAssociationApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            System.out.println("\nAuthor with all books ...");
+            bookstoreService.fetchAuthorWithAllBooks();
+
+            System.out.println("\nAuthor with cheap books ...");
+            bookstoreService.fetchAuthorWithCheapBooks();
+
+            System.out.println("\nAuthor with rest of books ...");
+            bookstoreService.fetchAuthorWithRestOfBooks();
+        };
+    }
 }
