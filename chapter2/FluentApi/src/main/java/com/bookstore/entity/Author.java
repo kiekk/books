e83d726c21e45059d76e7a48132ccb99eaa6266a
shyoon <shyoon991@gmail.com>
2025-04-10 21:12:37 +0900

    chapter2-13: setter를 사용한 플루언트 스타일

--- chapter2/FluentApi/src/main/java/com/bookstore/entity/Author.java
+++ chapter2/FluentApi/src/main/java/com/bookstore/entity/Author.java
@@ -29,4 +29,43 @@ public class Author {
         this.books.remove(book);
         return this;
     }
+
+    public Author setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Author setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Author setGenre(String genre) {
+        this.genre = genre;
+        return this;
+    }
+
+    public Author setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public Author setBooks(List<Book> books) {
+        this.books = books;
+        return this;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- chapter2/FluentApi/src/main/java/com/bookstore/entity/Book.java
+++ chapter2/FluentApi/src/main/java/com/bookstore/entity/Book.java
@@ -22,4 +22,33 @@ public class Book {
     public void removeAuthor() {
         this.author = null;
     }
+
+    public Book setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Book setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Book setIsbn(String isbn) {
+        this.isbn = isbn;
+        return this;
+    }
+
+    public Book setAuthor(Author author) {
+        this.author = author;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
