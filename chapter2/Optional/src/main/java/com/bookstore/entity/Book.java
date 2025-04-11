9faca95b9bfbeba00610f96e1b64a682238305e0
shyoon <shyoon991@gmail.com>
2025-04-11 21:17:27 +0900

    chapter2-15: 영속성 레이어에서 자바 8 Optional 사용 방법 - Project Init

---
+++ chapter2/Optional/src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,43 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Book)) {
+            return false;
+        }
+
+        return id != null && id.equals(((Book) obj).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 2021;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", isbn=" + isbn + '}';
+    }
+
+}
