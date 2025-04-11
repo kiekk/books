4a19317f775b9c14734eb98da90a4820d0f6b9f8
shyoon <shyoon991@gmail.com>
2025-04-11 20:35:14 +0900

    chapter2-14: 하이버네이트 프록시를 통한 자식 측에서 부모 연관관계 채우기 - Project Init

---
+++ chapter2/PopulatingChildViaProxy/src/main/java/com/bookstore/PopulatingChildViaProxyApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulatingChildViaProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopulatingChildViaProxyApplication.class, args);
+    }
+
+}
---
+++ chapter2/PopulatingChildViaProxy/src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,34 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public static Book createBook(String title, String isbn, Author author) {
+        Book book = new Book();
+        book.title = title;
+        book.isbn = isbn;
+        book.author = author;
+        return book;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+}
