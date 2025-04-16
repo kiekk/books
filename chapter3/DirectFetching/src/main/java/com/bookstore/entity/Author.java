340672770462129e335b9369f500fb0e071488e9
shyoon <shyoon991@gmail.com>
2025-04-16 22:02:44 +0900

    chapter3-21: 다이렉트 페치 사용 방법 - Project Init
    * 다이렉트 페칭, ID로 가져오기는 식별자를 알고 있고 지연 연관관계가 현재 영속성 컨텍스트에서 필요하지 않을 때에 엔터티를 가져오는 바람직한 방법이다.
    * 다이렉트 페칭은 기본, 지정된 FetchType에 따라 엔터티를 로딩하는데 @~ToMany는 LAZY, @~ToOne은 EAGER로 처리되는 점에 유의해야 한다.

---
+++ chapter3/DirectFetching/src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,27 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int age;
+    private String name;
+    private String genre;
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+}
