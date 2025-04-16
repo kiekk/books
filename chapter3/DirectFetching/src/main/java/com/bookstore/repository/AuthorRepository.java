59e1926a0094ff7e62c463442dd2e2d530f36aa6
shyoon <shyoon991@gmail.com>
2025-04-16 22:18:50 +0900

    chapter3-21: JPQL 사용 시 실행 결과 확인
    * JPQL은 먼저 쿼리 실행 후 영속성 컨텍스트를 확인하기 때문에 Author가 영속성 컨텍스트에 존재해도 select 쿼리가 추가로 발생

--- chapter3/DirectFetching/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ chapter3/DirectFetching/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -2,6 +2,13 @@ package com.bookstore.repository;
 
 import com.bookstore.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query("SELECT a FROM Author a WHERE a.id = :id")
+    @Override
+    Optional<Author> findById(@Param("id") Long id);
 }
