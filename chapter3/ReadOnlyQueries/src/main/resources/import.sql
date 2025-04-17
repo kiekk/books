a1c596c840858e91bc38e646651961a05271e667
shyoon <shyoon991@gmail.com>
2025-04-17 20:42:12 +0900

    chapter3-22: 미래 영속성 컨텍스트에서 데이터베이스 변경 사항 전파를 위한 읽기 전용 엔터티의 사용 이유 - Project Init

---
+++ chapter3/ReadOnlyQueries/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into author (age, name, genre) values (23, 'Mark Janel', 'Anthology');
+insert into author (age, name, genre) values (43, 'Olivia Goy', 'Horror');
+insert into author (age, name, genre) values (51, 'Quartis Young', 'Anthology');
+insert into author (age, name, genre) values (34, 'Joana Nimar', 'History');
+insert into author (age, name, genre) values (38, 'Alicia Tom', 'Anthology');
+insert into author (age, name, genre) values (56, 'Katy Loin', 'Anthology');
