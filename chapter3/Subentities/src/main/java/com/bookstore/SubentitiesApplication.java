d0acf9adc9e86b23b0a1554c9ea155047fa0817b
shyoon <shyoon991@gmail.com>
2025-04-26 19:45:09 +0900

    chapter3-24: 서브엔터티를 통한 엔터티 속셩 지연 로딩 방법 - Project Init

---
+++ chapter3/Subentities/src/main/java/com/bookstore/SubentitiesApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubentitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubentitiesApplication.class, args);
+	}
+
+}
