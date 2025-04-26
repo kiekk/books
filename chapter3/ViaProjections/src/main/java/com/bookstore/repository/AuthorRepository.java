8cb124834976f7d6dbffd6570d6023b6cbff4fc1
shyoon <shyoon991@gmail.com>
2025-04-26 20:33:04 +0900

    chapter3-25: 인터페이스 기반 닫힌 프로젝션(interface-based closed projection)
    * 프로젝션에 정의된 메서드는 엔터티 속성의 이름과 정확히 일치한다.

--- chapter3/ViaProjections/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ chapter3/ViaProjections/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -3,5 +3,14 @@ package com.bookstore.repository;
 import com.bookstore.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<AuthorNameAge> findFirst2ByGenre(String genre);
+
+    interface AuthorNameAge {
+        String getName();
+
+        int getAge();
+    }
 }
