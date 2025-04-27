0501dca5838c14e3b6ec617bce603a5e6df1394b
shyoon <shyoon991@gmail.com>
2025-04-27 21:36:15 +0900

    chapter3-27: @Value를 사용하여 도메인 속성이 아닌 virtual 속성 포함하기
    * 도메인 모델에서 이름이 일치하지 않고 실행 시에 반영되는 결과를 반환하는 메서드를 갖는 인터페이스 기반 프로젝션을 인터페이스 기반 열린 프로젝션(interface-based open projection)이라고 한다.

--- chapter3/DtoViaProjectionsAndVirtualProperties/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ chapter3/DtoViaProjectionsAndVirtualProperties/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -1,7 +1,14 @@
 package com.bookstore.repository;
 
+import com.bookstore.dto.AuthorNameAge;
 import com.bookstore.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query("SELECT a.name AS name, a.age AS age FROM Author a WHERE a.age >= :age")
+    List<AuthorNameAge> fetchByAge(@Param("age") int age);
 }
