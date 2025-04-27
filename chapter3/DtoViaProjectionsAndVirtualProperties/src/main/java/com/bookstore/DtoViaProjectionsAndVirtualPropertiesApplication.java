3af72cc20bed19ea71c67fd86df95ff3463ef170
shyoon <shyoon991@gmail.com>
2025-04-27 21:28:26 +0900

    chapter3-27: 엔터티의 일부 또는 외부 가상 속성을 통한 스프링 프로젝션 보완 방법 - Project Init

---
+++ chapter3/DtoViaProjectionsAndVirtualProperties/src/main/java/com/bookstore/DtoViaProjectionsAndVirtualPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoViaProjectionsAndVirtualPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtoViaProjectionsAndVirtualPropertiesApplication.class, args);
+    }
+
+}
