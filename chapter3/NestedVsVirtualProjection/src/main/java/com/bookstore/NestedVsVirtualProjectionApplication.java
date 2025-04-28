eb5fe8537623bf9c3f9a62ffeb48d5c32f452269
shyoon <shyoon991@gmail.com>
2025-04-28 20:38:55 +0900

    chapter3-28: *-to-One 연관관계를 포함하는 스프링 프로젝션의 효율적 로딩 방법 - Project Init

---
+++ chapter3/NestedVsVirtualProjection/src/main/java/com/bookstore/NestedVsVirtualProjectionApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestedVsVirtualProjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NestedVsVirtualProjectionApplication.class, args);
+    }
+
+}
---
+++ chapter3/NestedVsVirtualProjection/src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,42 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String genre;
+    private int age;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "author", orphanRemoval = false)
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        this.books.add(book);
+        book.addAuthor(this);
+    }
+
+    public void removeBook(Book book) {
+        book.removeAuthor();
+        this.books.remove(book);
+    }
+
+    public void removeBooks() {
+        Iterator<Book> iterator = this.books.iterator();
+
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+
+            book.removeAuthor();
+            iterator.remove();
+        }
+    }
+}
