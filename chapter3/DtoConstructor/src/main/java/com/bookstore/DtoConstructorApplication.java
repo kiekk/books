7735e73397426c788277f01ab72a175623aa47e1
shyoon <shyoon991@gmail.com>
2025-05-01 16:41:30 +0900

    chapter3-31: 생성자 표현식을 통해 DTO를 가져오는 방법 - Project Init

---
+++ chapter3/DtoConstructor/src/main/java/com/bookstore/DtoConstructorApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoConstructorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtoConstructorApplication.class, args);
+    }
+
+}
