17214ce2c754e2073073c1cb6a2dfc31e427d24f
shyoon <shyoon991@gmail.com>
2025-05-01 16:17:02 +0900

    chapter3-30: 프로젝션 or Object[] 사용
    * 프로젝션을 통해 인터페이스로 반환받는 것과 Object[]로 반환받는 것에는 큰 차이가 있다.
    * 주석에 작성한대로 동작 방식을 확인해보는 것이 좋다.

--- chapter3/JoinDtoAllFields/src/main/java/com/bookstore/JoinDtoAllFieldsApplication.java
+++ chapter3/JoinDtoAllFields/src/main/java/com/bookstore/JoinDtoAllFieldsApplication.java
@@ -1,13 +1,39 @@
 package com.bookstore;
 
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JoinDtoAllFieldsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JoinDtoAllFieldsApplication.class, args);
-	}
+    private final BookstoreService bookstoreService;
+
+    public JoinDtoAllFieldsApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoinDtoAllFieldsApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            System.out.println("\n\n Fetch authors read-only entities:");
+            System.out.println("-----------------------------------------------------------------------------");
+            bookstoreService.fetchAuthorAsReadOnlyEntities();
+
+            System.out.println("\n\n Fetch authors as projection");
+            System.out.println("-----------------------------------------------------------------------------");
+            bookstoreService.fetchAuthorAsProjection();
+
+            System.out.println("\n\n Fetch authors as array of objects");
+            System.out.println("-----------------------------------------------------------------------------");
+            bookstoreService.fetchAuthorAsArrayOfObject();
+        };
+    }
 
 }
--- chapter3/JoinDtoAllFields/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ chapter3/JoinDtoAllFields/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -1,7 +1,17 @@
 package com.bookstore.repository;
 
+import com.bookstore.dto.AuthorDto;
 import com.bookstore.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<AuthorDto> findBy();
+//    @Query("SELECT a.id, a.age, a.name, a.genre FROM Author a") // 엔터티 컬럼 조회
+//    List<Object[]> findBy();
+
+    @Query("SELECT a FROM Author a")
+    List<Object[]> fetchAsArray();
 }
