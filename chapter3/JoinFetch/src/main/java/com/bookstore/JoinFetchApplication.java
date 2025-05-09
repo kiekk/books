6a55ebbf0fac3691cb88c78e516f45bb81b6ed70
shyoon <shyoon991@gmail.com>
2025-05-09 21:04:14 +0900

    chapter3-39: FETCH JOIN으로 연관관계 함께 조회하기
    * 데이터를 수정할 계획이 있을 경우 FETCH JOIN을 사용
    * 읽기 전용인 경우 JOIN + DTO를 사용하여 함께 조회하는 것이 좋다.
    * N+1 또는 카타시안 곱 문제가 발생하기 때문에

--- chapter3/JoinFetch/src/main/java/com/bookstore/JoinFetchApplication.java
+++ chapter3/JoinFetch/src/main/java/com/bookstore/JoinFetchApplication.java
@@ -1,13 +1,34 @@
 package com.bookstore;
 
+import com.bookstore.entity.Author;
+import com.bookstore.entity.Book;
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JoinFetchApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public JoinFetchApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JoinFetchApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            Author author = bookstoreService.fetchAuthorWithBooksByName();
+            System.out.println("\nAuthor name: " + author.getName() + " Books: " + author.getBooks() + "\n");
+
+            Book book = bookstoreService.fetchBookWithAuthorByIsbn();
+            System.out.println("\nTitle: " + book.getTitle() + " author: " + book.getAuthor() + "\n");
+        };
+    }
 }
--- chapter3/JoinFetch/src/main/java/com/bookstore/repository/BookRepository.java
+++ chapter3/JoinFetch/src/main/java/com/bookstore/repository/BookRepository.java
@@ -2,6 +2,12 @@ package com.bookstore.repository;
 
 import com.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query(value = "SELECT b FROM Book b JOIN FETCH b.author WHERE b.isbn = :isbn")
+        // or, via JOIN
+        //@Query(value = "SELECT b, a FROM Book b JOIN b.author a WHERE b.isbn = :isbn")
+    Book fetchBookWithAuthorByIsbn(@Param("isbn") String isbn);
 }
