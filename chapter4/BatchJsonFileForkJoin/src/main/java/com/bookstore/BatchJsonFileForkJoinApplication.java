2efdd005d56b1b2dd01002f84ec988284f66c4fd
shyoon <shyoon991@gmail.com>
2025-06-03 20:35:30 +0900

    chapter4-49: 포크/조인 배치 처리 동작 확인

--- chapter4/BatchJsonFileForkJoin/src/main/java/com/bookstore/BatchJsonFileForkJoinApplication.java
+++ chapter4/BatchJsonFileForkJoin/src/main/java/com/bookstore/BatchJsonFileForkJoinApplication.java
@@ -1,13 +1,30 @@
 package com.bookstore;
 
+import com.bookstore.forkjoin.ForkJoinService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BatchJsonFileForkJoinApplication {
+
+    private static final String FILE_NAME = "citylots.json";
+
+    private final ForkJoinService forkJoinService;
+
+    public BatchJsonFileForkJoinApplication(ForkJoinService forkJoinService) {
+        this.forkJoinService = forkJoinService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BatchJsonFileForkJoinApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            forkJoinService.fileToDatabase(FILE_NAME);
+        };
+    }
 }
