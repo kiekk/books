9b4162643dfa3ac2d5ab5cc81068bde8e9a7c71c
shyoon <shyoon991@gmail.com>
2025-06-04 20:18:33 +0900

    chapter4-50: CompletableFuture를 통한 엔터티 배치 처리 - Project Init

---
+++ chapter4/BatchInsertsCompletableFuture/src/main/java/com/bookstore/BatchInsertsCompletableFutureApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchInsertsCompletableFutureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchInsertsCompletableFutureApplication.class, args);
+    }
+
+}
