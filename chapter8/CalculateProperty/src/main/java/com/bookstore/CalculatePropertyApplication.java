f2e6383c0327ba9853fb3ef1b567dd2e58d38ca6
shyoon <shyoon991@gmail.com>
2025-06-13 20:32:33 +0900

    chapter8-77: 산출된 비영속 속성 매핑 방법 - getter 메서드에 @Transient 사용하여 비영속 속성 계산
    * 매번 호출 시 로직을 계산한다는 단점이 있다.

--- chapter8/CalculateProperty/src/main/java/com/bookstore/CalculatePropertyApplication.java
+++ chapter8/CalculateProperty/src/main/java/com/bookstore/CalculatePropertyApplication.java
@@ -1,13 +1,26 @@
 package com.bookstore;
 
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CalculatePropertyApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public CalculatePropertyApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatePropertyApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner init() {
+        return args -> bookstoreService.fetchBooks();
+    }
 }
--- chapter8/CalculateProperty/src/main/java/com/bookstore/entity/Book.java
+++ chapter8/CalculateProperty/src/main/java/com/bookstore/entity/Book.java
@@ -3,6 +3,7 @@ package com.bookstore.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,5 +12,21 @@ public class Book {
     private String title;
     private String isbn;
     private double price;
-    private double discounted;
+
+    @Transient
+    public double getDiscounted() {
+        System.out.println("Call getDiscounted() method");
+        return this.price - this.price * 0.25;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", price=" + price +
+                ", discounted=" + getDiscounted() +
+                '}';
+    }
 }
