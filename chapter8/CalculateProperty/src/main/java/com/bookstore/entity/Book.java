45dcf376ff01f4bd1f6d845df8e1304ff89c384f
shyoon <shyoon991@gmail.com>
2025-06-13 20:34:21 +0900

    chapter8-77: 산출된 비영속 속성 매핑 방법 - 필드에 @Transient 사용 & @PostLoad 애노테이션 사용하여 개선
    * 엔터티가 로드된 후에 한 번 속성을 계산하고 이후에는 계산한 값을 반환

--- chapter8/CalculateProperty/src/main/java/com/bookstore/entity/Book.java
+++ chapter8/CalculateProperty/src/main/java/com/bookstore/entity/Book.java
@@ -12,11 +12,21 @@ public class Book {
     private String title;
     private String isbn;
     private double price;
+    @Transient
+    private double discounted;
 
+    /*
     @Transient
     public double getDiscounted() {
         System.out.println("Call getDiscounted() method");
         return this.price - this.price * 0.25;
+    }
+    */
+
+    @PostLoad
+    private void postLoad() {
+        System.out.println("Call postLoad() method");
+        this.discounted = this.price - this.price * 0.25;
     }
 
     @Override
@@ -26,7 +36,7 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", price=" + price +
-                ", discounted=" + getDiscounted() +
+                ", discounted=" + discounted +
                 '}';
     }
 }
