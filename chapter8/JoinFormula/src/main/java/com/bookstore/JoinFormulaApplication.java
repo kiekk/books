bd7842b2f1f119b9308740c718d9e7e9fc8d8cf1
shyoon <shyoon991@gmail.com>
2025-06-14 09:25:08 +0900

    chapter8-80: @JoinFormula를 통해 @ManyToOne 관계를 SQL 쿼리에 매핑하는 방법 - Project Init

---
+++ chapter8/JoinFormula/src/main/java/com/bookstore/JoinFormulaApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinFormulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoinFormulaApplication.class, args);
+    }
+
+}
