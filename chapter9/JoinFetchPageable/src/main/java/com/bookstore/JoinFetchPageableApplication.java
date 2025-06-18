d0f85fce1fad88b737ae1b6597d713f265687cae
shyoon <shyoon991@gmail.com>
2025-06-18 10:18:55 +0900

    chapter13-97: JOIN FETCH 및 Pageable 사용 방법 - Project Init

---
+++ chapter9/JoinFetchPageable/src/main/java/com/bookstore/JoinFetchPageableApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinFetchPageableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinFetchPageableApplication.class, args);
+	}
+
+}
---
+++ chapter9/JoinFetchPageable/src/main/java/com/bookstore/entity/Book.java
@@ -0,0 +1,17 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
