e2b612461a56e5efdbc7679876987b3f8260c621
shyoon <shyoon991@gmail.com>
2025-06-18 09:36:13 +0900

    chapter13-95: COUNT(*) OVER & DTO 프로젝션으로 페이지네이션 최적화

--- chapter9/PageDtoOffsetPaginationWF/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ chapter9/PageDtoOffsetPaginationWF/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -1,7 +1,15 @@
 package com.bookstore.repository;
 
+import com.bookstore.dto.AuthorDto;
 import com.bookstore.entity.Author;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+    @Query(value = "SELECT name, age, COUNT(*) OVER() AS total FROM author",
+            nativeQuery = true)
+    List<AuthorDto> fetchAll(Pageable pageable);
 }
