e7f2e391e0301e7426ac74c7cd048e03380b186a
shyoon <shyoon991@gmail.com>
2025-06-20 08:10:34 +0900

    chapter16-144: CLOB 및 BLOB 매핑 방법 - Project Init

---
+++ chapter16/MappingLob/src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,17 @@
+package com.bookstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int age;
+    private String name;
+    private String genre;
+}
